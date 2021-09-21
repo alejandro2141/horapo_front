115507c0fd45a5eaa2a3db60cd6ddc2c891dc41d
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-21 18:54:58 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+/*
 export default defineConfig({
+  plugins: [vue()]
+})
+*/
+const { resolve } = require('path')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        aboutUs: resolve(__dirname, './nested/aboutUs.html'),
+        register: resolve(__dirname, './nested/register.html'),
+        getappointments: resolve(__dirname, './nested/getAppointments.html')
+
+      }
+    }
+  },
   plugins: [vue()]
 })
