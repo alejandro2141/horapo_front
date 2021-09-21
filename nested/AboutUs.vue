3f3af90888db56dbcc355fdbc5b40cb5236aa2e9
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-21 18:56:44 -0400

    Create AboutUs.vue

---
+++ nested/AboutUs.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+import GeneralHeader from '../src/components/GeneralHeader.vue'
+
+</script>
+
+<template>
+<GeneralHeader></GeneralHeader>
+    <div id="app" class="text-center m-4" >     
+   
+        123Hora es una plataforma de agendamiento de horas profesionales, aqui puede buscar y agendar una hora con profesionales de Psicologia, Kinesiologia y otras terapias. <br> Esta plataforma reune a los mejores profesionales en un solo lugar para sus pacientes.
+        <br>
+    
+        <div class="mt-4 pt-4">
+        
+        <hr>
+            <a HREF="/index.html" type="button" class="btn btn-outline-secondary">Regresar</a>
+        </div>
+    </div>
+
+</template>
+
+
+<style scoped>
+
+
+</style>
