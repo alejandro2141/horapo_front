378e67b88a392b883434d7fc5cf2512671b83ded
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-21 18:56:05 -0400

    Create register.vue

---
+++ nested/register.vue
@@ -0,0 +1,21 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import GeneralHeader from '../src/components/GeneralHeader.vue'
+import RegisterForm from '../src/components/RegisterForm.vue'
+
+</script>
+
+<template>
+<GeneralHeader></GeneralHeader>
+    <RegisterForm></RegisterForm>
+
+</template>
+
+
+<style scoped>
+
+
+</style>
+
+
