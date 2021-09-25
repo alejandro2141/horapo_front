8ca6a7e18830a8db6af5f684fa78e45971a7ad95
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-24 22:10:36 -0400

    Create publicSearch.vue

---
+++ nested/publicSearch.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+import GeneralHeader from '../src/components/GeneralHeader.vue'
+import SearchAppointment from '../src/components/publicSearch/SearchAppointment.vue'
+
+</script>
+
+<template>
+
+
+<GeneralHeader></GeneralHeader>
+    <div id="app" class="text-center m-4" >     
+        PUBLIC SEARCH     
+        <SearchAppointment></SearchAppointment>
+    </div>
+
+</template>
+
+
+<style scoped>
+
+
+</style>
