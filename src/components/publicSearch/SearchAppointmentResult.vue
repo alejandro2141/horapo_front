91dd373e6fb3b8b0f241cb1a6868c2886698ac76
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-24 22:09:57 -0400

    Create SearchAppointmentResult.vue

---
+++ src/components/publicSearch/SearchAppointmentResult.vue
@@ -0,0 +1,99 @@
+<script setup>
+import { ref } from 'vue'
+import patientAppointmentAvailable  from './PatientAppointmentAvailable.vue'
+import modalReserveAppointment from './modalReserveAppointment.vue'
+
+defineProps({
+  msg: String,
+  session_params : Array  ,
+  appointments  : Array , 
+  daterequired : String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+       <div class="" v-if="appointment_list != null && daterequired != null" >
+            <small class="mb-2 pl-3" > {{ notificationMessage }} </small> 
+         
+            <div class="mt-0"  v-for="appointment in appointment_list" :key="appointment.id" >
+               <patientAppointmentAvailable v-if="appointment != null"  v-on:click="setModalReserve(appointment)" :appointment='appointment'  > </patientAppointmentAvailable>            
+            </div>
+         
+            
+                <!-- Start make room for Modal data when it display-->
+                <div style="height: 400px">
+                </div>
+        
+        </div>	
+        
+        <!-- Modal Reserve and Confirm  as Component with a teleport to Main Page -->
+        <modalReserveAppointment  :app="app" v-on:showConfirmationModal="showConfirmationModal"  > </modalReserveAppointment>
+        <!-- Modal Reserve and Confirm End -->
+          
+
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+
+
+export default {
+  data() {
+    return {
+            app : null ,
+            showModal : false , 
+            showModalConfirmation : false ,
+            appConfirmed : null, 
+            appointment_list : null,
+            notificationMessage: null, 
+    }
+  },
+   
+   mounted () {    
+        },
+
+    watch: {
+            appointments(newAppointments, oldAppointments ) {
+                this.appointment_list =  newAppointments ;   
+                this.notificationMessage="Econtramos "+this.appointments.length+" resultados, desde dia "+this.daterequired +" ";	
+                      
+            },
+        },
+
+    methods: {
+            setModalReserve(appointment)
+            {
+                console.log("Set Modal Reserve method in SearchApp Resutl"+JSON.stringify(appointment));
+                this.app=appointment;
+              
+            },
+            showConfirmationModal(appconfirm)
+            {
+                console.log ("showConfirmationModal method. Display Confirmation Modal: "+ JSON.stringify(appconfirm)) ;
+                this.appConfirmed=appconfirm;
+            },
+
+            updateSearchResult()
+            {
+                console.log (" update search Result. ");
+                //this.appointment_list=null ;
+                this.$emit('updateLastSearch');
+            },
+
+            selectedInsuranceCode(code)
+            {
+            console.log("Insurance Code:"+code);
+            this.form_patient_insurance_code = code;
+            },
+    
+
+        },
+
+}
+</script>
