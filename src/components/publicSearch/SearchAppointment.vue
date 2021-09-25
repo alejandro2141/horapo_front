23d15959f527c4dde636cdfde4376f10f94285e7
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-24 22:10:06 -0400

    Create SearchAppointment.vue

---
+++ src/components/publicSearch/SearchAppointment.vue
@@ -0,0 +1,99 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+   SearchAppointment Template
+        <div>
+            <div>
+            <searchAppointmentForm  v-on:searchAppointments="searchAppointments"></searchAppointmentForm>
+           
+            <searchAppointmentResult  v-if="daterequired != null && appointments != null"  v-on:updateLastSearch="updateLastSearch"  :appointments="appointments" :daterequired="daterequired"  > </searchAppointmentResult> 	    
+            </div>
+        </div>
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+//const showForm = ref(false)
+import searchAppointmentForm  from './SearchAppointmentForm.vue' 
+import searchAppointmentResult  from './SearchAppointmentResult.vue'
+
+export default {
+  data() {
+    return {
+            daterequired : null,
+            specialty: null,
+            location: null,
+            insurance: null,
+            params_bkp: null, 
+           
+            appointments : [] ,
+            appointment : null ,
+            show_modal : null ,
+            appointment_confirm : null ,
+            key : 0 ,
+
+            notificationMessage : null,
+            notificationMessage_alert : false,
+    }
+  },
+
+ mounted () {
+             this.daterequired = new Date().toISOString().split('T')[0] ;
+            
+        },
+
+methods: {
+        show_modal_appointment_confirm(param)
+        {
+            console.log("Modal Confirmation: show_modal_appointment_confirm:"+JSON.stringify(param) )
+            this.appointment_confirm = param ;
+        },
+
+        set_daterequired : function (year_month_day) {
+            console.log("TAB HOME GoToDay "+year_month_day);
+           // this.getAppointments(year_month_day);
+            this.daterequired = year_month_day ;
+            },
+
+        async searchAppointments(params) {	
+            this.params_bkp = params ; 
+                console.log("search Appointments input "+JSON.stringify(params) )
+                 const json = { 
+				// agenda_id : this.par_agenda_id ,			 
+				 date : this.daterequired ,
+				 specialty : params.specialty ,
+                 comuna : params.comuna ,
+                 insurance : params.insurance,
+                  		  };
+				console.log ("getAppointments REQUEST :"+ JSON.stringify(json)  );
+				let response_json = await axios.post("http://192.168.0.110:8080/patient_get_appointments_day",json);
+				console.log ("getAppointments RESPONSE:"+JSON.stringify(response_json.data.rows)) ;
+				this.appointments = response_json.data.rows;
+               // this.notificationMessage="Econtramos "+this.appointments.length+" resultados, desde dia "+this.daterequired +" ";	
+                this.notificationMessage_alert=	false ;
+
+            },
+
+            updateLastSearch()
+            {
+                console.log ("update Last Search") ;
+                this.searchAppointments(this.params_bkp);
+            }
+
+
+        },
+
+}
+</script>
