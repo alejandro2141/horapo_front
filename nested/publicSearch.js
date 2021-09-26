a2ac393a77a3dff86686093cfe062cb97d291b23
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-26 17:58:39 -0400

    Update publicSearch.js

--- nested/publicSearch.js
+++ nested/publicSearch.js
@@ -3,4 +3,10 @@ import App from './publicSearch.vue'
 
 
 createApp(App).mount('#app')
+//App.config.globalProperties.db_host_config = 'http://192.168.0.110:8080/'
+/*
+// After
+const app = createApp({})
+app.config.globalProperties.db_host = 'http://192.168.0.110:8080/'
 
+*/
