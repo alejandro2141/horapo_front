2e2f305ade17fed2c5ef9bd42527442fe56f8de4
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-27 12:17:19 -0400

    Create config123.js

---
+++ config123.js
@@ -0,0 +1,11 @@
+
+let BKND_CONFIG ;
+
+BKND_CONFIG = {
+    $BKND_HOST : "http://192.168.0.110:8080",
+    $BKND_TIME: 1000,
+    $BNKD_USER: 'abc'
+  };
+
+export {BKND_CONFIG}
+//app.config.globalProperties.BKD_HOST = 'http://192.168.0.110:8080'
