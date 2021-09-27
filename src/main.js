2613b7105b515d908d4895d60930014aaf9ab5f2
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-27 12:18:33 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { BKND_CONFIG } from '../config123.js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+app.config.globalProperties.BKND_CONFIG = BKND_CONFIG
+
+console.log(app.config)
