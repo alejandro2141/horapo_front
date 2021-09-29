a5a4d3470637b0bfdd63f9e7d2492206ec2aace1
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-29 16:36:13 -0400

    Update RecoverAppointmentsForm.vue

--- src/components/RecoverAppointmentsForm.vue
+++ src/components/RecoverAppointmentsForm.vue
@@ -3,36 +3,33 @@ import { ref } from 'vue'
 import GeneralHeader from './GeneralHeader.vue';
 import LandingContent from './LandingContent.vue'
 import FooterContent from './FooterContent.vue'
-
+import axios from 'axios';
 
 defineProps({
   nameTitle : String
 })
 
-const count = ref(0)
+ 
 </script>
 
+
+
+
 <template>
-   
-        <div id="form" class=" p-0">
-            <form autocomplete="off">
-                    <div class="form-row">
-            
-                        <div class="form-group col-md-6">
-                            <label for="inputNames"></label>
-                            <i class="bi bi-mailbox h1"></i> <input type="text" class="form-control" id="inputNames" placeholder="Ej: Email " v-model="email">
-                        </div>
-                       
-                    </div>
-                    
-                    <div class="pt-2 text-center m-2 p-2">
-                     <a  HREF="/index.html" type="button" class="btn btn-outline-secondary m-2 p-2 ">Regresar</a>
-                     <button  v-on:click="sendRecoverApp()" type="button" class="btn btn-success m-2 p-2 ">Enviar</button>
-                   </div>
+<div class="p-0" id="form"><form autocomplete="off">
+    
+    <div class="form-row"><div class="form-group col-md-6">
+        <label for="inputNames"></label>
 
-            </form>
+        <input  type="text" placeholder="Ej: Email " v-model='user_email' ></div></div>
+        
+        <div class="pt-2 text-center m-2 p-2"><a class="btn btn-outline-secondary m-2 p-2" HREF="/index.html" type="button">Regresar</a>
+        
+        <button class="btn btn-success m-2 p-2" v-on:click="sendRecoverApp()" type="button">Enviar</button>
         </div>
-
+        </form>
+        {{ user_email }}
+        </div>
 
 </template>
 
@@ -43,3 +40,47 @@ body {
 
 }
 </style>
+
+
+<script>
+
+export default {
+    data : function() {
+        return {
+            user_email : null ,
+           }   
+    },
+    
+    mounted () {   
+       },
+
+    computed: {
+        },
+        
+    methods: {
+
+            async sendRecoverApp()
+            {
+               console.log("sendRecoverApp");  
+                
+               // var r =confirm("Continuar con la Reservar esta cita?");
+               var r = true;
+                 
+               if (r == true) {
+                    const json = { 
+                        email : this.user_email,
+                                };
+                    console.log ("Send Recover Appointments   REQUEST :"+ JSON.stringify(json)  );
+                    let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/recover_appointments",json );
+                    console.log ("Send Recover Appointments RESPONSE:"+JSON.stringify(response_json.data)) ;
+                    window.alert("Las citas registradas seran enviadas a su correo");
+                    window.location.replace("/index.html");
+                    }
+                //location.href = "professional_view_appointments_agenda.html?"+this.getUrlParam() ;
+                //location.reload();
+            },
+
+        },
+
+}
+</script>
