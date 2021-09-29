a09ceefd5695a1bfe14065ff81bfb9ad395a2d91
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-09-29 16:36:17 -0400

    Update RegisterForm.vue

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-
+import axios from 'axios';
 
 </script>
 
@@ -21,51 +21,51 @@ import { ref } from 'vue'
 
                 <div class="form-group col-md-6">
                         <label for="inputNames">Nombres</label>
-                        <input type="text" class="form-control" id="inputNames" placeholder="Ej: Carlos Alberto" v-model="name">
+                        <input type="text" autocomplete="off" class="form-control" id="inputNames" placeholder="Ej: Carlos Alberto" v-model="name">
                 </div>
                 
                 <div class="form-group col-md-6">
                     <label for="inputLastName">Apellido Paterno</label>
-                    <input type="text" class="form-control" id="inputLastName" placeholder="Ej: Morales" v-model="last_name1">
+                    <input type="text" autocomplete="off"  class="form-control" id="inputLastName" placeholder="Ej: Morales" v-model="last_name1">
                 </div>
 
                 <div class="form-group col-md-6">
                     <label for="inputSecondLastName">Apellido Materno</label>
-                    <input type="text" class="form-control" id="inputSecondLastName" placeholder="Ej:Gonzales" v-model="last_name2">
+                    <input type="text"  autocomplete="off" class="form-control" id="inputSecondLastName" placeholder="Ej:Gonzales" v-model="last_name2">
                 </div>
 
                 <div class="form-group col-md-6">
                     <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder="Ej: micorreopersonal@gmail.com " v-model="email">
+                    <input type="email"  autocomplete="off" class="form-control" id="inputEmail4" placeholder="Ej: micorreopersonal@gmail.com " v-model="email">
                 </div>
 
                 <div class="form-group col-md-6">
                     <label for="inputDocId">N° Cedula RUT </label>
-                    <input type="text" class="form-control" id="inputDocId" placeholder="Ej 13909365-8" v-model="doc_id">
+                    <input type="text"  autocomplete="off" class="form-control" id="inputDocId" placeholder="Ej 13909365-8" v-model="doc_id">
                 </div>
 
                 <div class="form-group col-md-6">
                     <label for="inputPassword4">Ingrese una contraseña</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="***" v-model="passwd">
+                    <input type="password"  autocomplete="off" class="form-control" id="inputPassword4" placeholder="***" v-model="passwd">
                 </div>
 
                 </div>
                 <div class="form-group">
                 <label for="inputAddress">Direccion Particular</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="Ej: Av providencia 1234, Providencia, Santiago" v-model="personal_address">
+                <input type="text"  autocomplete="off" class="form-control" id="inputAddress" placeholder="Ej: Av providencia 1234, Providencia, Santiago" v-model="personal_address">
                 </div>
 
                 <div class="form-group">
                     <label for="inputPhone">Telefono personal de contacto </label>
-                    <input type="text" class="form-control" id="inputPhone" placeholder="Ej: +56975397201" v-model="personal_phone">
+                    <input type="text"  autocomplete="off" class="form-control" id="inputPhone" placeholder="Ej: +56975397201" v-model="personal_phone">
                 </div>
 
                 <div class="form-group">
                     <label for="inputSpecialty">Ingrese su especialidad</label>
-                    <input type="text" class="form-control" id="specialty" placeholder="Ej: Kinesiologia, psicologia, terapia.." v-model="specialty">
+                    <input type="text"  autocomplete="off"  class="form-control" id="specialty" placeholder="Ej: Kinesiologia, psicologia, terapia.." v-model="specialty">
                 </div>
 
-                <div class="text-center pt-5 mb-5 pb-5">
+                <div class="text-center pt-5 mb-5 pb-5" >
                     <button @click='sendFormRegister' class="btn btn-primary">Registrarme!</button>
                 </div>
 
@@ -84,8 +84,10 @@ import { ref } from 'vue'
 
 <script>
 //const showForm = ref(false)
+
+
 export default {
-  data() {
+  data : function() {
     return {
        showForm : false ,
        name : null ,
@@ -127,7 +129,7 @@ export default {
                         };
                  //app.config.globalProperties.dbhost = 'http://192.168.0.114:8080' ;       
                 console.log ("sendReserveAppointment  REQUEST :"+ JSON.stringify(json)  );
-                let response_json = await axios.post(this.BKND_CONFIG.$BKND_HOST+"/public_register_professional",json );
+                let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/public_register_professional",json );
                 console.log ("RESPONSE:"+JSON.stringify(response_json.data)) ;
                 //app = response_json.data[0];
                 console.log ("Rows : "+JSON.stringify(response_json.data.rows[0].name )  );
@@ -137,7 +139,7 @@ export default {
                     //display modal 
                     //this.display = true ;
                 window.alert("Gracias por su registro, pronto nos contactaremos a su correo");
-                window.location.replace("index.html");
+                window.location.replace("/index.html");
                 // this.$emit('showConfirmationModal', response_json.data );
                 //}
 
