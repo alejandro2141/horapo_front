a6392f7393250294218c7a4e3df3e7afb20865cb
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-10-08 23:42:48 -0400

    Update config123.js

--- config123.js
+++ config123.js
@@ -2,6 +2,7 @@
 let BKND_CONFIG ;
 
 BKND_CONFIG = {
+//    BKND_HOST : "http://ec2-18-222-88-128.us-east-2.compute.amazonaws.com:8080",
     BKND_HOST : "http://192.168.0.110:8080",
     BKND_TIME : 1000,
     BNKD_USER : 'abc'
@@ -9,5 +10,5 @@ BKND_CONFIG = {
 
 export { BKND_CONFIG } ;
 //app.config.globalProperties.BKD_HOST = 'http://192.168.0.110:8080'
-//app.config.globalProperties.BKD_HOST = 'http://ec2-18-222-88-128.us-east-2.compute.amazonaws.com/'
+//app.config.globalProperties.BKD_HOST = 'http://ec2-18-222-88-128.us-east-2.compute.amazonaws.com:8080'
 
