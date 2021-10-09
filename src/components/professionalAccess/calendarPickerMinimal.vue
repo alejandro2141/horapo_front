76a4d8e0df2b25beaae34f15af5425586bcf3533
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-10-08 23:43:42 -0400

    Create calendarPickerMinimal.vue

---
+++ src/components/professionalAccess/calendarPickerMinimal.vue
@@ -0,0 +1,124 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+
+
+</script>
+
+<template>
+    
+    <div class="text-center">
+        <div class="display-1">
+          {{this.calendar[parseInt(this.req_year)][parseInt(this.req_month)][parseInt(this.req_day)] }}
+        </div>
+        <div class="display-1">
+            <i v-on:click="prevDay()" class="bi bi-caret-left"></i>  {{this.req_day}} <i  v-on:click="nextDay()" class="bi bi-caret-right"></i>
+        </div>
+        <div class="display-4">
+            <i v-on:click="prevMonth()" class="bi bi-caret-left display-1"></i> {{ this.calendar[parseInt(this.req_year)][parseInt(this.req_month)][0]  }} 20{{this.req_year}}<i v-on:click="nextMonth()" class="bi bi-caret-right display-1"></i>
+        </div>
+    </div>
+    <hr>
+
+</template>
+
+<style scoped>
+</style>
+
+
+<script>
+
+export default {
+   data : function() {
+        return {
+        calendar : null, 
+        today_day : null,
+        today_month : null,
+        today_year : null,
+
+        req_day : null,
+        req_month : null,
+        req_year : null,
+        }   
+    },
+   	
+    props: ['required_day'],
+    
+    emits: ['set_daterequired'] ,
+
+	created () {
+		console.log("CREATED CALENDAR-PICKER");
+		let tempDate=new Date().toISOString().split('T')[0].split('-');
+	    console.log("tempDate : "+tempDate);
+        this.today_day = parseInt(tempDate[2])
+        this.today_month = parseInt(tempDate[1])
+        this.today_year = parseInt(tempDate[0].substring(2,4))
+
+        this.req_day = this.today_day ,
+        this.req_month = this.today_month,
+        this.req_year = this.today_year,
+
+		console.log("tday:"+ this.today_day + " tmont:"+this.today_month + " tyear:"+this.today_year );
+  
+        this.setCalendar();
+	},
+	methods :{
+        nextDay()
+        {
+            this.req_day++;
+            console.log("NextDay method");
+            this.$emit('set_daterequired', "20"+this.req_year+"-"+this.req_month+"-"+ this.req_day ) ;
+        },
+        prevDay()
+        {
+            this.req_day--;
+            console.log("NextDay method");
+            this.$emit('set_daterequired', "20"+this.req_year+"-"+this.req_month+"-"+ this.req_day ) ;
+      
+        },
+        nextMonth()
+        {
+            this.req_month++;
+            console.log("NextMonth method");
+            this.$emit('set_daterequired', "20"+this.req_year+"-"+this.req_month+"-"+ this.req_day ) ;
+        },
+        prevMonth()
+        {
+            this.req_month--;
+            console.log("NextMonth method");
+            this.$emit('set_daterequired', "20"+this.req_year+"-"+this.req_month+"-"+ this.req_day ) ;
+        },
+
+
+        setCalendar()
+        {
+        console.log("set Calendar method");
+        this.calendar = [] ;
+        this.calendar[21] = [] ;
+        this.calendar[22] = [] ;
+        this.calendar[21][10] = [] ;
+        this.calendar[21][11] = [] ;
+        this.calendar[21][10] = [ "Octubre", 
+                                            "Viernes" , "Sabado" , "Domingo", 
+                                            "Lunes" , "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo", 
+                                            "Lunes" , "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo", 
+                                            "Lunes" , "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo", 
+                                            "Lunes" , "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"    ] ;
+
+        this.calendar[21][11] = [ "Noviembre", "Lunes" , "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo", "Lunes" , "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo", "Lunes" , "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo", "Lunes" , "Martes"     ] ;
+        console.log("calendar al zoom "+ JSON.stringify(this.calendar )  );
+        },
+
+	 //********* GO TO DAY
+	
+    setRequiredDay(year_month_day) {
+    console.log("goToDay: "+year_month_day);
+	this.required_day_month=year_month_day.substring(5,10);
+	 //$emit('goToDay',year_month_day) ;
+	}
+   
+
+    }
+}
+</script>
+
