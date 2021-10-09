1337647c2e4d4b1c9ae8fe6ae699ecde664e2026
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-10-08 23:43:15 -0400

    Update GeneralHeader.vue

--- src/components/GeneralHeader.vue
+++ src/components/GeneralHeader.vue
@@ -10,9 +10,11 @@ const count = ref(0)
 
 <template>
 
-    <div class="text-center pt-0 pb-0 text-dark ">
+    <div class="text-center pt-2 pb-0 text-dark ">
 		<div class="display-4 "  > 
-			<a HREF="/index.html" class="text-dark text-decoration-none"> <i class="bi bi-arrow-left-square"></i> 123Hora  
+			<a HREF="/index.html" class="text-dark text-decoration-none"> 
+			<i class="bi bi-arrow-left-square"></i> 
+			123Hora  
 			</a> 
 		</div>
 		<div class="text-muted">	Las mejores consultas privadas en un solo lugar
