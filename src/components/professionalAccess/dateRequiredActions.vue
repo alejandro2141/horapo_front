b123a84ab4e528429206fc1f9d64e703cc3bdcf4
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-10-08 23:43:46 -0400

    Create dateRequiredActions.vue

---
+++ src/components/professionalAccess/dateRequiredActions.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+
+
+</script>
+
+<template>
+    <div class="d-flex justify-content-between" >	
+        <div class="m-2">
+            <a style="font-size: 1em;" > Disp</a>  
+        </div>
+        
+        <div class="m-2 display-2">
+        {{daterequired}}
+        </div>
+        
+        <div class="m-2">
+            <a style="font-size: 2em;" data-bs-toggle="modal" data-bs-target="#duplicate_day" > 
+                <i class="far fa-copy"></i> 
+            </a> 
+        </div>   
+    </div>
+</template>
+
+<style scoped>
+</style>
+
+
+<script>
+
+export default {
+   data : function() {
+        return {
+        }   
+    },
+   	
+    props: ['daterequired'],
+
+	created () {
+	},
+	methods :{
+    }
+}
+</script>
+
