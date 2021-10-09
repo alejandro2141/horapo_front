c2d763ab939e9d04cbafaff4346431e90933a607
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-10-08 23:44:13 -0400

    Create tabCenter.vue

---
+++ src/components/professionalAccess/tabCenter.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+
+defineProps({
+  msg: String
+})
+</script>
+
+<template>
+    TAB CENTER
+</template>
+
+<style scoped>
+</style>
+
+<script>
+
+
+</script>
