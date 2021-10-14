1f1713303a63b2e36e57ada76a192599e2e59333
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-10-14 18:52:59 -0400

    Update modalCreateAppointment.vue

--- src/components/professionalAccess/modalCreateAppointment.vue
+++ src/components/professionalAccess/modalCreateAppointment.vue
@@ -4,8 +4,6 @@ import axios from 'axios';
 import InputFormSpecialtyProfessional from './inputFormSpecialtyProfessional.vue';
 import InputFormCenterProfessional from './inputFormCenterProfessional.vue';
 
-
-
 </script>
 
 <template>
@@ -15,56 +13,55 @@ import InputFormCenterProfessional from './inputFormCenterProfessional.vue';
 		    <transition name="modal">
 			<div class="modal-mask "   >
 			<div class="modal-wrapper ">
-			<div class="modal-container  m-1 p-2 modal-background">
+			<div class="modal-container  m-1 p-1 modal-background">
  
-                <div class="modal-header h1" >
-                    <div class="d-flex flex-row bd-highlight mb-3">
-                        <div class="p-2 bd-highlight">Crear Hora <br/>
+                <div class="modal-header" >
+                    <div class="d-flex flex-row bd-highlight mb-1 display-3">
+                        <div class="p-1 bd-highlight">Crear Hora <br/>
                        </div>
-                        <div class="p-2 bd-highlight"></div>
-                         <div class="p-2 bd-highlight"><i class=" bi bi-x display-1"  v-on:click="showModalCreateApp = false" aria-label="Close"></i>
+                        <div class="p-1 bd-highlight"></div>
+                         <div class="p-1 bd-highlight"><i class="display-1 bi bi-x "  v-on:click="showModalCreateApp = false" aria-label="Close"></i>
                        </div>
                     </div>
                 </div>
 
-                <div class="modal-body " > 
-                  
+                <div class="modal-body mt-0" > 
+                 
+
                    <form autocomplete="off"  >	
-
-                    <h1 class="display-4"> Dia {{daterequired}} </h1>
-                    <h1 class="display-4">  Hora Inicio: </h1>
-
-                     <input class="form-control form-control-lg" type="text"  v-model="form_start_time" >
-
+                     
+                      <p class="text-center h1">Dia {{daterequired}} </p>
                       <input class="form-control form-control-lg" type="hidden" placeholder="form_date" name="form_date"   value="par_required_day"  >
-                      <h1 class="display-4"> Especialidad:</h1>
+                      <h2> Hora Inicio: </h2>
+                      <input class="form-control " type="text"  v-model="form_start_time" >
+                      <h2> Tiempo de atencion: </h2>
+                            <select class="form-select form-control-lg" aria-label="Default" id="time" name="timeextension" v-model="form_app_duration">
+                                <option value="15">15 Minutos </option>
+                                <option value="30">30 Minutos </option>
+                                <option value="45">45 Minutos </option>
+                                <option value="60">60 Min </option>
+                                <option value="75">1 Hora 15 Min</option>
+                                <option value="90">1 Hora 30 Min</option>
+                                <option value="105">1 Hora 45 Min</option>
+                                <option value="120">2 Horas</option>
+                                <option value="150">2 Horas 30 Min</option>
+                                <option value="205">3 Horas 45 Min</option>
+                            </select>
+                     
+                      
+                      <h2> Especialidad:</h2>
                         <InputFormSpecialtyProfessional v-on:selectedSpecialtyCode="selectedSpecialtyCode"  :session_params="session_params" > </InputFormSpecialtyProfessional> 
                       
-                      <h1 class="display-4">Centro de Atencion: </h1>
+                      <h2 >Centro de Atencion: </h2>
                         <InputFormCenterProfessional v-on:selectedCenterCode="selectedCenterCode" :session_params="session_params" > </InputFormCenterProfessional> 
                      
-                      <h1 class="display-4"> Tiempo de atencion:</h1> 
-                   
-                        <select class="form-select form-control-lg" aria-label="Default" id="time" name="timeextension" v-model="form_app_duration">
-                            <option value="15">15 Min </option>
-                            <option value="30">30 Min </option>
-                            <option value="45">45 Min </option>
-                            <option value="60">60 Min </option>
-                            <option value="75">1 Hora 15 Min</option>
-                            <option value="90">1 Hora 30 Min</option>
-                            <option value="105">1 Hora 45 Min</option>
-                            <option value="120">2 Horas</option>
-                            <option value="150">2 Horas 30 Min</option>
-                            <option value="205">3 Horas 45 Min</option>
-                        </select>
-                                         
-                        <h1 class="display-3">Citas Publicas Internet</h1> 
+                      <h2>Citas Publicas Internet</h2> 
                       <div class="row">
                         <div class="col">
                           <i class="display-1 fas fa-wifi"></i>
                         </div>
                         <div class="col">
-                          <select  class="autocomplete form-select form-select-lg mb-3" placeholder="Disonible Internet"  aria-label=".form-select-lg example" id="form_public"  v-model="form_public" name="form_public" >
+                          <select  class="autocomplete form-select form-select mb-2" placeholder="Disonible Internet"  aria-label=".form-select-lg example" id="form_public"  v-model="form_public" name="form_public" >
                             <option value="0">NO</option>
                             <option value="1">SI</option>
                           </select>
@@ -87,6 +84,7 @@ import InputFormCenterProfessional from './inputFormCenterProfessional.vue';
 
 <style scoped>
 
+
 .modal {
   position: absolute;
   display: flex;
@@ -96,10 +94,75 @@ import InputFormCenterProfessional from './inputFormCenterProfessional.vue';
   display: flex;
   flex-direction: column;
 }
- .modal-background {
+/*
+.modal-background {
     background-color:#DAEFF3
         }
+*/
+/************************* */
 
+
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+ /*width: 100px;*/
+  margin: 2px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  display: block;
+  margin-top: 1rem;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
+
+/************************ */
 </style>
 
 
@@ -120,7 +183,7 @@ export default {
     },
    	
    props: ['daterequired','hourCreate', 'session_params' ],
-   emits: ['UpdateHoursList'] , 
+   emits: ['updateAppList'] , 
       
    	mounted () {
            this.auxHourCreate = ref(this.hourCreate) ;
@@ -160,6 +223,9 @@ export default {
     },
 
 	  async createHours() {
+    	var r =confirm("Se procedera a crear esta hora en su agenda");
+					  if (r == true) {
+
         const json = { 
               form_center_id  : this.form_center_code ,
               form_professional_id : this.session_params.professional_id  ,
@@ -177,7 +243,11 @@ export default {
         //location.reload();
         //this.agendas = response_json.data.rows;
         //location.reload();
-        this.$emit('UpdateHoursList');
+        this.$emit('updateAppList');
+        this.showModalCreateApp = false ;
+            }
+
+
         }
 
 
