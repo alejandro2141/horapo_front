b92b226b9cf9cf71d3b3536685404e42099769f4
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-10-20 18:19:26 -0400

    Update modalShowAppointmentDetails.vue

--- src/components/professionalAccess/modalShowAppointmentDetails.vue
+++ src/components/professionalAccess/modalShowAppointmentDetails.vue
@@ -4,10 +4,10 @@ import axios from 'axios';
 import InputFormSpecialtyProfessional from './inputFormSpecialtyProfessional.vue';
 import InputFormCenterProfessional from './inputFormCenterProfessional.vue';
 
+
 </script>
 
 <template>
-
 	<teleport to="body"   >
 		<div  v-if="showModalAppointmentDetais" class="modal bg-secondary"    >
 		    <transition name="modal">
@@ -29,9 +29,9 @@ import InputFormCenterProfessional from './inputFormCenterProfessional.vue';
                    <form autocomplete="off" class="p-2"  >	
                     
                       <input class="form-control form-control-lg" type="hidden" placeholder="form_date" name="form_date"   value="par_required_day"  >
-                      <h2> Hora Inicio: </h2>
+                      <h3> Hora Inicio: </h3>
                       <input class="form-control " type="text"  v-model="form_start_time" >
-                      <h2 class="p-2"> Tiempo de atencion: </h2>
+                      <h3 class=""> Tiempo de atencion: </h3>
                             <select class="form-select form-control-lg" aria-label="Default" id="time" name="timeextension" v-model="form_app_duration" >
                                 <option value="15">15 Minutos </option>
                                 <option value="30">30 Minutos </option>
@@ -46,24 +46,25 @@ import InputFormCenterProfessional from './inputFormCenterProfessional.vue';
                             </select>
                      
                       
-                                            <h2 class="p-2">{{ hourDetails.specialty_name}}</h2>
-<!-- <InputFormSpecialtyProfessional v-on:selectedSpecialtyCode="selectedSpecialtyCode"  :session_params="session_params" > </InputFormSpecialtyProfessional> 
-                      -->
-                      <h2 class="p-2" > {{ hourDetails.center_name}} <text class="h5" > {{ hourDetails.center_address}} </text>  </h2>
+                      <h3 class="">{{ hourDetails.specialty_name}}</h3>
+                      <h3 class="" > {{ hourDetails.center_name}} <text class="h5" > {{ hourDetails.center_address}} </text>  </h3>
                       <!-- <InputFormCenterProfessional v-on:selectedCenterCode="selectedCenterCode" :session_params="session_params" > </InputFormCenterProfessional> 
                      -->
                       
                      <div class="d-flex flex-row " >
-                        <div class="h2 p-2">En Internet</div>
+                        <div class="h4 p-2">Disponible Internet ? </div>
 
-                        <div class="m-3" v-on:click="form_public = !form_public">  
-                            <i v-if="form_public" class="display-1 bi bi-wifi"> </i>
-                            <i v-else  class="display-3 bi bi-wifi-off"> </i>
+                        <div class="" v-on:click="form_public = !form_public">  
+
+                            <i v-if="form_public" class="display-1 bi bi-wifi text-success"><text class="h5">Si</text> </i>
+                            <i v-else  class="display-3 bi bi-wifi-off"> <text class="h5">No</text> </i>
                         </div>
                      </div>
-       
-                      <button type="button" @click="updateApp(hourDetails);" data-bs-dismiss="modal" class="btn btn-primary m-2">GUARDAR CAMBIOS</button>
-                      <button type="button" @click="cancelHour(hourDetails);" data-bs-dismiss="modal" class="btn btn-danger m-2">CANCELAR ESTA HORA</button>
+                      <button type="button" @click="updateApp(hourDetails);" data-bs-dismiss="modal" class="btn btn-secondary m-2"><i class="bi bi-save"></i> Guardar Cambios</button>
+<hr/>
+                      <button type="button" @click="reserveHour(hourDetails);" data-bs-dismiss="modal" class="btn btn-primary m-2"> <i class="bi bi-person-square"></i> Reservar para un Paciente</button>
+                      
+                      <button type="button" @click="cancelHour(hourDetails);" data-bs-dismiss="modal" class="btn btn-primary m-2"><i class="bi bi-x-square"></i> Cancelar Hora</button>
 
                       </form>			
         
@@ -178,7 +179,7 @@ export default {
     },
    	
    props: ['daterequired','hourDetails', 'session_params' ],
-   emits: ['updateAppList'] , 
+   emits: ['updateAppList','showReserveModal'] , 
       
    	mounted () {
            this.auxHourCreate = ref(this.hourCreate) ;
@@ -187,24 +188,35 @@ export default {
     },
 
 	methods :{
-//GET CENTERS      
+     
+    reserveHour(hour)
+    {
+    console.log("Launch Reserve Modal : ");
+    this.showModalAppointmentDetais=false ;
+    this.$emit('showReserveModal',hour);
+    },
 
-    /*
-        async getCenters() {
-			const json = { 
-			   professional_id : this.session_params.professional_id  ,			   
-   			   	      };
-			console.log ("getCenters REQUEST :"+ JSON.stringify(json)  );
-			let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_get_centers",json);
-			console.log ("getCenters RESPONSE :"+JSON.stringify(response_json.data.rows)) ;
-			this.centers = response_json.data.rows;
-            //this.prevCenterName="noset";
-			},
+    async cancelHour(hour)
+    {
+    console.log("cancel appointment id: "+hour.app_id);
+        var r =confirm("¿Esta seguro que desea CANCELAR esta Hora? Si esta hora desaparecera de su agenda y pacientes no podran agendada en esta hora");
+	      if (r == true) {
 
-        selectedSpecialtyCode(value){
-            console.log("selecte Specialty Code "+value);
-        },
-    */
+        const json_input = {  
+              //only variable
+              appointment_id : hour.app_id,
+                        }
+                      
+        console.log ("Professional Cancel Appoitnment :"+ JSON.stringify(json_input)  );
+        let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/cancel_hour",json_input);
+        this.$emit('updateAppList');
+        this.showModalAppointmentDetais= false ;
+
+
+        }
+
+    },
+
     async updateApp(hour)
     {
     console.log("Update App : ");
@@ -236,51 +248,8 @@ export default {
         */
         this.$emit('updateAppList');
         this.showModalAppointmentDetais = false ;
-    }
+      }
     },
-/*
-    selectedSpecialtyCode(value)
-    {
-    console.log("speciality selected code: "+value);
-    this.form_specialty_code = value ;
-    },
-
-    selectedCenterCode(value)
-    {
-    console.log("Center selected code: "+value);
-    this.form_center_code = value ;
-    },
-
-	  async createHours() {
-    	var r =confirm("Se procedera a crear esta hora en su agenda");
-					  if (r == true) {
-
-        const json = { 
-              form_center_id  : this.form_center_code ,
-              form_professional_id : this.session_params.professional_id  ,
-             //hours 
-              form_date : this.daterequired ,
-              form_start_time : this.form_start_time , //cambio
-              form_appointment_duration : this.form_app_duration , 
-              form_specialty_code : this.form_specialty_code,
-              form_public : 1 ,
-                        };
-                  
-        console.log ("createHours REQUEST :"+ JSON.stringify(json)  );
-        let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_create_appointment",json);
-        console.log ("createHours RESPONSE:"+JSON.stringify(response_json.data.rows)) ;
-        //location.reload();
-        //this.agendas = response_json.data.rows;
-        //location.reload();
-        this.$emit('updateAppList');
-        this.showModalCreateApp = false ;
-            }
-
-
-        }
-*/
-
-
 
     },
     watch : {
