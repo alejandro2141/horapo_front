53cfd0722cccc6053a6a23a34a904af3008d9e0e
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-10-25 18:41:44 -0400

    Update InputFormSpecialty.vue

--- src/components/publicSearch/InputFormSpecialty.vue
+++ src/components/publicSearch/InputFormSpecialty.vue
@@ -1,10 +1,9 @@
 <script setup>
 import { ref } from 'vue'
-import axios from 'axios';
+import axios from 'axios'
+import { BKND_CONFIG } from '../../../config123.js' 
 
-defineProps({
-  msg: String
-})
+
 
 const count = ref(0)
 </script>
@@ -12,8 +11,6 @@ const count = ref(0)
 <template>
         <div class="" >
             <div class="row" style="--bs-gutter-x: 0rem ; margin-right: 0rem ; margin-left: 0rem ">
-                
-               
 
                 <div class="col" style="position: relative;" >
 
@@ -26,10 +23,18 @@ const count = ref(0)
                     </div>
                     
                      <div  style="position: absolute; z-index: 9; top : 1px ; right : 3px " class="mb-2  rounded" > 
-                        <i class="display-2 m-1  bi bi-x  text-muted "  @click="form_specialty = null; display_error = false ; $emit('selectedSpecialtyCode', null); " ></i>
+                        <i class="display-2 m-1  bi bi-x  text-muted "  @click="form_specialty = null; display_error = false ; $emit('selectedSpecialtyCode', null) " ></i>
                     </div>
                     
-                </div>    
+                </div> 
+                
+                <div >
+                     <div class="h3 m-3 text-primary " v-for="specialty in specialty_filtered" :key="specialty.id" > 
+                        <div @click="this.form_specialty = specialty.name ; $emit('selectedSpecialtyCode', specialty.id); this.clearfiltered = true ; ">
+                             <i class="display-6   bi bi-search  text-muted" ></i> {{ specialty.name }} 
+                        </div>
+                     </div>
+                </div>   
                
             </div>
         </div>
@@ -48,92 +53,46 @@ export default {
         return {
             form_specialty  : null,
             specialty_list : [],
-            cursor_position : 1, 
+            specialty_filtered : [],
             display_error : false ,
-            ready_input : false, 
-            position_cpy: null,
+            ready_input : false , 
+            clearfiltered : false ,  
         }
     },  
-    props: ['noparams','position'], 
+    props: [], 
+    emits: ["selectedSpecialtyCode"],
 
- mounted () {
-             this.daterequired = new Date().toISOString().split('T')[0] ;       
+beforeCreate() {},
+created() {},
+beforeMount() {},
+mounted() {   
+        this.getSpecialtyList() ;    
+        this.daterequired = new Date().toISOString().split('T')[0] ;  
         },
-
- beforeUpdate ()  {    
-        this.getSpecialtyList();
-        this.position_cpy = this.position ; 
-        },
-
+beforeUpdate() {},
+activated() {},
+ 
 watch: {
     //WATCHER PREDICTOR SPECIALTY
     form_specialty(value, oldValue) {
-        if (value !=null )
+        if (value !=null && value.length >= 0 && !this.clearfiltered )
         {
-
-               //i get text behind cursor: 
-                let textToSearch= this.form_specialty.substring(0, document.getElementById('form_specialty').selectionStart ) ;
-                //capitalize Text To Search
-                //textToSearch = textToSearch.charAt(0).toUpperCase() + textToSearch.slice(1).toLowerCase();
-
-                var separateWord = textToSearch.toLowerCase().split(' ');
-                for (var i = 0; i < separateWord.length; i++) {
-                    if (separateWord[i].length > 1 )
-                    { 
-                        separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
-                        separateWord[i].substring(1);
-                    }
+            console.log("Text to search "+value);
+            let tempfiltered = this.specialty_list.filter(item => item.name.substring(0,value.length)  ===  value );
+            if (tempfiltered.length >= 1 )
+                {
+                this.specialty_filtered = this.specialty_list.filter(item => item.name.substring(0,value.length)  ===  value );
                 }
-                textToSearch=separateWord.join(' ');
-                console.log ("Start CursorPosition:"+document.getElementById('form_specialty').selectionStart+" textToSearch("+textToSearch+")   Value="+value+"   oldValue="+oldValue);
-                if (oldValue === null) { oldValue = " " ;}
-                let result = this.specialty_list.filter(item => item.name.substring(0,textToSearch.length)  ===  textToSearch );
-                //result contain an array with match 
-                if (result.length >0 && textToSearch.length > 0 )
-                {console.log("MATCH!!  ("+textToSearch+")  Match to:"+result[0].name);
-                    //check if both are diferent
-                    if (textToSearch.localeCompare(result[0].name) != 0 )
-                    { console.log("Text to search ("+textToSearch+") not equal to: "+result[0].name+" Comparison result="+textToSearch.localeCompare(result[0].name) );
-                    this.ready_input = false;
-                            //si nuevo valor es mayor que el antiguo
-                            if (value.length > oldValue.length )
-                            {
-                            console.log("replacing FORM value to :"+result[0].name);
-                            //before replace save cursor
-                            this.cursor_position=textToSearch.length
-                            this.form_specialty=result[0].name; 
-                           
-                             }
-                            else{
-                                console.log("No reemplaza, OLD > NEW ");
-                                this.ready_input = false;
-                                this.$emit("selectedSpecialtyCode", null); 
-                                
-                            }
-                    }
-                    else
-                    {
-                        console.log("BUTE are the same, skip replacing. Comparison result="+textToSearch.localeCompare(result[0].name));
-                        console.log("Moving Cursor to cursor_position stored before replace:"+this.cursor_position);
-                        document.getElementById('form_specialty').selectionStart = this.cursor_position ;
-                        document.getElementById('form_specialty').selectionEnd = this.cursor_position ;
-                    }
-                    this.$emit("selectedSpecialtyCode",result[0].id); 
-                    this.display_error = false ; 
-                    this.ready_input = true ; 
-                }
-                else {console.log("No Match!! Do Nothing testToSearch("+textToSearch+") " );
-                this.form_specialty=textToSearch ;
-                this.$emit("selectedSpecialtyCode", null); 
-                this.display_error=true ;
-                this.ready_input = false;
-                //We should deleete all letters ahead. 
-                }
-        
+            else
+             {
+               
+             }         
         }
         else
         {
-            this.ready_input = false;
+           // this.ready_input = false;
+           this.specialty_filtered = null
+           this.clearfiltered = false ; 
         }        
 
     },
@@ -156,5 +115,4 @@ watch: {
         },
 
 }
-
 </script>
