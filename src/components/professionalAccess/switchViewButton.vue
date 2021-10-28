c2fb702819ed1791dbc14267086af6e8999e495a
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-10-28 19:24:51 -0400

    Create switchViewButton.vue

---
+++ src/components/professionalAccess/switchViewButton.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+
+
+</script>
+
+<template>
+<div class="position-absolute border-1  d-flex flex-row-reverse " >
+    <div class="border text-primary">
+     <i @click="switchView()" class=" text-center display-2 bi bi-geo"></i> 
+     </div>
+</div>
+</template>
+
+<style scoped>
+</style>
+
+
+<script>
+
+export default {
+   data : function() {
+        return {
+       
+        }   
+    },
+   	
+    props: [],
+    emits: ['switchView'] ,
+
+	created () {
+
+	},
+	methods :{
+        switchView()
+        {
+            console.log("switch view to");
+            this.$emit('switchView');
+        }
+    }
+}
+</script>
+
