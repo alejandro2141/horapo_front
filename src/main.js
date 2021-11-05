3a493599dc6ed917c9b409d151d1c6cc5a9df20d
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-11-04 20:44:28 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { BKND_CONFIG } from '../config123.js'
+import axios from 'axios'
+//import { BKND_CONFIG, GLOBAL_COMUNAS } from '../config123.js'
+
 
 const app = createApp(App)
 app.mount('#app')
-app.config.globalProperties.BKND_CONFIG = BKND_CONFIG
 
-console.log(app.config)
+//app.config.globalProperties.BKND_CONFIG = BKND_CONFIG
+/*
+app.config.globalProperties.GLOBAL_REGIONS = GLOBAL_REGIONS
+*/
+//app.config.globalProperties.GLOBAL_COMUNAS = GLOBAL_COMUNAS
+
+
+console.log(app.config) ;
+
+//LOAD GLOBAL VARIABLES HERE in order to use later and avoid multiple calls to backend. 
+/*
+        let response_comunas = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_comuna_list");
+        app.config.globalProperties.GLOBAL_COMUNA = response_comunas.data.rows;	
+*/      
