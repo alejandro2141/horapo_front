f4745eef525da892ba80b2d1f5fef305e4bb2e41
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-11-04 20:45:15 -0400

    Update tabCenter.vue

--- src/components/professionalAccess/tabCenter.vue
+++ src/components/professionalAccess/tabCenter.vue
@@ -3,39 +3,48 @@ import { ref } from 'vue'
 import axios from 'axios';
 import ModalCreateCenter from './modalCreateCenter.vue'
 import ModalViewCenterDetails from './modalViewCenterDetails.vue'
-
-
 </script>
 
 <template>
 
-     <ModalViewCenterDetails  :session_params='session_params' :activatorViewCenterDetails='activatorViewCenterDetails' :centerToShowDetails='centerToShowDetails' > </ModalViewCenterDetails> 
-                    
-     <ModalCreateCenter v-on:updateCenterList='updateCenterList' :session_params='session_params' :activatorCreateNewCenter='activatorCreateNewCenter' :centerToShowDetails='centerToShowDetails' >  </ModalCreateCenter>
+     <ModalViewCenterDetails  :session_params='session_params' :activatorViewCenterDetails='activatorViewCenterDetails' :centerToShowDetails='centerToShowDetails' v-on:updateCenterList="updateCenterList"> </ModalViewCenterDetails>                    
+     <ModalCreateCenter  :session_params='session_params' :activatorCreateNewCenter='activatorCreateNewCenter' :centerToShowDetails='centerToShowDetails'  v-on:updateCenterList="updateCenterList" >  </ModalCreateCenter>
       
 <div  class="mx-auto " style="width: 95%;" >
         <text class="h4">Centros de Atencion en su agenda </text> 
 	
-        <div id="search_result" >
+        <div  id="search_result" v-if='centers.length > 0'  >
             <div v-for="center in centers"  :key='center.id' >
-    
-                <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title"><i class="bi bi-building display-2"></i> {{center.name}}</h5>
-                    <p class="card-text">{{center.address}}</p>
-                    <p class="text-end" > <text @click="showCenter(center)" class="text-primary">Ver</text>  </p>
-                </div>
+                <div class="card m-3 border border-secondary" style="width: 18rem; ">
+                    <div class="card-body">
+                        <h5 class="card-title"><i class="bi bi-building display-2"></i> {{center.name}}</h5>
+                        <p class="card-text">{{center.address}} </p>
+                        <b>{{ this.comuna_list.find(o => o.id === center.comuna).name }} </b>
+                        <p class="text-end" > <text @click="showCenter(center)" class="text-primary">Ver</text>  </p>
+                    </div>
                 </div>   
             </div>
+        
         </div>
 
-        <div class="d-flex justify-content-between"> 
-            <button @click="addNewCenter()" type="button" class="btn btn-primary m-3"> <i class="bi bi-plus-square h5"></i> Agregar un nuevo centro </button>
+        <div  v-else class=" m-2 p-2">
+           <p> Algo de informacion antes de continuar: <br>
+            -Puede crear mas de un Centro<br>
+            -La direccion del centro es donde el paciente debe asistir <br>
+            -Siempre puede ver la lista de Centros en el link  <text class="text-primary">"Ver Centros"</text> <br>
+           </p>
+         <p class="">Para crear su primer centro, por favor click en el link siguiente </p>
+           
+        
         </div>
 
-       <!--
-    <ModalCreateNewCenter  :session_params="session_params" v-on:updateCenterList="updateCenterList" > </ModalCreateNewCenter>
-  -->  
+        <div class="text-center p-3 m-3"> 
+            <text @click="addNewCenter()"  class="text-primary m-3"> <i class="bi bi-plus-square h5"></i> Agregar un nuevo centro </text>
+        </div>
+
+        <!--
+        <ModalCreateNewCenter  :session_params="session_params" v-on:updateCenterList="updateCenterList" > </ModalCreateNewCenter>
+        --> 
 </div>       
 
 </template>
@@ -69,25 +78,18 @@ data: function () {
          },
  
     methods: {
-        updateCenterList(){
-            console.log('Update Center List in Tab Center');
-        },
-
         addNewCenter()
         {
             console.log("Add Center");
             //this.centerToShowDetails = center ; 
             this.activatorCreateNewCenter = Math.random(); 
         },
-
         showCenter(center){
             console.log("show center details");
             this.centerToShowDetails = center ;
-            this.activatorViewCenterDetails = Math.random();
-         
+            this.activatorViewCenterDetails = Math.random();     
         },
-
-            async  getComunaList() {
+        async  getComunaList() {
                     console.log ("GET COMUNA LIST METHOD"); 
                         const json = { 
                         nodata : 'nodata' ,
@@ -97,43 +99,25 @@ data: function () {
                         console.log ("getComunaList RESPONSE:"+JSON.stringify(response_json.data.rows)) ;
                         this.comuna_list = response_json.data.rows;		
                         console.log("getComunaList list: "+JSON.stringify(this.comuna_list) );
-                    },
-    
-                //CREATE New Center
-            async getCenters() {
+                    },   
+        //CREATE New Center
+        async getCenters() {
                         const json = { 
                         professional_id : this.session_params.professional_id ,			   
                                         };
                         console.log ("GET CENTERS REQUEST :"+ JSON.stringify(json)  );
                         let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_get_centers",json);
                         this.centers = response_json.data.rows;
-                        console.log ("RESPONSE CENTERS:"+JSON.stringify(this.centers)) ;
-                       
-                    },	
-            //DELETE Center
-            async deleteCenter(id) {
-                        confirm("Esta seguro que desea continuar con la eliminacion de este centro ? ");
-                        const json = { 
-                            center_id : id ,	
-                            professional_id : this.session_params.professional_id ,			   
-                                };
-                        console.log ("deleteCenter REQUEST :"+ JSON.stringify(json)  );
-                        let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_delete_center",json);
-                        console.log ("RESPONSE:"+JSON.stringify(response_json.data)) ;
-                        //location.href = "professional_show_centers.html?"+this.getUrlParam()  ;
-                        this.getCenters();
-                        //this.centers = response_json.data.rows;
-                    },		
+                        console.log ("RESPONSE CENTERS:"+JSON.stringify(this.centers)) ;                       
+                    },			
+        //UPDATE CENTER LIST
+        updateCenterList()
+            {
+                console.log("UpdateCenter List");
+                this.getCenters();
+            },
 
-                //UPDATE CENTER LIST
-                updateCenterList()
-                {
-                    console.log("UpdateCenter List");
-                    this.getCenters();
-                },
-
-       
-		    },
+		},
   
 
 }
