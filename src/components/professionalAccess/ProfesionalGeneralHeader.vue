7ec4d26cf609b44c4a8ac877b5f378f1f0b56d4d
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-11-04 20:45:28 -0400

    Create ProfesionalGeneralHeader.vue

---
+++ src/components/professionalAccess/ProfesionalGeneralHeader.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+
+    <div class="text-left m-2 pb-0 text-dark bg-light">
+		<div class="display-6 "  > 
+			<a HREF="/index.html" class="text-primary text-decoration-none"> 
+			<i class="bi bi-arrow-left-square"></i> 
+			Salir
+			</a> 
+		</div>
+		<div class="text-muted">	
+		</div>
+		<hr>
+	</div>
+
+</template>
+
+<style scoped>
+
+</style>
+
