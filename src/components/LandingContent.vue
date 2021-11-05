0bb38543f2f2dc0f4cbb434d6074bcd09a0ea11c
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-11-04 20:46:20 -0400

    Update LandingContent.vue

--- src/components/LandingContent.vue
+++ src/components/LandingContent.vue
@@ -13,7 +13,7 @@ const count = ref(0)
     <div class="mt-5 mb-5 text-center pb-5 " >
 
 		<a  HREF='./nested/publicSearch.html'  type="button" class="btn btn-primary text-white btn-lg m-2">&nbsp;&nbsp;&nbsp; Buscar una Hora &nbsp;&nbsp;</a>
-		<a HREF="./nested/getAppointments.html"  type="button" class="btn btn-success btn-lg text-white m-2">Consultar mis Horas</a>
+		<a HREF="./nested/getAppointments.html"  type="button" class="btn btn-success btn-lg text-white m-2">Recordar mis Horas</a>
 
 	</div>
 
