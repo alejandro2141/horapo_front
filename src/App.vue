9edef7f8aebec2d66b4bdd1e006bd38edc2eb6be
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-11-04 20:46:24 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,16 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
 import LandingPage from './components/LandingPage.vue'
+import axios from 'axios'
+import { BKND_CONFIG, GLOBAL_COMUNAS, GLOBAL_SPECIALTIES } from '../config123.js'
 
 
 </script>
 
 <template>
-<LandingPage nameTitle="123Hora"></LandingPage>
 
+    <LandingPage nameTitle="123Hora"></LandingPage>
+   
 </template>
 
 
@@ -19,3 +22,8 @@ body {
 }
 
 </style>
+
+<script>
+
+
+</script>
