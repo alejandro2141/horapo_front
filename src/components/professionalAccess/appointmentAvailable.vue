eaedc330fd103861052064f2957f36cfb104c5e1
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-11-08 19:06:54 -0400

    Update appointmentAvailable.vue

--- src/components/professionalAccess/appointmentAvailable.vue
+++ src/components/professionalAccess/appointmentAvailable.vue
@@ -8,55 +8,51 @@ import axios from 'axios';
 <template>
      
 <div   class="bg-light mb-2" data-bs-toggle="modal" :data-bs-target="'#modal_reserve_'+index" >
-                        <div id="app" class="d-flex  "  >	
+                        <div id="app" class="d-flex justify-content-start"  >	
                             <!--
                             <div class="m-0"  :style="{'background-color' : '#'+appointment.center_color }"  >
                                 &nbsp;&nbsp;&nbsp;
                             </div>
                             -->
                             <div class="">
-                                <div class="display-3 "  style="padding-right: 3rem;">
-                                {{appointment.start_time.substring(0, 5) }}							
-                                </div>
-                                  <div class="">
-                                <i class="bi bi-clock-history"></i> {{appointment.duration }} Min
-                                </div>
 
+                                <div class="display-4 "  style="margin-left: 0.2em;">
+                                    {{appointment.start_time.substring(0, 5) }}	
+                                </div>
+                                <div class="text-muted text-center" style=" font-size: 1.0em;" >
+                                     {{appointment.duration }}min 					
+                                </div>
                             </div>
                             
-                            <div class=" w-100 " style="padding-left: 15px;" >
-                                <div  >
-                                    <div v-if="appointment.app_status == 1 " class="text-danger" >
+                            <div class="" style="margin-left: 0.5em;" >
+                               
+                                <div v-if="appointment.app_status == 1 " class="text-danger" >
                                         Hora Cancelada 
-                                    </div>
-                                    <div v-else class="text-success"> 
-                                        DISPONIBLE <i v-if="appointment.available_public_search" class="bi bi-wifi text-success h3"></i> 
-                                    </div> 
-
-                                      
                                 </div>
+                                <div v-else class="text-success"> 
+                                        DISPONIBLE <i v-if="appointment.available_public_search" style="margin-left: 1.0em;" class="bi bi-wifi text-success ">On Line</i> 
+                                </div> 
 
-                                <div class="d-flex justify-content-between" >
-                                        <div>
-                                        {{appointment.specialty_name }}
-                                        
-                                        </div>
-
-                                      
-                                         
-                                       
-                                        
+                                <div class="">
+                                       {{appointment.specialty_name }}<br>
+  
                                 </div>
                                         
                               
                                
                                     
                             </div>
+
+            	  
                             
                         </div>
-                         <div>
-                           <i class="bi bi-geo-alt"></i>    {{appointment.center_address }}
-                        </div>	
+                        <text style=" font-size: 1.0em;" >
+                          <i class="bi bi-geo-alt"  ></i>    {{appointment.center_address }}       
+                        </text>
+
+
+                        
+
     </div>
 
 </template>
