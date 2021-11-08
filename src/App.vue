1a3fa497d26dd0949014f0093fafa7465f221197
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-11-08 19:07:04 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,14 @@
 
 import LandingPage from './components/LandingPage.vue'
 import axios from 'axios'
-import { BKND_CONFIG, GLOBAL_COMUNAS, GLOBAL_SPECIALTIES } from '../config123.js'
+import { BKND_CONFIG } from '../config123.js'
 
 
 </script>
 
 <template>
 
-    <LandingPage nameTitle="123Hora"></LandingPage>
+    <LandingPage nameTitle="123Hora"   ></LandingPage>
    
 </template>
 
@@ -25,5 +25,47 @@ body {
 
 <script>
 
+export default {
+ data : function() {
+        return {
+            global_specialties : [],
+            global_comunas : [],  
+        }
+    },  
+    props: [], 
+    emits: [],
+
+created() {
+        console.log("APP Fill Global variables");
+        this.loadGlobalSpecialties();
+        this.loadGlobalComunas();
+},
+mounted() {},
+
+watch: {
+    },//end watch
+
+methods: {
+        async loadGlobalSpecialties() {
+                console.log ("APP GET SPECIALTY LIST METHOD"); 
+				let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_specialty_list");
+                this.global_specialties = response_json.data.rows;
+                console.log("APP Specialty list: "+JSON.stringify(this.global_specialties) );
+
+            },
+        async loadGlobalComunas() {
+                 console.log ("APP GET COMUNA LIST METHOD"); 
+				let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_comuna_list");
+                this.global_comunas = response_json.data.rows;
+                console.log("APP Comuna list: "+JSON.stringify(this.global_comunas) );
+
+            },
+
+
+
+
+        },
+
+}
 
 </script>
