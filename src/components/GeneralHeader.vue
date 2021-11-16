8384200476288484ddb0d6518db1e1c673d07e65
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-11-16 19:20:20 -0400

    Update GeneralHeader.vue

--- src/components/GeneralHeader.vue
+++ src/components/GeneralHeader.vue
@@ -10,9 +10,9 @@ const count = ref(0)
 
 <template>
 
-    <div class="text-center pt-2 pb-0 text-dark bg-light ">
+    <div class="text-center pt-2 pb-0 " style="background-color :#ffffff "  >
 		<div class="display-4 "  > 
-			<a HREF="/index.html" class="text-dark text-decoration-none"> 
+			<a HREF="/index.html" class="text-decoration-none" style="color :#2e5668"> 
 			<i class="bi bi-arrow-left-square"></i> 
 			123Hora  
 			</a> 
