7a4fa22ebdfe122ab684f5ce0a33bb326b2120dc
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-11-16 19:20:10 -0400

    Update modalPublicReserveConfirmation.vue

--- src/components/publicSearch/modalPublicReserveConfirmation.vue
+++ src/components/publicSearch/modalPublicReserveConfirmation.vue
@@ -11,19 +11,17 @@ import axios from 'axios'
   <div>
   	<teleport to="body"   >
 
-		<div v-if="showModalConfirmation" class="modal bg-secondary"    >
+		<div v-if="showModalConfirmation" class="modal bg-secondary scroll"    >
 		    
 			<transition name="modal">
 			<div class="modal-mask "  >
 			<div class="modal-wrapper ">
-			<div class="modal-container  m-1 p-0 modal-background">
+			<div class="modal-container  m-1 p-0 modal-background " style="border: 0px solid rgb(168, 168, 168); border-radius: 20px;">
               
                 <div class="modal-body " > 
 
- 
-
                   <div class="d-flex flex-row justify-content-end  m-1">
-                      <div class="h3 " style="margin-right: 1em;" >  {{ app.specialty_name}}  </div>
+                      <div class="display-4 " style="margin-right: 1em;  color:#1f9d94 "  >  {{ app.specialty_name}}  </div>
                       <div class="" style="margin-right: 1em;" > </div>
                       <div class=""><i class="display-1 bi bi-x-lg ml-0"  v-on:click="showModalConfirmation = false ; this.updateSearchResult() " aria-label="Close"></i> </div>
                   </div>
@@ -32,18 +30,21 @@ import axios from 'axios'
                   <i class="bi bi-hand-thumbs-up"> Reserva Exitosa </i> 
               </div>
 
-							<p> 
-								{{appConfirmed.date.substring(0, 10) }}  a las : 
-								{{appConfirmed.start_time.substring(0, 5) }} Hrs 
-								<br>
-								Profesional: {{app.name }}
-							</p>
+                    <div class=" w-100 display-6 m-0 p-0" style="color:#2e5668">	
+                   		 {{ transform_date( appConfirmed.date.substring(0, 10) ) }} 
+                        {{appConfirmed.start_time.substring(0, 5) }} hrs   
+                    </div> 
+
+							<div class="h3 mb-2" style="color:#2e5668" > 
+						  Con: {{app.name }}
+							</div>
 							<p>
-							Dirección:<br> 
-							<text class="">
-							{{app.center_name }} <br> 
-							Direccion: {{app.center_address }}<br> </text> 
-							</p>
+							<text class="" style="color:#2e5668" >
+							"{{app.center_name }}" <br> 
+							Direccion: {{app.center_address }}
+              </text> 
+							
+              </p>
 
 								<p>
 								Informacion de su cita ha sido enviada a su correo <b> {{appConfirmed.patient_email}} </b> 
@@ -56,7 +57,8 @@ import axios from 'axios'
 
                 <button type="button" @click="updateSearchResult();" class="btn btn-primary" data-bs-dismiss="modal"   >Regresar a la Busqueda</button>
                           
-
+	                      <div class="" style="height : 500px"> 
+                				</div>
                 </div>
         </div> 
         </div> 		
@@ -71,34 +73,31 @@ import axios from 'axios'
 
 <style scoped>
 
-
 .modal {
-  position: absolute;
-  display: flex;
+	position: fixed; 
+   display: flex; 
+
 }
 
-.modal div {
-  display: flex;
-  flex-direction: column;
-}
-/*
-.modal-background {
-    background-color:#DAEFF3
-        }
-*/
-/************************* */
-
-
+div.scroll {
+       			margin:4px, 4px;
+                padding:4px;
+                width: 100%; 
+                /* height: 190%;*/
+                overflow-x: auto;
+                overflow-y: auto;
+                text-align:justify;
+      }
 
 .modal-mask {
-  position: fixed;
+  /*position: fixed;*/
   z-index: 9998;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
-  display: table;
+ /* display: table;*/
 }
 
 .modal-wrapper {
@@ -108,8 +107,8 @@ import axios from 'axios'
 
 .modal-container {
  /*width: 100px;*/
-  margin: 2px auto;
-  padding: 20px 30px;
+  /*margin: 1px auto;*/
+  /*padding: 20px 30px;*/
   background-color: #fff;
   border-radius: 2px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
@@ -122,22 +121,13 @@ import axios from 'axios'
 }
 
 .modal-body {
-  margin: 20px 0;
+  margin: 0px 0;
 }
 
 .modal-default-button {
   display: block;
   margin-top: 1rem;
 }
-
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
 
 .modal-enter-active,
 .modal-leave-active {
@@ -179,7 +169,13 @@ computed: {
       	},
 
 	methods: {
-	          updateSearchResult()
+
+            transform_date(date)
+            {
+              let temp = date.split("-") ;
+              return (" "+temp[2]+"/"+temp[1]+"/"+temp[0])
+            },
+            updateSearchResult()
             {
                 console.log (" update search Result. In Modal Public Reserve Confirmation ");
                 //this.appointment_list=null ;
