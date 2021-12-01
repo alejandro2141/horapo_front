12cf3ae25471f582c4ddfde70e9c06a0e13927db
unknown <74009893+alejandro2141@users.noreply.github.com>
2021-12-01 19:16:47 -0400

    Create loadProgress.vue

---
+++ src/components/professionalAccess/loadProgress.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+
+
+</script>
+
+<template>
+<div class="d-flex float-end bg-light " >
+    <div class=" text-primary ">
+     <text v-if="showLoader" class="" >LOADING ...</text> 
+     </div>
+ </div>
+</template>
+
+<style scoped>
+</style>
+
+
+<script>
+
+export default {
+   data : function() {
+        return {
+            // : 'Centros',
+       
+        }   
+    },
+   	
+    props: ['showLoader'],
+    emits: ['timeout'] ,
+
+	created () {
+	},
+	methods :{
+    }
+}
+</script>
+
