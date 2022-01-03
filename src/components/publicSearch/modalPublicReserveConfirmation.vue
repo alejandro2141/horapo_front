9322138ebb84fded059b9b28c804f5a90c2629cf
unknown <74009893+alejandro2141@users.noreply.github.com>
2022-01-03 18:32:04 -0400

    Update modalPublicReserveConfirmation.vue

--- src/components/publicSearch/modalPublicReserveConfirmation.vue
+++ src/components/publicSearch/modalPublicReserveConfirmation.vue
@@ -21,39 +21,46 @@ import axios from 'axios'
                 <div class="modal-body " > 
 
                   <div class="d-flex flex-row justify-content-end  m-1">
-                      <div class="display-4 " style="margin-right: 1em;  color:#1f9d94 "  >  {{ app.specialty_name}}  </div>
+                      <div class="display-4 " style="margin-right: 1em;  color:#1f9d94 "  >    Reserva Exitosa   </div>
                       <div class="" style="margin-right: 1em;" > </div>
                       <div class=""><i class="display-1 bi bi-x-lg ml-0"  v-on:click="showModalConfirmation = false ; this.updateSearchResult() " aria-label="Close"></i> </div>
                   </div>
 
-              <div class="h1 text-success m-3 ">              
-                   Reserva Exitosa  
-              </div>
-                
-                    <div class=" w-100 display-6 m-0 p-0" style="color:#2e5668">	
-               {{ transform_date( appConfirmed.date.substring(0, 10) ) }}   {{appConfirmed.start_time.substring(0, 5) }} hrs
+            
 
-
-                    </div> 
-
-							<div class="h3 mb-2" style="color:#2e5668" > 
-						  Con: {{app.name }}
+            	<div class=" d-flex justify-content-start border border-2" style="border: 0px solid rgb(168, 168, 168); border-radius: 10px ">
+						      <div class="m-0 p-2  bg-primary text-white" style="border: 0px solid rgb(168, 168, 168); border-radius: 10px 0px 0px 10px;" > <text class="display-5"> {{getShortMonthName(appConfirmed.date.substring(5, 7) )}}</text><br><text class="display-1 d-flex align-items-center justify-content-center"> {{appConfirmed.date.substring(8, 10) }} </text>  </div>
+					        <div  class=" display-3 p-3" style="color:#1f9d94 ;" >{{ showSpecialtyName(appConfirmed) }} </div> 
+					    </div>
+        
+              <div class="h3" >
+						        <i class="bi bi-smartwatch h"></i>  {{ transform_date( appConfirmed.date.substring(0, 10) ) }}   {{appConfirmed.start_time.substring(0, 5) }}hrs  
+				    	</div>
+				<br> 
+				      <div class="">	
+						        <i class="bi bi-person m-1"></i>Con :  {{app.name }} 
+				    	</div>
+        <br>
+				<!-- IF APP IN CENTER -->
+        <div v-if="appConfirmed.app_type_center" class="" >
+					<div  style="color:#2e5668"> <i class="bi bi-building  m-1"></i> <b>Cita en Centro</b><br> {{appConfirmed.center_name }}</div>
+					<div class="">Direccion:  {{app.center_address }}</div>
+				</div>  
+        <!-- IF HOME VISIT -->
+        <div v-if="app.app_type_home" class="" >
+					<div><i class="bi bi-house m-1 "></i> <b>Cita a domicilio</b>  
+          </div>
+							<div class="m-2">
+                  Incluir direccion ingresada
 							</div>
-							<p>
-							<text class="" style="color:#2e5668" >
-							"{{app.center_name }}" <br> 
-							Direccion: {{app.center_address }}
-              </text> 
-							
-              </p>
+				</div>
 
-								<p>
-								Informacion de su cita ha sido enviada a su correo <b> {{appConfirmed.patient_email}} </b> 
-								</p>
-								<p style="border-style: dotted;" class="p-2 bg-warning text-dark">
+								
+							<!--	<p style="border-style: dotted;" class="p-2 bg-info text-white"> -->
+                <div class="p-2 border border-2">
 								IMPORTANTE: <br>
-								48 Hrs antes de su cita, debe confirmar su asistencia en el enlace que fue enviado a su correo. 
-								</p>
+								Debe confirmar su asistencia 48 Hrs antes de su cita en el enlace que fue enviado a su correo {{appConfirmed.patient_email}} 
+								</div>
 							
 
                 <button type="button" @click="updateSearchResult();" class="btn btn-primary" data-bs-dismiss="modal"   >Regresar a la Busqueda</button>
@@ -170,17 +177,25 @@ computed: {
       	},
 
 	methods: {
+          showSpecialtyName(app)
+            {
+                if (this.searchParameters.specialty != null)
+                {return this.searchParameters.specialty.name }
+                else {
+                    return (app.specialty_name )
+                }
+            },
 
            	transform_date(date)
             {
               let temp = date.split("-") ;
-              return (""+temp[2]+" de "+this.getShortMonthName(temp[1])+" "+temp[0])
+              return (""+temp[2]+" "+this.getShortMonthName(temp[1])+" "+temp[0])
             },
              getShortMonthName(month)
             {
-              console.log("MONTH:"+month);
+              console.log("MONTH:"+parseInt(month));
               let months = ['nodata','Ene.','Feb.' ,'Marz.','Abr.','May.','Jun.','Jul.','Ago.','Sept.','Oct.','Nov.','Dic.' ]
-              return months[month];
+              return months[parseInt(month)];
 
             },
             updateSearchResult()
