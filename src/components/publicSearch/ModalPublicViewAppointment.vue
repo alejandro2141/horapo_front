2bdf844f8ce299f2cfd5cdc5178ae13ffa058e0e
unknown <74009893+alejandro2141@users.noreply.github.com>
2022-01-03 18:31:59 -0400

    Update ModalPublicViewAppointment.vue

--- src/components/publicSearch/ModalPublicViewAppointment.vue
+++ src/components/publicSearch/ModalPublicViewAppointment.vue
@@ -31,12 +31,37 @@ import loadProgress from './loadProgress.vue'
                       <div class=""><i class="display-1 text-primary bi bi-x-lg ml-0"  v-on:click="showModalPublicAppDetails = false" aria-label="Close"></i> </div>
                     </div>
 
-					
-
 					<div class=" d-flex justify-content-start border border-2" style="border: 0px solid rgb(168, 168, 168); border-radius: 10px ">
 						<div class="m-0 p-2  bg-primary text-white" style="border: 0px solid rgb(168, 168, 168); border-radius: 10px 0px 0px 10px;" > <text class="display-5"> {{getShortMonthName(app.date.substring(5, 7) )}}</text><br><text class="display-1 d-flex align-items-center justify-content-center"> {{app.date.substring(8, 10) }} </text>  </div>
 					    <div  class=" display-3 p-3" style="color:#1f9d94 ;" >{{ showSpecialtyName(app) }} </div> 
 					</div>
+
+
+				<div v-if="app.app_type_home" class="" >
+						<div class="h4" style=" color:#1f9d94 " ><i class="bi bi-house m-1 "></i> <b>Cita a domicilio</b> en comunas: </div>
+				
+							<div class="m-2">
+							<text v-if=" app.location1 != null " >  
+                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location1) }} 
+                            </text>
+                            <text v-if=" app.location2 != null " >  
+                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location2) }} 
+                            </text>
+                            <text v-if=" app.location3 != null " >  
+                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location3) }}
+                            </text>
+                            <text v-if=" app.location4 != null " >  
+                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location4) }} 
+                            </text>
+                            <text v-if=" app.location5 != null " >  
+                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location5) }} 
+                            </text>
+                            <text v-if=" app.location6 != null " >  
+                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location6) }} 
+                            </text>	
+							</div>
+				</div>
+
 
 			<div style="margin-top: 1em; margin-left: 1em;" class="h4">
 					<div class="" >
@@ -62,29 +87,7 @@ import loadProgress from './loadProgress.vue'
 				-->
 				</div>
 
-				<div v-if="app.app_type_home" class="" >
-					<div><i class="bi bi-house m-1 "></i> <b>Cita a domicilio</b> en Comunas: </div>
-							<div class="m-2">
-							<text v-if=" app.location1 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location1) }} 
-                            </text>
-                            <text v-if=" app.location2 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location2) }} 
-                            </text>
-                            <text v-if=" app.location3 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location3) }}
-                            </text>
-                            <text v-if=" app.location4 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location4) }} 
-                            </text>
-                            <text v-if=" app.location5 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location5) }} 
-                            </text>
-                            <text v-if=" app.location6 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ comuna_id2name(app.location6) }} 
-                            </text>	
-							</div>
-				</div>
+
 
 			</div>
 
@@ -266,9 +269,9 @@ computed: {
 
 		getShortMonthName(month)
 		{
-			console.log("MONTH:"+month);
+			console.log("MONTH:"+parseInt(month));
 			let months = ['nodata','Ene.','Feb.' ,'Marz.','Abr.','May.','Jun.','Jul.','Ago.','Sept.','Oct.','Nov.','Dic.' ]
-			return months[month];
+			return months[parseInt(month)];
 
 		},
 
