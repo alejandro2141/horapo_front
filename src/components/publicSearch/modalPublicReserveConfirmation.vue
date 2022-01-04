e58bb322caf888b982d262119bdfe97a8c26722f
unknown <74009893+alejandro2141@users.noreply.github.com>
2022-01-04 19:12:32 -0400

    Update modalPublicReserveConfirmation.vue

--- src/components/publicSearch/modalPublicReserveConfirmation.vue
+++ src/components/publicSearch/modalPublicReserveConfirmation.vue
@@ -23,14 +23,14 @@ import axios from 'axios'
                   <div class="d-flex flex-row justify-content-end  m-1">
                       <div class="display-4 " style="margin-right: 1em;  color:#1f9d94 "  >    Reserva Exitosa   </div>
                       <div class="" style="margin-right: 1em;" > </div>
-                      <div class=""><i class="display-1 bi bi-x-lg ml-0"  v-on:click="showModalConfirmation = false ; this.updateSearchResult() " aria-label="Close"></i> </div>
+                      <div class=""><i class="display-1 text-primary bi bi-x-lg ml-0"  v-on:click="showModalConfirmation = false ; this.updateSearchResult() " aria-label="Close"></i> </div>
                   </div>
 
             
 
             	<div class=" d-flex justify-content-start border border-2" style="border: 0px solid rgb(168, 168, 168); border-radius: 10px ">
 						      <div class="m-0 p-2  bg-primary text-white" style="border: 0px solid rgb(168, 168, 168); border-radius: 10px 0px 0px 10px;" > <text class="display-5"> {{getShortMonthName(appConfirmed.date.substring(5, 7) )}}</text><br><text class="display-1 d-flex align-items-center justify-content-center"> {{appConfirmed.date.substring(8, 10) }} </text>  </div>
-					        <div  class=" display-3 p-3" style="color:#1f9d94 ;" >{{ showSpecialtyName(appConfirmed) }} </div> 
+					        <div  class=" display-3 p-3" style="color:#1f9d94 ;" >{{ showSpecialtyName(app) }} </div> 
 					    </div>
         
               <div class="h3" >
@@ -55,19 +55,23 @@ import axios from 'axios'
 							</div>
 				</div>
 
-								
 							<!--	<p style="border-style: dotted;" class="p-2 bg-info text-white"> -->
                 <div class="p-2 border border-2">
 								IMPORTANTE: <br>
 								Debe confirmar su asistencia 48 Hrs antes de su cita en el enlace que fue enviado a su correo {{appConfirmed.patient_email}} 
 								</div>
 							
-
+              <br>
                 <button type="button" @click="updateSearchResult();" class="btn btn-primary" data-bs-dismiss="modal"   >Regresar a la Busqueda</button>
-                          
-	                      <div class="" style="height : 500px"> 
+              <br>
+              <br>
+                <text class="text-secondary" style="">#{{appConfirmed.id}}</text>
+           
+	                      <div class="" style="height : 200px"> 
                 				</div>
                 </div>
+                	<br>
+	             
         </div> 
         </div> 		
 		  </div> 
