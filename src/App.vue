8c37d847e6598164b99f63a7e353fa37ff829caa
unknown <74009893+alejandro2141@users.noreply.github.com>
2022-01-13 19:00:59 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -35,34 +35,35 @@ export default {
     props: [], 
     emits: [],
 
+
 created() {
-        console.log("APP Fill Global variables");
+    /*
+   console.log("APP Fill Global variables");
         this.loadGlobalSpecialties();
         this.loadGlobalComunas();
+    */
 },
-mounted() {},
 
 watch: {
     },//end watch
 
 methods: {
+        /*
         async loadGlobalSpecialties() {
-                console.log ("APP GET SPECIALTY LIST METHOD"); 
-				let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_specialty_list");
+                let metric=Date.now()  ; 
+              	let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_specialty_list");
                 this.global_specialties = response_json.data.rows;
-                console.log("APP Specialty list: "+JSON.stringify(this.global_specialties) );
-
+               // console.log("APP Specialty list: "+JSON.stringify(this.global_specialties) );
+                console.log("GET SPECIALTY LIST METHOD, performance , "+ (Date.now() - metric) );
             },
         async loadGlobalComunas() {
-                 console.log ("APP GET COMUNA LIST METHOD"); 
-				let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_comuna_list");
+                let metric=Date.now()  ; 
+               	let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_comuna_list");
                 this.global_comunas = response_json.data.rows;
-                console.log("APP Comuna list: "+JSON.stringify(this.global_comunas) );
-
+              // console.log("APP Comuna list: "+JSON.stringify(this.global_comunas) );
+                console.log("APP GET COMUNA LIST METHOD, performance , "+ (Date.now() - metric));
             },
-
-
-
+        */
 
         },
 
