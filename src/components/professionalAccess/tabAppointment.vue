46a737f5829d7a10e6770fc047cb1cb9ca913117
unknown <74009893+alejandro2141@users.noreply.github.com>
2022-01-19 18:59:39 -0400

    Update tabAppointment.vue

--- src/components/professionalAccess/tabAppointment.vue
+++ src/components/professionalAccess/tabAppointment.vue
@@ -51,7 +51,7 @@ data: function () {
             //this.getCenters();
             console.log("TAB APPOINTMENT session_params "+ this.session_params.professional_id );   
             this.daterequired = new Date().toISOString().split('T')[0] ;
-            this.updateAppointmentList();
+           // this.updateAppointmentList();
          },
  
     methods: {
