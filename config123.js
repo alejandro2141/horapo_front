aa41aceea028f1ec272725934bb5820ad47a08e9
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-01-25 15:26:31 -0300

    Update config123.js

--- config123.js
+++ config123.js
@@ -5,7 +5,7 @@ let BKND_REGIONS = [] ;
 */
 let BKND_CONFIG = {
   //  BKND_HOST : "http://ec2-18-222-88-128.us-east-2.compute.amazonaws.com:8080",
-   BKND_HOST : "http://192.168.0.110:8080",
+   BKND_HOST : "http://localhost:8080",
     BKND_TIME : 1000,
     BNKD_USER : 'abc'
   };
