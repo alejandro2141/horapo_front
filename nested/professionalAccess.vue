b6361191b4001b151cf503ed65da8051eadde3ce
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-01-31 19:59:18 -0300

    Update professionalAccess.vue

--- nested/professionalAccess.vue
+++ nested/professionalAccess.vue
@@ -58,8 +58,8 @@ export default {
         visible_tab_centers: 'none',
         visible_tab_assistants:'none', 
         visible_tab_appointments: 'none',
-        visible_tab_login: 'none' ,
-        visible_tab_timetable: 'block' ,
+        visible_tab_login: 'block' ,
+        visible_tab_timetable: 'none' ,
         
 
         product : 'Socks',
