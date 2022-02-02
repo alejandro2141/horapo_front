efe5f626b3323b994d4f7a79b7cc71870ba1d4c2
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-02-02 18:29:48 -0300

    Create modalViewCalendar.vue

---
+++ src/components/professionalAccess/modalViewCalendar.vue
@@ -0,0 +1,228 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+import inputFormComuna from  '../publicSearch/InputFormComuna.vue'
+import GenericBlockDateSpecialtyVue from '../GenericBlockDateSpecialty.vue';
+import InputFormCenterProfessional from './inputFormCenterProfessional.vue';
+import InputFormComunaProfessional from './inputFormComunaProfessional.vue' ;
+
+
+</script>
+
+<template>
+
+     	<teleport to="body"   >
+		<div  v-if="showModalViewCalendar" class="modal bg-secondary scroll" >
+		    <transition name="modal">
+			<div class="modal-mask "   >
+			<div class="modal-wrapper ">
+			<div class="modal-container  m-1 p-1 modal-background">
+ 
+                <div class="modal-header">
+                        <div class="text-secondary  display-5">
+                        Detalles Horario
+                        </div>
+                        <div class="p-1 "><i class="display-1 text-primary bi bi-x-lg ml-0"  v-on:click="showModalViewCalendar = false ; $emit('updateCalendarList'); "  aria-label="Close"></i>
+                        </div>
+                </div>
+
+                <div class="modal-body mt-0"> 
+                  {{calendar_details.id}}
+                  {{ idSpecialty2name(calendar_details.specialty1) }}
+                  
+                  <button v-if="calendar_details.active" @click="inactiveCalendar(calendar_details)" type="button" class="btn btn-primary  btn-danger ">Desactivar</button>
+                  <button  v-else   @click="activeCalendar(calendar_details)" type="button" class="btn btn-primary btn-success ">Activar</button>
+                  
+
+                </div>
+
+            </div> 
+            </div> 		
+            </div> 
+            </transition>
+    	</div>
+	</teleport> 
+
+</template>
+
+
+
+<style scoped>
+
+.modal {
+   /*position: static;  */
+   /*position: static; */
+	/*position: relative; */ 
+	/*position: absolute; */ 
+	position: fixed; 
+	/*position: sticky; */
+ /* position: fixed;  */
+ /* display: block; */ 
+   display: flex; 
+
+}
+
+/*
+.modal div {
+  display: flex; 
+  flex-direction: column; 
+
+}
+*/
+
+div.scroll {
+       			margin:4px, 4px;
+                padding:4px;
+                background-color: green;
+                width: 100%; 
+                /* height: 190%;*/
+                overflow-x: auto;
+                overflow-y: auto;
+                text-align:justify;
+      }
+
+/*
+.modal-background {
+    background-color:#DAEFF3
+        }
+*/
+/************************* */
+
+
+
+.modal-mask {
+  /*position: fixed;*/
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+ /* display: table;*/
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+ /*width: 100px;*/
+  margin: 2px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  display: block;
+  margin-top: 1rem;
+}
+
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
+
+/************************ */
+
+</style>
+
+
+<script>
+
+export default {
+
+data: function () {
+		return {
+			      showModalViewCalendar  : false ,
+		 }
+	},
+
+	props: ['session_params','activatorViewCalendar','global_comunas','calendar_details', 'global_specialties'],
+    emits: ['updateCalendarList','switchView'],
+
+    created () {
+    },
+
+    mounted () {
+         },
+ 
+    methods: {
+
+       idSpecialty2name(id){
+            let temp= this.global_specialties.find(elem => elem.id ==  id  )
+            if (temp != null) { return temp.name }
+            else { return null }
+        },
+        
+        switchView(){
+              this.$emit('switchView');
+                  },
+
+        async activeCalendar(calendar_details)
+        {
+          console.log("Activate Calendar")
+          
+                  const json = { 
+                    professional_id: this.session_params.professional_id ,
+                    calendar_id :  calendar_details.id,
+                    };
+
+                  console.log("Activate Calendar REQUEST :"+JSON.stringify(json));
+                  let response_json = await axios.post("http://localhost:8080"+"/professional_activate_calendar",json);
+                  console.log ("Activate Calendar RESPONSE:"+JSON.stringify(response_json.data.rows)) ;
+                  let aux_resp = response_json.data.rows ; 
+        },
+
+        async inactiveCalendar(calendar_details)
+        {
+          console.log("INActivate Calendar")
+          
+                  const json = { 
+                    professional_id: this.session_params.professional_id ,
+                    calendar_id :  calendar_details.id,
+                    };
+
+                  console.log("INActivate Calendar REQUEST :"+JSON.stringify(json));
+                  let response_json = await axios.post("http://localhost:8080"+"/professional_inactivate_calendar",json);
+                  console.log ("INActivate Calendar RESPONSE:"+JSON.stringify(response_json.data.rows)) ;
+                  let aux_resp = response_json.data.rows ; 
+        },
+
+
+	    },
+    
+    watch : {
+        activatorViewCalendar(newValue){
+            console.log ("showModalViewCalendar !!!"+newValue );  
+            this.showModalViewCalendar = true ;
+            console.log(" Show Calendar : "+JSON.stringify(this.calendar_details) );
+
+
+        },
+
+
+    },
+    
+
+
+}
+</script>
+
