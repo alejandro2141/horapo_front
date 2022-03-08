8ba9f2d63345744fb39f4ee145b8e11d42b1dac4
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-03-08 20:03:12 -0300

    Update appointmentReserved.vue

--- src/components/professionalAccess/appointmentReserved.vue
+++ src/components/professionalAccess/appointmentReserved.vue
@@ -8,42 +8,35 @@ import axios from 'axios';
 <template>
      
     <div  :style="{ 'background-color' : appointment.color  }"  class=" " data-bs-toggle="modal" :data-bs-target="'#modal_appdetails_'+index"  >
-    <text class="text-secondary" style="">#{{appointment.id}} </text>
-        <div id="app " class="m-0 d-flex  "  >			
-                
-                <div class="m-0 border-bottom" :style="{'background-color' : '#'+appointment.center_color }" >
-                    &nbsp;  &nbsp;&nbsp;
+    
+        <div class="d-flex justify-content-between" >
+            
+            <div class="mr-2 pr-2" style="marginLeft : 5px">
+                <div v-if="appointment.center_visit"  >
+                    <i class="h5 bi bi-geo-alt"></i> <text class="text-success "  >{{ id2name(appointment.specialty ) }}  </text> 
                 </div>
+                <div v-if="appointment.home_visit">
+                    <i class="h5 bi bi-house" > </i> <text class="text-success "  >{{ id2name(appointment.specialty ) }}  </text> 
+                </div>  
+                               
+            </div>   
+
+            
+            <div class="display-4">
+                <p>{{appointment.start_time.substring(0, 5) }} <text class="text-muted h6 mt-0"> {{appointment.duration }}m </text> </p>
+            </div>
         
-                <div class="m-2">
-                    
-                    <div class="display-4">
-                    {{appointment.start_time.substring(0, 5) }}							
-                    </div>
-                    <div class="text-muted">
-                   {{appointment.duration }}min.  
-                    </div>
-                 
-                </div>
+        </div>
+  <!--
+        <div id="app " class="m-0 d-flex  "  >			
                 
 
                 <div class="mt-2 w-100" >
                     <div class="d-flex justify-content-between " >
                         <div>
-                       <text class="text-success display-5"  >{{ id2name(appointment.specialty ) }}  </text> 
-                       <!--	
-                        <i v-if="appointment.public" style="margin-left: 1.0em;" class="bi bi-wifi text-success ">On Line</i>
-                        <br>
-                        -->
-                        <!--
-                        <div class="text-danger" v-if="appointment.app_status == 1 ">
-                            Cita Activa 
-                        </div>
-                        -->
-                        <text class="" v-if='appointment.confirmation_status == 0' >
-                          <br>  Reservada.<br> <text class="bg-secondary text-white" > Sin Confirmar </text>
-                        </text>
-                       
+                            <text class="" v-if='appointment.confirmation_status == 0' >
+                            <br>  Reservada.<br> <text class="bg-secondary text-white" > Sin Confirmar </text>
+                            </text>
                         </div>
                     
                     </div>
@@ -52,59 +45,22 @@ import axios from 'axios';
                      Espacio de tiempo Bloqueado
                      </div>
                 </div>
-
-
-        
         </div>
+-->
+            <div style=" marginRight : 15px;  " >
 
-            <div>
-                    <h4>{{appointment.patient_name }} </h4>
-                    id:{{appointment.patient_doc_id }},  
-                    Edad:{{appointment.patient_age }} 
+                    <ul class="list-group-flush "  style="border-radius: 15px;">
+                        <li class="list-group-item " >{{appointment.patient_name }}</li>
+                        <li class="list-group-item"> id:{{appointment.patient_doc_id }} </li>
+                        <li class="list-group-item">Edad:{{appointment.patient_age }} </li>
+                        <li v-if="appointment.home_visit" class="list-group-item" style="color:#3399FF"> A Domicilio :  {{appointment.patient_address }} </li>
+                        <li v-if="appointment.center_visit" class="list-group-item"> En Consulta   </li>
+                    </ul>
+
             </div>
             
-            <div v-if="appointment.home_visit" style=" font-size: 1.0em;" >
-                        <text style="color:#3399FF" >  
-                            <i class="bi bi-house">   </i> A Domicilio
-                        </text>
-                        <br>
-                         
-                           Direccion Paciente:  {{appointment.patient_address }} <br>
-                            <!--
-                            <text v-if=" appointment.location1 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ id2comuna(appointment.location1) }} <br>
-                            </text>
-                            <text v-if=" appointment.location2 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ id2comuna(appointment.location2) }} <br>
-                            </text>
-                            <text v-if=" appointment.location3 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ id2comuna(appointment.location3) }} <br>
-                            </text>
-                            <text v-if=" appointment.location4 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ id2comuna(appointment.location4) }} <br>
-                            </text>
-                            <text v-if=" appointment.location5 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ id2comuna(appointment.location5) }} <br>
-                            </text>
-                            <text v-if=" appointment.location6 != null " >  
-                                <i class="bi bi-geo-alt"></i> {{ id2comuna(appointment.location6) }} <br>
-                            </text>
-                            -->
-            </div>
-             <div v-if="appointment.center_visit" style=" font-size: 1.0em;" >
-                <i class="bi bi-building"> En Consulta  </i> <br>
-                    <div v-if="appointment.center_name != null " >
-                            {{appointment.center_name }}<br>
-                    </div>
-                    <div v-if="appointment.center_address != null ">
-                            {{appointment.center_address }}<br>
-                    </div>
-            </div>
-            
-           
 
-
-           	
+      <text class="text-secondary" style="">#{{appointment.id}} </text>      	
     </div>
 
 </template>
