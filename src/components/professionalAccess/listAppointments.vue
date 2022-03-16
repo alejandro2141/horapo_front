4ecc580e1f6cf4cf8f4788c9d110805acc853d77
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-03-16 20:25:13 -0300

    Update listAppointments.vue

--- src/components/professionalAccess/listAppointments.vue
+++ src/components/professionalAccess/listAppointments.vue
@@ -82,11 +82,11 @@ import ModalDuplicateDay from './modalDuplicateDay.vue';
     </div>
 
 
-    <div v-if="hours.length == 0 " class="d-flex justify-content-between mt-1   border border-secondary"  style="border-radius: 15px;" >
+    <div v-if="hours.length == 0 " class="d-flex justify-content-between mt-1  "  style="border-radius: 15px;" >
             
                 <div class="p-4 text-center" >    
                     <i class="display-1 bi bi-emoji-expressionless"></i><br>
-                  No existen Citas Reservadas o Calendarios Activos para este dia
+                  No existen Horarios o  Citas Reservadas para este dia
                 </div>
 
     </div>
