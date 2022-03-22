aa9a43f8d31b4bd9125e6e5a7decb17accc00913
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-03-22 19:34:25 -0300

    Update firstTimeLogin.vue

--- src/components/professionalAccess/firstTimeLogin.vue
+++ src/components/professionalAccess/firstTimeLogin.vue
@@ -4,30 +4,30 @@ import { ref } from 'vue'
 </script>
 
 <template>
-
+<div>
   
-<div class="text-center" >
-  
+        <div class="text-center" >
+        
+        </div>
+
+        <div class=" border text-center m-2 p-3">
+            <p class="display-1">Bienvenido </p>
+            <p> <i class="display-1 bi bi-emoji-wink"></i> </p>
+            <p>
+            Lo primero que debes hacer es crear un Centro. 
+            </p>
+            <p>
+            Un Centro es un lugar donde provees tus servicios, y donde tus pacientes debe asistir a tus citas.
+            </p>
+
+        <div class="display-4 text-center text-primary" @click='switchToCenter()' >
+            Crear mi primer centro 
+        </div>
+
+        </div>
+
+
 </div>
-
-<div class=" border text-center m-2 p-3">
-    <p class="display-1">Bienvenido </p>
-    <p> <i class="display-1 bi bi-emoji-wink"></i> </p>
-    <p>
-    Lo primero que debes hacer es crear un Centro. 
-    </p>
-    <p>
-    Un Centro es un lugar donde provees tus servicios, y donde tus pacientes debe asistir a tus citas.
-    </p>
-
-<div class="display-4 text-center text-primary" @click='switchToCenter()' >
-    Crear mi primer centro 
-</div>
-
-</div>
-
-
-
 
 </template>
 
