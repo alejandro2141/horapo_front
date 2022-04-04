926deee21d2f5c33172a20fbf2e017a328952d1d
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-04-04 00:03:32 -0400

    Update SearchAppointmentForm.vue

--- src/components/publicSearch/SearchAppointmentForm.vue
+++ src/components/publicSearch/SearchAppointmentForm.vue
@@ -4,58 +4,64 @@ import { ref } from 'vue'
 import inputFormSpecialty  from './InputFormSpecialty.vue'
 import inputFormComuna  from './InputFormComuna.vue'
 
-
 </script>
 
 <template>
-      
-
       <div class="container" style="padding-right: 0 ;padding-left: 0 ; " >
 
             <div id="formSearch" class="mx-auto "  >
                 <form autocomplete="off" >
                 <input  v-model="form_token" id="form_token" name="form_token" type="hidden"  > 
-                   
+
+<!--FORM INPUT SPECIALTY -->                   
                 <div class="w-100 mb-1">
                     <div class="">
                         <inputFormSpecialty  v-on:selectedSpecialtyCode="selectedSpecialtyCode" :global_specialties="global_specialties" > </inputFormSpecialty> 
                     </div>
                 </div>
-                
-                <div class="row  mb-1  border-secondary ">
+               
+<!--FORM INPUT  APP TYPE -->
+                <div v-if="show_app_type()" class=" d-flex justify-content-between border " style=" border-radius: 25px;"  >
+                    <button type="button" @click="form_app_type_center=!form_app_type_center ;form_app_type_home=false; form_app_type_remote=false " class="m-1 btn btn-outline-primary border-2 " :class="{ 'active' : form_app_type_center }" style="border-color: #781ED1; border-radius: 25px;" > 
+                        <i  class="h4 bi bi-building m-0 p-0"></i><br>
+                        Consulta  
+                    </button>
+
+                    <button type="button" @click="form_app_type_home=!form_app_type_home ;form_app_type_center=false; form_app_type_remote=false   "  class="m-1 btn btn-outline-primary border-2" :class="{ 'active' : form_app_type_home }" style="border-color:#3399FF; border-radius: 25px;" > 
+                        <i class="h4 bi bi-house-door m-0 p-0"></i><br>
+                        Domicilio 
+                    </button>
+
+                    <button type="button" @click="form_app_type_remote=!form_app_type_remote ;form_app_type_center=false; form_app_type_home=false  "  class="m-1 btn btn-outline-primary border-2" :class="{ 'active' : form_app_type_remote }"  style="border-color:#b36b00; border-radius: 25px; " > 
+                       <i class="h4 bi bi-camera-video m-0 p-0"></i><br>
+                        Remota 
+                    </button>
+
+                    <span class="align-middle mt-2">
+                        <i class="display-1 m-1  bi bi-x  text-muted "></i>
+                    </span>
+                    
+                </div>
+
+<!-- FORM INPUT LOCATION-->
+                <div v-if="show_input_location()"  class="row  mb-1  border-secondary ">
                     <div class="col">
                         <inputFormComuna position="true"  v-on:selectedComunaCode="selectedComunaCode" :global_comunas="global_comunas" > </inputFormComuna>
                     </div>
                 </div>
-                
-                <!-- DATE  --> 
-                <div class="row  mb-1  border-secondary ">
+
+<!-- FORM INPUT DATE -->
+                <div v-if="show_input_date()"  class="row  mb-1  border-secondary ">
                     <div class="col">
-                        <input style="border-radius: 25px;" v-model="form_current_date" :min="form_minimum_date" type="date" id="birthday" name="birthday" class="form-control form-control-lg border border-primary" >
+                        <input style="border-radius: 25px;" v-model="form_current_date" :min="form_minimum_date" type="date" id="app_date" name="app_date" class="form-control form-control-lg border border-primary" >
                     </div>
                 </div>
-                <!-- --> 
-    <!--
+                 
+            <!--
                 <button type="button" @click="form_app_type_center=!form_app_type_center ; form_app_type_home= false" class=" m-1 btn"  :class="{ 'btn-primary' : form_app_type_center , 'btn-secondary' : !form_app_type_center , 'btn-lg' : form_app_type_center }" >  En Consulta  </button>
                 <button type="button" @click="form_app_type_home=!form_app_type_home ; form_app_type_center= false "     class=" m-1 btn " :class="{ 'btn-primary' : form_app_type_home   , 'btn-secondary' : !form_app_type_home , 'btn-lg' : form_app_type_home     }" > A Domicilio </button>
             --> 
-            <div class="bg-light d-flex justify-content-between" >
-                    <button type="button" @click="form_app_type_center=!form_app_type_center ;form_app_type_home=false; form_app_type_remote=false " class="m-1 btn btn-outline-primary border-2 " :class="{ 'active' : form_app_type_center }" style="border-color: #781ED1; border-radius: 25px;" > 
-                        <i  class="h2 bi bi-building"></i><br>
-                        En Consulta  
-                    </button>
-
-                    <button type="button" @click="form_app_type_home=!form_app_type_home ;form_app_type_center=false; form_app_type_remote=false   "  class="m-1 btn btn-outline-primary border-2" :class="{ 'active' : form_app_type_home }" style="border-color:#3399FF; border-radius: 25px;" > 
-                        <i class="h2 bi bi-house-door"></i><br>
-                        A Domicilio 
-                    </button>
-
-                    <button type="button" @click="form_app_type_remote=!form_app_type_remote ;form_app_type_center=false; form_app_type_home=false  "  class="m-1 btn btn-outline-primary border-2" :class="{ 'active' : form_app_type_remote }"  style="border-color:#b36b00; border-radius: 25px; " > 
-                       <i class="h2 bi bi-camera-video"></i><br>
-                        Tele Atención 
-                    </button>
-
-            </div>
+           
 
 
                 <!--
@@ -94,6 +100,7 @@ import inputFormComuna  from './InputFormComuna.vue'
                 -->
              <!--       <i  type="submit" v-on:click="sendFormSearch()" class="btn  btn-lg btn-block text-white bg-dark " style="width: 100%;"  >{{ search_button_message }}</i>-->
                 </form> 
+               
             </div>	
         </div>			
 
@@ -133,7 +140,7 @@ export default {
     }
   },
 
- props: [ 'global_specialties','global_comunas', 'currentDate' ], 
+ props: [ 'global_specialties','global_comunas', 'currentDate','n_appointments_found' ], 
  emits: [ 'searchAppointments' ,'form_app_type_center_emit' , 'form_app_type_home_emit' , 'form_app_type_remote_emit'  ],
  
 
@@ -146,6 +153,29 @@ export default {
  
 
     methods: {
+
+        show_input_location()
+        {   if ( this.form_app_type_center || this.form_app_type_home   )
+            { return true } 
+            else
+            { return false }
+        },
+
+        show_input_date()
+        {   if ( this.form_specialty != null &&  this.form_comuna_code != null )
+            {   return true } 
+            else
+            { return false }
+        },
+
+        show_app_type()
+        {   if ( this.form_specialty != null && this.n_appointments_found>0 )
+            { return true } 
+            else
+            { return false }
+        },
+
+
         //V-ON to capture selection 
         selectedComunaCode(code)
         {
@@ -193,13 +223,11 @@ export default {
     
     watch : {
         
-        form_current_date(oldValue, newValue)
+        form_current_date(newValue, oldValue)
         {
-            console.log("DATE  CHANGE :"+oldValue+ " New Value"+newValue );
+            console.log("DATE  CHANGE :"+ oldValue+ " To New Value : "+newValue );
             if (newValue !=null)
-            {
-                this.sendFormSearch() ; 
-            }
+            {  this.sendFormSearch() ; }
         },
 
         form_app_type_center(newValue){
