256b75bf9b0238d20a20d0af57c12d840dfe1ac1
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-04-04 18:37:52 -0400

    Update SuggestedSearch.vue

--- src/components/publicSearch/SuggestedSearch.vue
+++ src/components/publicSearch/SuggestedSearch.vue
@@ -5,17 +5,19 @@ import axios from 'axios';
 
 <template>
        <div class="" >
-           <p   >Busquedas mas comunes</p><hr>
+           <hr>
+           <p   >Busquedas mas comunes</p>
+           <hr>
             
-            <p class="text-start"> <text class="display-5"> Kinesiologia </text><br> <text> A domicilio, Santiago </text> </p> 
+            <p class="text-start"> <text class="display-5"> Kinesiologia </text><br> <text class="text-muted"> A domicilio, Santiago </text> </p> 
             
-            <p class="text-end"> <text class="display-5"> Psicologia </text><br> <text> Tele Atención </text> </p> 
+            <p class="text-end"> <text class="display-5"> Psicologia </text><br> <text class="text-muted"> Tele Atención </text> </p> 
             
-            <p class="text-start"> <text class="display-5"> Nutrición </text><br> <text> En Centro, Providencia </text> </p> 
+            <p class="text-start"> <text class="display-5"> Nutrición </text><br> <text class="text-muted"> En Centro, Providencia </text> </p> 
    
-            <p class="text-end"> <text class="display-5"> Psicologia </text><br> <text> En Centro, Las Condes </text> </p> 
+            <p class="text-end"> <text class="display-5"> Psicologia </text><br> <text class="text-muted"> En Centro, Las Condes </text> </p> 
             
-            <p class="text-start"> <text class="display-5"> Kinesiologia </text><br> <text> A domicilio, San Miguel </text> </p> 
+            <p class="text-start"> <text class="display-5"> Kinesiologia </text><br> <text class="text-muted"> A domicilio, San Miguel </text> </p> 
             
 
 
