e24b587eea48fd1071b38443a77c7e77bfe4195b
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-04-07 18:48:15 -0400

    Update GeneralHeader.vue

--- src/components/GeneralHeader.vue
+++ src/components/GeneralHeader.vue
@@ -8,8 +8,8 @@ import { ref } from 'vue'
     <div class="text-center pt-2 pb-0 " style="background-color :#ffffff "  >
 		<div class="display-4 "  > 
 			<a HREF="/index.html" class="text-decoration-none" style="color :#2e5668"> 
-			<i class="bi bi-arrow-left-square"></i> 
-			123Hora  
+			<i class="bi bi-clipboard-pulse" style="font-size: 2rem; color: cornflowerblue;"></i>
+				123Hora  
 			</a> 
 		</div>
 		<div class="text-muted">	Las mejores consultas de salud en un solo lugar
