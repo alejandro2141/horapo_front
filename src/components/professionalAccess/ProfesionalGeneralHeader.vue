bb3c589a685cb008aa65c01431f248ddcb08a881
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-04-30 21:01:35 -0400

    Update ProfesionalGeneralHeader.vue

--- src/components/professionalAccess/ProfesionalGeneralHeader.vue
+++ src/components/professionalAccess/ProfesionalGeneralHeader.vue
@@ -11,7 +11,8 @@ import SwitchViewButton from './switchViewButton.vue'
     <div v-if="session_params['professional_name'] != null"  class="d-flex p-0 justify-content-around bg-white border-bottom "> 
 
 		<button v-if="session_params['professional_name'] != null"   @click="showUserMenu=!showUserMenu;  header_menu_appointments = false ; header_menu_calendars = false ; header_menu_centers = false "  type="button" class="btn btn-outline-primary border border-0 "  :class="{ active: showUserMenu }" >	
-			<i class="bi bi-key display-5"></i>	
+		
+			<i class="display-5 bi bi-person-circle"></i>
 		</button>
 
 		<button v-if="session_params['professional_name'] != null"   @click="switchViewTo(2); showUserMenu = false ; header_menu_appointments = true; header_menu_calendars = false ; header_menu_centers = false  "  type="button" class="btn btn-outline-primary border border-0" :class="{ active: header_menu_appointments }"  >	
@@ -21,20 +22,31 @@ import SwitchViewButton from './switchViewButton.vue'
 		<button v-if="session_params['professional_name'] != null"   @click="switchViewTo(1); showUserMenu = false ; header_menu_appointments = false ; header_menu_calendars = true ; header_menu_centers = false  "  type="button" class="btn btn-outline-primary border border-0" :class="{ active: header_menu_calendars }"  >	
 			<i class="bi bi-calendar display-5 "  ></i> Horario	
 		</button>
-
+		<!--
 		<button v-if="session_params['professional_name'] != null"   @click="switchViewTo(3); showUserMenu = false ; header_menu_appointments = false ; header_menu_calendars = false ; header_menu_centers = true "  type="button" class="btn btn-outline-primary border border-0" :class="{ active: header_menu_centers }"  >	
 			<i class="bi bi-geo-alt display-5 "  ></i> Consultas	
 		</button>
+		-->
 		
 	</div>
 
 
-	<div v-if="showUserMenu" class="bg-secondary text-primary bg-light border border-primary">
-		<div class="list-group list-group-flush   ">
+	<div v-if="showUserMenu" class="bg-secondary text-primary bg-white border  border-1 border-primary ml-1 mr-1" >
+		<div class="  ">
 			<hr>
 			<a HREF="/index.html" class="h3  text-decoration-none"> &nbsp; <i class="bi bi-arrow-left-circle"></i> &nbsp; Salir </a><hr>
 			<a @click="switchViewTo(4);showUserMenu=!showUserMenu" class="h3  text-decoration-none"> &nbsp; <i class="bi bi-person-circle "></i> &nbsp; Información de Usuario </a><hr>
+
+			<button v-if="session_params['professional_name'] != null"   @click="switchViewTo(3); showUserMenu = false ; header_menu_appointments = false ; header_menu_calendars = false ; header_menu_centers = true "  type="button" class="btn btn-outline-primary border border-0" :class="{ active: header_menu_centers }"  >	
+			<i class="bi bi-geo-alt display-5 "></i> <text class="h3"> Consultas/Direcciones </text>	
+			</button>
+			<hr>
+
+		<div  style="width: 00px; height: 600px;" >
 			
+		</div>
+		
+		
 		</div>
 	</div>
 
