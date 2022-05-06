230e8cddb2dc20631e857f3225b5abca70051368
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-05-06 11:27:52 -0400

    Update appointmentReserved.vue

--- src/components/professionalAccess/appointmentReserved.vue
+++ src/components/professionalAccess/appointmentReserved.vue
@@ -7,33 +7,48 @@ import axios from 'axios';
 
 <template>
      
-    <div  :style="{ 'background-color' : getCenter(appointment.center_id).center_color   }"  style="border-radius: 25px;"  class="mt-1 border border-secondary" data-bs-toggle="modal" :data-bs-target="'#modal_appdetails_'+index"  >
+    <div  :style="{ 'background-color' : getCenter(appointment.center_id).center_color   }"  style="border-radius: 25px;"  class="mt-1 border border-secondary text-muted" data-bs-toggle="modal" :data-bs-target="'#modal_appdetails_'+index"  >
     
         <div class="d-flex justify-content-between ml-4" >
-            
+
+             <div class="d-flex justify-content-start display-6"  >
+                             <text class="p-1" >
+                                        <i v-if="appointment.center_visit" class=" bi bi-building"></i>      
+                                        <i v-if="appointment.home_visit"  class=" bi bi-house-door" > </i>                                  
+                                        <i v-if="appointment.remote_care" class=" bi bi-camera-video"></i> 
+                             </text> 
+                             <text class="p-1  "> {{ appointment.start_time.substring(0,5) }} </text>
+                             
+                             <text class="p-1  "><small> {{ id2name(appointment.specialty)}} </small> </text>
+             </div>    
+
+
+            <!--
             <div class="mr-2 pr-2" style="marginLeft : 5px">
      
                 <div v-if="appointment.center_visit"  >
                     <i class="h1 bi bi-building"></i> 
-                    <text class=" h2 text-success "  >&nbsp;&nbsp;{{ id2name(appointment.specialty ) }}  </text> 
+                    <text class=" h2"  >&nbsp;&nbsp;{{ id2name(appointment.specialty ) }}  </text> 
                 </div>
 
                 <div v-if="appointment.home_visit">
                     <i class="h2 bi bi-house-door" > </i> 
-                    <text class="h2 text-success "  >&nbsp;&nbsp; {{ id2name(appointment.specialty ) }}  </text> 
+                    <text class="h2  "  >&nbsp;&nbsp; {{ id2name(appointment.specialty ) }}  </text> 
                 </div>  
                 
                 <div v-if="appointment.remote_care">
                        <i class="h2 bi bi-camera-video"></i>
-                       <text class="h2 text-success "  >&nbsp;&nbsp; {{ id2name(appointment.specialty ) }}  </text> 
+                       <text class="h2  "  >&nbsp;&nbsp; {{ id2name(appointment.specialty ) }}  </text> 
                 </div>  
                                
             </div>   
+            -->
 
-            
-            <div class="display-4 ">
-               {{appointment.start_time.substring(0, 5) }} <text class="text-muted h6 mt-0"> {{appointment.duration }}m </text> 
+            <!--
+            <div class="display-6 ">
+             <text class="text-muted"> <small>{{appointment.duration }}min </small></text> 
             </div>
+            -->
         
         </div>
   <!--
@@ -56,7 +71,7 @@ import axios from 'axios';
                 </div>
         </div>
 -->
-            <div class="">
+            <p class="ml-5">
                         <text >{{appointment.patient_name }}</text><br>
                         <text > id:{{appointment.patient_doc_id }} </text><br>
                         <text >Edad:{{appointment.patient_age }} </text><br>
@@ -71,7 +86,7 @@ import axios from 'axios';
                     
                     {{appointment.message1 }}
 
-            </div>
+            </p>
            
             <text class="text-secondary p-2 m-2" style="">#{{appointment.id}} </text>   
 
