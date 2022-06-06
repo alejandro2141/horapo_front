be044c8112b6cce1efae155de5a8752fb3667faa
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-06-06 17:33:20 -0400

    Update appointmentReserved.vue

--- src/components/professionalAccess/appointmentReserved.vue
+++ src/components/professionalAccess/appointmentReserved.vue
@@ -6,7 +6,60 @@ import axios from 'axios';
 </script>
 
 <template>
-     
+
+    <div class="d-flex justify-content-between mb-3">
+                    
+        <div class="p-2">
+            <text class="fs-1 fw-light"> {{ appointment.start_time.substring(0,5) }} </text><br>
+            <text class="fs-3 fw-lighter" >
+                <i v-if="appointment.center_visit" class=" bi bi-building"></i>      
+                <i v-if="appointment.home_visit"  class=" bi bi-house-door" > </i>                                  
+                <i v-if="appointment.remote_care" class=" bi bi-camera-video"></i>                         
+            </text> 
+        </div>
+                    
+        <div  class="w-100 text-white  p-2" :style="{ 'background-color' : appointment.calendar_color  }"  style="border-radius: 15px;"  >
+                            
+            <div class=""  >
+                <text class="display-6 m-0 p-0" ><small> {{specialtyId2name(appointment.specialty)}} </small> </text>
+            </div>  
+                       
+            <div class="">
+                <text class="display-6 m-0 p-0" > 
+                    <small> <small> {{ getCenterName(appointment.center_id) }} </small> </small>
+                </text>
+            </div>
+
+            <div class=" mt-2">
+                        <text > {{appointment.patient_name }}</text><br>
+                        <text ><i class="bi bi-dot"></i> id:{{appointment.patient_doc_id }} </text><br>
+                        <text ><i class="bi bi-dot"></i> Edad:{{appointment.patient_age }} </text><br>
+                        <text v-if="appointment.home_visit"  ><i class="bi bi-dot"></i> A Domicilio <br> 
+                        - {{appointment.patient_address }} </text>
+                        
+                        <text v-if="appointment.center_visit"><i class="bi bi-dot"></i>  En Consulta <br>
+                        </text>
+                        
+                        
+                        <text v-if="appointment.remote_care" ><i class="bi bi-dot"></i> Atención Remota  </text>
+                    
+                   {{appointment.message1 }} 
+                   <p></p>
+
+            </div>
+
+
+
+                            
+        </div>
+
+
+    </div>
+
+
+
+
+     <!--
     <div  :style="{ 'background-color' : appointment.calendar_color   }"  style="border-radius: 25px;"  class="mb-2 border border-secondary text-muted" data-bs-toggle="modal" :data-bs-target="'#modal_appdetails_'+index"  >
     
         <div class=" ml-4" >
@@ -24,58 +77,10 @@ import axios from 'axios';
 
             <p class="display-6 mb-2 mt-0 "> <small><small>&nbsp;&nbsp;&nbsp; {{getCenter(appointment.center_id).name }} </small> </small>   </p>
 
-            <!--
-            <div class="mr-2 pr-2" style="marginLeft : 5px">
-     
-                <div v-if="appointment.center_visit"  >
-                    <i class="h1 bi bi-building"></i> 
-                    <text class=" h2"  >&nbsp;&nbsp;{{ id2name(appointment.specialty ) }}  </text> 
-                </div>
-
-                <div v-if="appointment.home_visit">
-                    <i class="h2 bi bi-house-door" > </i> 
-                    <text class="h2  "  >&nbsp;&nbsp; {{ id2name(appointment.specialty ) }}  </text> 
-                </div>  
-                
-                <div v-if="appointment.remote_care">
-                       <i class="h2 bi bi-camera-video"></i>
-                       <text class="h2  "  >&nbsp;&nbsp; {{ id2name(appointment.specialty ) }}  </text> 
-                </div>  
-                               
-            </div>   
-            -->
-
-            <!--
-            <div class="display-6 ">
-             <text class="text-muted"> <small>{{appointment.duration }}min </small></text> 
-            </div>
-            -->
-        
+          
         </div>
 
 
-
-        
-  <!--
-        <div id="app " class="m-0 d-flex  "  >			
-                
-
-                <div class="mt-2 w-100" >
-                    <div class="d-flex justify-content-between " >
-                        <div>
-                            <text class="" v-if='appointment.confirmation_status == 0' >
-                            <br>  Reservada.<br> <text class="bg-secondary text-white" > Sin Confirmar </text>
-                            </text>
-                        </div>
-                    
-                    </div>
-                    
-                     <div class="text-danger" v-if="appointment.app_blocked == 1 ">
-                     Espacio de tiempo Bloqueado
-                     </div>
-                </div>
-        </div>
--->
             <div class="">
                         <text >&nbsp;&nbsp;&nbsp;<i class="bi bi-dot"></i> {{appointment.patient_name }}</text><br>
                         <text >&nbsp;&nbsp;&nbsp;<i class="bi bi-dot"></i> id:{{appointment.patient_doc_id }} </text><br>
@@ -94,9 +99,8 @@ import axios from 'axios';
 
             </div>
            
-           <!-- <text class="text-secondary p-2 m-2" style="">#{{appointment.id}} </text>    -->
-
     </div>
+    -->
 
 </template>
 
@@ -117,7 +121,20 @@ export default {
 	created () {
 	},
 	methods :{
-      	
+
+        specialtyId2name(id){
+            let temp= this.global_specialties.find(elem => elem.id ==  id  )
+            if (temp != null) { return temp.name }
+            else { return null }
+
+        },
+         getCenterName(id){
+            let temp= this.session_params.centers.find(elem => elem.id ==  id  )
+            if (temp != null) { return temp.name }
+            else { return "No Existe Centro" }
+        },
+
+      	/*
         id2name(id){
             let temp= this.global_specialties.find(elem => elem.id ==  id  )
             if (temp != null) { return temp.name }
@@ -134,6 +151,7 @@ export default {
             if (temp != null) { return temp }
             else { return null }
         },
+        */
 
     }
 }
