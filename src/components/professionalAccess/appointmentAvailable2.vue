3a8e0df385c926923ead971a50d6cca63dc3dae1
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-06-06 17:33:23 -0400

    Create appointmentAvailable2.vue

---
+++ src/components/professionalAccess/appointmentAvailable2.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+
+</script>
+
+<template>
+
+                    <div class="d-flex justify-content-between mb-3">
+                    
+                        <div class="p-2">
+                            <text class="fs-1 fw-light"> {{ appointment.start_time.substring(0,5) }} </text><br>
+                            <text class="fs-3 fw-lighter" >
+                                <i v-if="appointment.center_visit" class=" bi bi-building"></i>      
+                                <i v-if="appointment.home_visit"  class=" bi bi-house-door" > </i>                                  
+                                <i v-if="appointment.remote_care" class=" bi bi-camera-video"></i>                         
+                            </text> 
+                        </div>
+                    
+                        <div  class="w-100 text-white display-6 p-2" :style="{ 'background-color' : appointment.calendar_color  }"  style="border-radius: 15px;"  >
+                            
+                            <div class=""  >
+                                <text class="" ><small> {{specialtyId2name(appointment.specialty)}} </small> </text>
+                            </div>  
+                           
+                            <div class="">
+                                <text class="" > 
+                                     <small> <small> {{ getCenterName(appointment.center_id) }} </small> </small>
+                                </text>
+                            </div>
+                            
+                        </div>
+
+
+                    </div>
+
+</template>
+
+<style scoped>
+</style>
+
+
+<script>
+
+export default {
+   data : function() {
+        return {
+        }   
+    },
+   	
+    props: ['daterequired','appointment','index','global_specialties', 'global_comunas', 'session_params' ],
+
+	created () {
+	},
+	methods :{
+      	
+       specialtyId2name(id){
+            let temp= this.global_specialties.find(elem => elem.id ==  id  )
+            if (temp != null) { return temp.name }
+            else { return null }
+
+        },
+         getCenterName(id){
+            let temp= this.session_params.centers.find(elem => elem.id ==  id  )
+            if (temp != null) { return temp.name }
+            else { return "No Existe Centro" }
+        },
+
+    }
+}
+
+</script>
+
