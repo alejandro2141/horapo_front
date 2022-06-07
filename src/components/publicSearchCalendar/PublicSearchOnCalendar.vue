6aecb447a0879eafcc4a8e8dff9c9d5d85f744a9
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-06-07 18:35:54 -0400

    Create PublicSearchOnCalendar.vue

---
+++ src/components/publicSearchCalendar/PublicSearchOnCalendar.vue
@@ -0,0 +1,207 @@
+<script setup>
+import { ref } from 'vue'
+import LoadProgress from '../loadProgress.vue'
+/*
+import patientAppointmentAvailable  from '../publicSearch/PatientAppointmentAvailable.vue'
+import modalPublicViewAppointment from '../publicSearch/ModalPublicViewAppointment.vue'
+
+*/
+</script>
+
+<template>
+    <div>
+  
+        <p>
+            Component GET PARAMETERS
+        </p>
+       
+       <!-- FORM INPUT DATE -->
+                <div v-if="true"  class="row  mb-1  ">
+                    <div class="col">
+                        <input style="border-radius: 25px;" v-model="form_current_date" :min="form_minimum_date" type="date" id="app_date" name="app_date" class="form-control form-control-lg border border-primary" >
+                    </div>
+                </div>
+
+        <!--
+        <loadProgress  :active_spinner="active_spinner" > </loadProgress>
+        
+        <div  v-if="appointments!= null" >
+            <div class="mt-0 "  v-for="appointment in appointments" :key="appointment.id" >
+                
+                    <patientAppointmentAvailable :searchParameters="searchParameters" class=" m-2 "  v-if="appointment != null"  v-on:click="setModalReserve(appointment)" :appointment='appointment'  :global_comunas="global_comunas" :global_specialties="global_specialties"  > </patientAppointmentAvailable>            
+             
+            </div>
+            <div style="height: 400px">
+            </div>
+
+        </div>	
+        <modalPublicViewAppointment  :searchParameters="searchParameters" :app="app" :openModalEvent="openModalEvent"   v-on:updateLastSearch="updateLastSearch"  :global_comunas='global_comunas' :global_specialties="global_specialties"  > </modalPublicViewAppointment>
+        -->
+    </div>     
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+
+export default {
+ data : function() {
+    return {
+
+            cal_id : null ,
+            token : null ,
+            appointments : null ,
+
+            date : null ,
+            form_current_date: null ,
+            form_minimum_date : null ,
+
+
+            active_spinner : false ,
+
+            /*
+            app : null , 
+            showModal : false , 
+            showModalConfirmation : false , 
+            appConfirmed : null, 
+            appointment_list : null, 
+          //  notificationMessage: null, 
+            modalOpen : ref(false), 
+            openModalEvent : null , 
+            showLoaderProgress : false , 
+            */
+           
+    }
+  },
+
+   props: ['searchParameters','session_params','daterequired','global_comunas', 'global_specialties', 'filter_center' , 'filter_home' , 'filter_remote' ],
+   emits: ["updateLastSearch"],
+
+ 
+
+    beforeCreate(){
+      
+    },
+    activated(){
+      
+    },
+   mounted () {    
+        let uri = window.location.search.substring(1); 
+        let params = new URLSearchParams(uri);
+        this.cal_id=params.get("cal_id")
+        this.token=params.get("token")
+        console.log("URL PARAMETROS : cal_id:"+this.cal_id+ " Token:"+this.token  )
+        },
+
+    beforeUpdate(){
+        /*
+        this.showLoaderProgress = true  ;
+        console.log("showloader progress BEFORE UPDATE !!!");
+        */
+        },
+
+    updated()
+        {
+            /*
+        this.showLoaderProgress = false  ;
+        console.log("showloader progress UPDATE !!!");
+        */
+        },
+
+    watch: {
+            /*
+            appointments(newAppointments, oldAppointments ) {
+                this.appointment_list =  newAppointments ;   
+             //   this.notificationMessage="Econtramos "+this.appointments.length+" resultados, desde dia "+this.daterequired +" ";	                 
+            },
+            */
+        },
+
+    methods: {
+
+        async searchAppointmentsCalendar(params) {	
+         
+              if (  params.specialty != null )
+              { 
+                            
+                          let metric = Date.now();
+                          this.active_spinner = true ; 
+                          console.log("searchAppointmentsCalendar input params :"+JSON.stringify(params) )
+                          
+                          let specialty_code = null ;
+                          specialty_code = params.specialty.id ; 
+                        
+                          const json = { 
+                            date : params.date ,
+                            token : this.token , 
+                            cal_id :  this.cal_id,
+                                };
+
+                  console.log ("searchAppointmentsCalendar input to send JSON :"+ JSON.stringify(json)  );
+                  let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/patient_get_appointments_calendar",json);
+                
+                  this.appointments = response_json.data;
+                  console.log ("searchAppointmentsCalendar RESPONSE:"+JSON.stringify(this.appointments)) ;
+
+                  // this.notificationMessage="Econtramos "+this.appointments.length+" resultados, desde dia "+this.daterequired +" ";	
+                          this.notificationMessage_alert=	false ;
+                          this.searchParameters = params ;
+                          this.params_bkp = params ; 
+                          this.active_spinner = false ;
+
+                    metric = (Date.now() - metric ) ;     
+                    this.metric_search = metric ;
+                    console.log("performance, searchAppointments , searchAppointments ,"+  this.metric_search  );
+                    
+                                  if (this.appointments != null)
+                                  {
+                                   // this.filtered_appointments = this.appointments
+                                   //this.n_appointments_found=this.appointments.length
+                                  }
+                                  else
+                                  {
+                                    this.n_appointments_found = 0 ;
+                                  }
+                   
+              }
+              else 
+              {
+                  this.appointments = null;
+              }
+                    
+                    
+           },
+
+
+
+          /*
+            setModalReserve(appointment)
+            {
+                console.log("Set Modal Reserve method in SearchApp Resutl"+JSON.stringify(appointment));
+                this.app=appointment;
+                //this.modalOpen = true; 
+                this.openModalEvent = Math.random();
+            },
+          
+            updateLastSearch()
+            {
+                console.log (" update search Result. ");
+                //this.appointment_list=null ;
+                this.$emit('updateLastSearch');
+            },
+
+            selectedInsuranceCode(code)
+            {
+            console.log("Insurance Code:"+code);
+            this.form_patient_insurance_code = code;
+            },
+            */
+
+        },
+
+}
+
+</script>
