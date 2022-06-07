2cf222794811b548af97c2bd6b49223129ec90a9
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-06-07 18:35:57 -0400

    Update publicSearchCalendar.vue

--- nested/publicSearchCalendar.vue
+++ nested/publicSearchCalendar.vue
@@ -1,25 +1,23 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-
+import { ref } from 'vue'
+import axios from 'axios'
+import { BKND_CONFIG } from '../config123.js'
+/*
 import GeneralHeader from '../src/components/GeneralHeader.vue'
+*/
+import PublicSearchOnCalendar from '../src/components/publicSearchCalendar/PublicSearchOnCalendar.vue'
+    
 
 </script>
 
 <template>
 <div>
-    <GeneralHeader></GeneralHeader>
-    <div id="app" class="text-center p-5 bg-light" >     
-   
-        
-        PUBLIC SEARCH in CALENDAR
-        <div class="mt-4 pt-4">
-        
-        <hr>
-            <a HREF="/index.html" type="button" class="btn btn-outline-secondary">Regresar</a>
-        </div>
+    <div id="app" class="p-3" >    
+        <PublicSearchOnCalendar :global_specialties="global_specialties" :global_comunas="global_comunas" ></PublicSearchOnCalendar>
     </div>
-    
+
 </div>
 </template>
 
@@ -28,3 +26,53 @@ import GeneralHeader from '../src/components/GeneralHeader.vue'
 
 
 </style>
+
+<script>
+
+export default {
+ data : function() {
+        return {
+            global_specialties : [],
+            global_comunas : [],  
+        }
+    },  
+    props: [], 
+    emits: [],
+
+created() {
+/*
+        console.log("NESTED PUBLIC SEARCH Fill Global variables");
+        this.loadGlobalSpecialties();
+        this.loadGlobalComunas();
+*/
+},
+mounted() {},
+
+watch: {
+    },//end watch
+
+methods: {
+
+        /*
+        async loadGlobalSpecialties() {
+                let metric=Date.now()  ; 
+				let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_specialty_list");
+                this.global_specialties = response_json.data.rows;
+                //console.log("APP GET COMUNA LIST METHOD, "+JSON.stringify(this.global_specialties) );
+                 console.log("performance , Public Search ,loadGlobalSpecialties, "+ (Date.now() - metric));
+            },
+
+        async loadGlobalComunas() {
+                let metric=Date.now()  ; 
+				let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_comuna_list");
+                this.global_comunas = response_json.data.rows;
+                //console.log("APP Comuna list: "+JSON.stringify(this.global_comunas) );
+                 console.log("performance , Public Search , loadGlobalComunas , "+ (Date.now() - metric));
+            },
+            */
+
+        },
+
+}
+
+</script>
