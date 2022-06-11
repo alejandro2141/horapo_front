ecf6aa417b6a8f08f6ded233844d32d8d712f0e3
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-06-11 19:08:17 -0400

    Update AppointmentAvailableSearchCalendar.vue

--- src/components/publicSearchCalendar/AppointmentAvailableSearchCalendar.vue
+++ src/components/publicSearchCalendar/AppointmentAvailableSearchCalendar.vue
@@ -9,88 +9,24 @@ import { ref } from 'vue'
 <template>
     
     
-    <div v-if="appointment != null"   style="border-radius: 15px; background-color: #2874A6;"  class="text-white fs-4 p-3 mb-1  ">
+    <div v-if="appointment != null"   style="border-radius: 15px; background-color: #2874A6;"  class="text-white p-3 mb-1  ">
         
-        <text>{{ appointment.date.split("-")[2].substring(0,2)  }} de {{ month_name[parseInt(appointment.date.split("-")[1]-1 ) ] }} </text>
-        <br><text>{{appointment.date.split("-")[0] }}   </text>   
- 
-
-
-    {{appointment.start_time}} 
-    <!-- 
-        <div id="app" class="m-0 d-flex  "  >	
-            <div class="">
-                     &nbsp;
-            </div>
+        <div class="">
             <div>
-                <div class="">
-                    <div class="display-5" style=" color:#2e5668">	 
-                   <b>  {{ transform_date( appointment.date.substring(0, 10) ) }} </b>
-                    
-                 {{appointment.start_time}} <text style=" font-size: 0.6em;">hrs</text>  
-                    </div>       
-                </div>
-           
-                <div class="display-5" style=" color:#1f9d94">
-                    <div >	
-                    {{ showSpecialtyName(appointment.specialty1) }} 
-                    </div>       
-                </div>
- 
-                <div class="">
+                <text class="fs-3"> {{day_name[ new Date(appointment.date).getDay()] }} &nbsp; </text>
+                <text class="fs-3"> {{ appointment.date.split("-")[2].substring(0,2) }} de {{ month_name[parseInt(appointment.date.split("-")[1]-1 ) ] }} </text>
+                <text class="fs-3">&nbsp;  {{appointment.date.split("-")[0] }}   </text>   
+            </div>    
 
-                    <div v-if="appointment.center_visit" class="">
-                 
-                        <div class="display-6" style="">  
-                            <div style="color: #781ED1" >
-                            <i class="h1 bi bi-building"></i> En Consulta  </div>
-                            </div>
-                            <div style="" class="text-dark display-6" >
-                               {{comuna_id2name(appointment.center_visit_location)}}
-                            </div>
-                            <div class="" style="color:#2e5668" >	
-                                {{appointment.center_name}}
-                            </div> 
-                            <div style="color:#2e5668">
-                               {{appointment.center_address}}
-                            </div>
-                            
-                    </div>
-                    
-                    <div v-if="appointment.home_visit" style="color:#3399FF">
-                            <div class="display-5" >
-                                <i class=" bi bi-house-door"></i><text >  Visita a Domicilio:</text> <br>
-                            </div>
-                            <div style="" class="text-dark display-6" >
-                              {{  comuna_id2name(appointment.home_visit_location1) }}
-                              {{  comuna_id2name(appointment.home_visit_location2) }}
-                              {{  comuna_id2name(appointment.home_visit_location3) }}
-                              {{  comuna_id2name(appointment.home_visit_location4) }}
-                              {{  comuna_id2name(appointment.home_visit_location5) }}
-                              {{  comuna_id2name(appointment.home_visit_location6) }}
-                            </div>
-                    </div>
-
-                    <div v-if="appointment.remote_care" class="">
-                         <div class="display-5" style="color:#b36b00" >
-                               <i class="bi bi-camera-video"></i> Tele Atención  	 
-                              <div style="" class="text-dark display-6" >
-                               Todas las comunas 
-                              </div>                      
-                        </div>
-                    </div>
-
-                </div>
-            
+            <div>
+                <text class="display-2">
+                    {{appointment.start_time.split(":")[0].padStart(2,0)  }} : {{appointment.start_time.split(":")[1].padStart(2,0)  }} 
+                </text>
             </div>
 
-            </div>
+        </div>
 
-                
-     <text style="color: #ffffff;" >#{{appointment.calendar_id}}</text> 
--->
-
-       </div>
+    </div>
 
 </template>
 
@@ -106,7 +42,7 @@ import { ref } from 'vue'
 export default {
   data : function() {
     return {
-        day_name : ['Lunes','Martes','Miercoles','jueves','Viernes','Sabado','Domingo'] , 
+        day_name : [ 'Domingo','Lunes','Martes','Miercoles','jueves','Viernes','Sabado'] , 
         month_name : ['Enero','Ferero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'] , 
 
             }
