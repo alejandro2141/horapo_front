fd416701a4ce137010df5c6d8d75c26a98874d16
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-06-21 21:04:12 -0400

    Update FooterContent.vue

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -9,46 +9,68 @@ const count = ref(0)
 </script>
 
 <template>
+
+	<div class="fixed-bottom">
+
+    <div  class="d-flex justify-content-between  mt-0 pt-0 bg-dark p-2"> 
+
+      <text class="">
+			<a href="/nested/aboutUs.html" class="m-1   text-decoration-none btn-floating "  role="button">
+				&nbsp;&nbsp;&nbsp;&nbsp;	<i class="text-white  text-decoration-none">123hora?</i>
+			</a>
+	  </text>
+
+      <text class="">
+			<a HREF="/nested/professionalAccess.html" class=" m-1  text-decoration-none btn-floating "  role="button">
+					<i class="text-white text-decoration-none">Profesionales</i>
+			</a>
+	  </text>
+
+	  <text>
+			<a HREF="/nested/register.html" class=" text-decoration-none btn-floating m-1"  role="button">
+					<i class="text-white text-decoration-none">Registrate</i>&nbsp;&nbsp;&nbsp;&nbsp;
+			</a>
+	  </text>
+
+
+<!--
+      <a HREF="/nested/getAppointments.html"  style="text-decoration: none;" class=" fw-lighter">
+-->
+	
+
+    </div>
+
+	</div>
+
+
+<!--
    <div class="fixed-bottom bg-light" >
 		
 			<div class=" text-center  ">
-			<!-- Grid container -->
+			
 			<div class="container p-0 pb-0">
-			<!-- Section: Social media -->
 			<section class="d-flex justify-content-between ">
-				<!-- button 1 -->
-				
+
 				<a href="/nested/aboutUs.html" class="m-1 text-secondary   text-decoration-none btn-floating "  role="button">
 				&nbsp;&nbsp;&nbsp;&nbsp;	<i class="text-primary">123hora?</i>
 				</a>
-				
-
-				<!-- button 1 -->
+		
 				<a HREF="/nested/professionalAccess.html" class=" m-1  text-secondary  text-decoration-none btn-floating "  role="button">
 					<i class="text-primary text-decoration-none">Profesionales</i>
 				</a>
-				<!-- button 1 -->
 				<a HREF="/nested/register.html" class="text-secondary  text-decoration-none btn-floating m-1"  role="button">
 					<i class="text-primary text-decoration-none">Registrate</i>&nbsp;&nbsp;&nbsp;&nbsp;
 				</a>
 			
 			</section>
-			<!-- Section: Social media -->
-			</div>
-			<!-- Grid container -->
-		
-			<!-- Copyright 
-			<div class="text-center p-1" style="background-color: rgba(0, 0, 0, 0.1);">
-			123Hora.cl © 2021 
-			</div>
-			-->
 			
-			<!-- Copyright -->
+			</div>
+		
 		</div>
 
 
 	</div>
-
+-->
 </template>
 
 <style scoped>
