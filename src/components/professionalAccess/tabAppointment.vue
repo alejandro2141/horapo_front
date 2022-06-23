a973d83dfd68c61be35c5611811ab003acceda42
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-06-23 17:54:20 -0400

    Update tabAppointment.vue

--- src/components/professionalAccess/tabAppointment.vue
+++ src/components/professionalAccess/tabAppointment.vue
@@ -19,7 +19,7 @@ import loadProgress from '../loadProgress.vue'
           <!-- <DateRequiredActions :daterequired="daterequired" ></DateRequiredActions> --> <br>
            <ListAppointments  v-on:updateAppointmentList="updateAppointmentList" v-if="session_params" :daterequired="daterequired" :appointments="appointments" :calendars_marks="calendars_marks" :session_params="session_params" v-on:switchView='switchView' :global_specialties='global_specialties' :global_comunas="global_comunas" ></ListAppointments>
 
-            <div id='footer' style='height : 800px'>
+            <div id='footer' style='height : 300px'>
             </div>
 	    </div>
 
