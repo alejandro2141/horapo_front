3d250705153f6c58335a951ff304c4fec5d575ee
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-06-24 19:09:19 -0400

    Update modalViewCenterDetails.vue

--- src/components/professionalAccess/modalViewCenterDetails.vue
+++ src/components/professionalAccess/modalViewCenterDetails.vue
@@ -12,11 +12,101 @@ import inputFormComuna from  '../publicSearch/InputFormComuna.vue'
 		    <transition name="modal">
 			<div class="modal-mask "   >
 			<div class="modal-wrapper ">
-			<div class="modal-container  m-1 p-1 modal-background">
+			<div class="modal-container  m-1 p-1  bg-dark">
  
-
                 <div class="modal-body mt-0"  > 
-                   
+
+                        <div class="card m-4  " style=" border-radius: 15px;"  >
+                        <div class="card-body "  >
+                            
+                              <div class="d-flex justify-content-between  m-1">
+                                <div class="fs-5 mt-1"  >
+                                    <i class="text-muted bi bi-geo-alt"></i> {{centerToShowDetails.name}} 
+                                </div>
+                                <div class="">
+                                    <i class="text-primary display-1 bi bi-x-lg ml-0"  v-on:click="showModalCenterDetails = false" aria-label="Close"></i> 
+                                </div>
+                              </div>
+
+                                <div >
+                                    <!-- CENTER VISIT -->
+                                    <div v-if="centerToShowDetails.center_visit" >
+                                      <!--
+                                        <div class="d-flex justify-content-between ">
+                                                <text class="h5"><i class="text-muted bi bi-geo-alt"></i> {{centerToShowDetails.name}} </text>
+                                                <text class="display-4"></text>  
+                                        </div>
+                                      -->
+                                        <hr class="m-0">
+                                        
+                                        <div class="d-flex justify-content-between mt-0">
+                                                <text class="mt-2">Tipo:</text>
+                                                <text class="text-end"> En Consulta <i class="text-muted h1 bi bi-building"></i> </text>
+                                        </div>
+                                        
+                                        <p class="mt-3">
+                                            <text >Direccion: </text><br>
+                                            <text class="text-end">{{ comunaId2Name(centerToShowDetails.comuna)  }}, {{centerToShowDetails.address}} </text>
+                                        </p>
+
+                                    </div>
+
+                                    <!-- A DOMICILIO -->
+                                    <div v-if="centerToShowDetails.home_visit" >
+
+                                            
+                                            <hr class="m-0">
+                                            <div class="d-flex justify-content-between mt-2">
+                                                <text class="mt-2">Tipo : </text>
+                                                <text></text>
+                                                <text class="text-end"> A Domicilio <i class="text-muted h1 bi bi-house-door"></i> </text>
+                                                
+                                            </div>
+                                           
+                                            <div class="d-flex justify-content-between mt-3">
+                                                    <text> En Comunas: </text>         
+                                                    <text></text>
+                                                    <text class="text-start">
+                                                        <text v-if="comunaId2Name(centerToShowDetails.home_comuna1)!=null" > {{ comunaId2Name(centerToShowDetails.home_comuna1)  }} &nbsp; <br></text> 
+                                                        <text v-if="comunaId2Name(centerToShowDetails.home_comuna2)!=null"> {{ comunaId2Name(centerToShowDetails.home_comuna2)  }} &nbsp;  <br></text> 
+                                                        <text v-if="comunaId2Name(centerToShowDetails.home_comuna3)!=null"> {{ comunaId2Name(centerToShowDetails.home_comuna3)  }} &nbsp; <br></text> 
+                                                        <text v-if="comunaId2Name(centerToShowDetails.home_comuna4)!=null"> {{ comunaId2Name(centerToShowDetails.home_comuna4)  }} &nbsp; <br></text> 
+                                                        <text v-if="comunaId2Name(centerToShowDetails.home_comuna5)!=null"> {{ comunaId2Name(centerToShowDetails.home_comuna5)  }} &nbsp; <br></text> 
+                                                        <text v-if="comunaId2Name(centerToShowDetails.home_comuna6)!=null"> {{ comunaId2Name(centerToShowDetails.home_comuna6)  }} &nbsp; <br></text> 
+                                                    </text>
+                                                    
+                                             </div>
+                                    </div>
+
+                                    <!-- REMOTE CARE  -->
+                                    <div v-if="centerToShowDetails.remote_care" >
+                                        
+                                         <hr class="m-0">
+                                        <div class="d-flex justify-content-between mt-0">
+                                            <text class="mt-2"> Tipo: </text>
+                                            <text> </text>
+                                            <text class="text-end"> Tele Atención <i class="text-muted h1 bi bi-camera-video"></i></text>
+                                            
+                                        </div>
+                                        <div class="d-flex justify-content-between mt-2">
+                                            <text> En Telefono: </text>
+                                          
+                                            <text> 9999999</text>
+                                        </div>
+                                    </div>
+                                
+                                </div>
+
+
+                               <p  @click="removeCenter(centerToShowDetails)" class="text-danger text-end m-3">Eliminar</p>
+
+                                   
+                         <!--   <p class="text-end text-muted mt-2"> #{{ center.id }} </p> -->
+                        </div>
+                    </div>
+
+
+                    <!--
                      <div class="d-flex flex-row justify-content-end  m-1">
                       <div class="display-5 col-9" style="margin-right: 1em;" > {{centerToShowDetails.name}} </div>
                       <div class=""><i class="display-1 bi bi-x-lg ml-0"  v-on:click="showModalCenterDetails = false" aria-label="Close"></i> </div>
@@ -28,10 +118,10 @@ import inputFormComuna from  '../publicSearch/InputFormComuna.vue'
                         <p>{{centerToShowDetails.comuna}}</p>
                     </div>
                     <p  @click="removeCenter(centerToShowDetails)" class="text-danger">Eliminar</p>
+                    -->
 
-
-                    
                 </div>
+
             </div> 
             </div> 		
             </div> 
@@ -44,15 +134,40 @@ import inputFormComuna from  '../publicSearch/InputFormComuna.vue'
 
 
 <style scoped>
+
 .modal {
-  position: absolute;
-  display: flex;
+   /*position: static;  */
+   /*position: static; */
+	/*position: relative; */ 
+	/*position: absolute; */ 
+	position: fixed; 
+	/*position: sticky; */
+ /* position: fixed;  */
+ /* display: block; */ 
+   display: flex; 
+
 }
 
+/*
 .modal div {
-  display: flex;
-  flex-direction: column;
+  display: flex; 
+  flex-direction: column; 
+
 }
+*/
+ checkbox {width: 30px; height: 30px;}
+
+div.scroll {
+       			margin:4px, 4px;
+                padding:4px;
+               /* background-color: green; */
+                width: 100%; 
+                /* height: 190%;*/
+                overflow-x: auto;
+                overflow-y: auto;
+                text-align:justify;
+      }
+
 /*
 .modal-background {
     background-color:#DAEFF3
@@ -63,14 +178,14 @@ import inputFormComuna from  '../publicSearch/InputFormComuna.vue'
 
 
 .modal-mask {
-  position: fixed;
+  /*position: fixed;*/
   z-index: 9998;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
-  display: table;
+ /* display: table;*/
 }
 
 .modal-wrapper {
@@ -112,7 +227,6 @@ import inputFormComuna from  '../publicSearch/InputFormComuna.vue'
   opacity: 0;
 }
 
-/************************ */
 </style>
 
 
@@ -152,6 +266,29 @@ data: function () {
                 this.$emit("updateCenterList"); 
                 this.showModalCenterDetails = false ;
             },
+
+          comunaId2Name(comuna_id)
+          {
+            if (comuna_id != null)
+            {
+            let aux = this.global_comunas.find(o => o.id === comuna_id)
+                if (aux != null)
+                {
+                    return  aux.name
+                }
+                else
+                {
+                    return "not found" ;  
+                }
+            }
+            else
+            {
+            return null ; 
+            }
+
+          },
+
+
 
         },
     
