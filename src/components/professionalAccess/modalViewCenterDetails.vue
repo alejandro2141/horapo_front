a379ddc37b23949b0914889796ef15922985b76e
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-06-27 01:49:12 -0400

    Update modalViewCenterDetails.vue

--- src/components/professionalAccess/modalViewCenterDetails.vue
+++ src/components/professionalAccess/modalViewCenterDetails.vue
@@ -16,12 +16,15 @@ import inputFormComuna from  '../publicSearch/InputFormComuna.vue'
  
                 <div class="modal-body mt-0"  > 
 
-                        <div class="card m-4  " style=" border-radius: 15px;"  >
+                        <div class="card m-1  " style=" border-radius: 15px;"  >
                         <div class="card-body "  >
                             
                               <div class="d-flex justify-content-between  m-1">
                                 <div class="fs-5 mt-1"  >
-                                    <i class="text-muted bi bi-geo-alt"></i> {{centerToShowDetails.name}} 
+                                    <i class="text-muted bi bi-geo-alt"></i> Modificar &nbsp; Consulta
+                                </div>
+                                <div>
+                                  &nbsp;&nbsp;&nbsp;
                                 </div>
                                 <div class="">
                                     <i class="text-primary display-1 bi bi-x-lg ml-0"  v-on:click="showModalCenterDetails = false" aria-label="Close"></i> 
@@ -38,24 +41,51 @@ import inputFormComuna from  '../publicSearch/InputFormComuna.vue'
                                         </div>
                                       -->
                                         <hr class="m-0">
-                                        
+                                        <div class="d-flex justify-content-between mt-4">
+                                          <p>
+                                                <text class="">Nombre:</text><br>
+                                                <input type="text" class="form-control form-control-lg border pl-2 border-primary" id="form_name" name="form_name" :value="centerToShowDetails.name" style="z-index: 9; padding-left: 40px; border-radius: 25px;">
+                                          </p>
+                                        </div>
+
                                         <div class="d-flex justify-content-between mt-0">
                                                 <text class="mt-2">Tipo:</text>
                                                 <text class="text-end"> En Consulta <i class="text-muted h1 bi bi-building"></i> </text>
-                                        </div>
+                                        </div>          
                                         
                                         <p class="mt-3">
                                             <text >Direccion: </text><br>
-                                            <text class="text-end">{{ comunaId2Name(centerToShowDetails.comuna)  }}, {{centerToShowDetails.address}} </text>
+                                           <!-- <text class="text-end"> {{centerToShowDetails.address}} , {{ comunaId2Name(centerToShowDetails.comuna)  }} </text> -->
+                                            <input type="text" class="form-control form-control-lg border pl-2 border-primary" id="form_address" name="form_address" :value="centerToShowDetails.address" style="z-index: 9; padding-left: 40px; border-radius: 25px;">
+
                                         </p>
+
+                                        
+                                         <div v-if="centerToShowDetails.phone1 != 'null' " class="d-flex justify-content-between mt-2">
+                                            <text class="">Telefono <i class="text-muted h3 bi bi-telephone"></i>:</text>
+                                            <input type="text" class="form-control form-control-lg  border pl-2 border-primary" id="form_phone1" name="form_phone1" :value="centerToShowDetails.phone1" style="z-index: 9; padding-left: 40px; border-radius: 25px;">
+                                         </div>
+
+                                         <div v-if="centerToShowDetails.phone2 != 'null' " class="d-flex justify-content-between mt-2">
+                                            <text class="">Telefono <i class="text-muted h3 bi bi-telephone"></i>:</text>
+                                            <input type="text" class="form-control form-control-lg  border pl-2 border-primary" id="form_phone2" name="form_phone2" :value="centerToShowDetails.phone2" style="z-index: 9; padding-left: 40px; border-radius: 25px;">
+                                         </div>
+
 
                                     </div>
 
                                     <!-- A DOMICILIO -->
                                     <div v-if="centerToShowDetails.home_visit" >
 
-                                            
                                             <hr class="m-0">
+
+                                            <div class="d-flex justify-content-between mt-4">
+                                              <p>
+                                                    <text class="">Nombre:</text><br>
+                                                    <input type="text" class="form-control form-control-lg border pl-2 border-primary" id="form_name" name="form_name" :value="centerToShowDetails.name" style="z-index: 9; padding-left: 40px; border-radius: 25px;">
+                                              </p>
+                                            </div>
+
                                             <div class="d-flex justify-content-between mt-2">
                                                 <text class="mt-2">Tipo : </text>
                                                 <text></text>
@@ -76,28 +106,50 @@ import inputFormComuna from  '../publicSearch/InputFormComuna.vue'
                                                     </text>
                                                     
                                              </div>
+
+                                            <div v-if="centerToShowDetails.phone1 != 'null' " class="d-flex justify-content-between mt-2">
+                                                <text class="">Telefono 1:</text>
+                                                <text class="text-end"><i class="text-muted h3 bi bi-telephone"></i> {{centerToShowDetails.phone1 }}  </text>
+                                            </div>
+
+                                            <div v-if="centerToShowDetails.phone1 != 'null' " class="d-flex justify-content-between mt-2">
+                                              <text class="">Telefono 2:</text>
+                                              <text class="text-end"><i class="text-muted h3 bi bi-telephone"></i> {{centerToShowDetails.phone2 }}  </text>
+                                            </div>
+
                                     </div>
 
                                     <!-- REMOTE CARE  -->
                                     <div v-if="centerToShowDetails.remote_care" >
                                         
                                          <hr class="m-0">
+                                        <div class="d-flex justify-content-between mt-4">
+                                          <p>
+                                                <text class="">Nombre:</text><br>
+                                                <input type="text" class="form-control form-control-lg border pl-2 border-primary" id="form_name" name="form_name" :value="centerToShowDetails.name" style="z-index: 9; padding-left: 40px; border-radius: 25px;">
+                                          </p>
+                                        </div>
+
                                         <div class="d-flex justify-content-between mt-0">
                                             <text class="mt-2"> Tipo: </text>
                                             <text> </text>
                                             <text class="text-end"> Tele Atención <i class="text-muted h1 bi bi-camera-video"></i></text>
                                             
                                         </div>
-                                        <div class="d-flex justify-content-between mt-2">
-                                            <text> En Telefono: </text>
-                                          
-                                            <text> 9999999</text>
-                                        </div>
+                                       
+                                          <div v-if="centerToShowDetails.phone1 != 'null' " class="d-flex justify-content-between mt-2">
+                                            <text class="">Telefon de Atención <i class="text-muted h3 bi bi-telephone"></i>:</text>
+                                            <input type="text" class="form-control form-control-lg  border pl-2 border-primary" id="form_phone1" name="form_phone1" :value="centerToShowDetails.phone1" style="z-index: 9; padding-left: 40px; border-radius: 25px;">
+                                           </div>
+
                                     </div>
                                 
                                 </div>
 
-
+                              <p class="text-center mt-4">
+                                <text type="button" class="btn btn-primary" > Guardar los cambios </text>
+                              </p>
+<hr>
                                <p  @click="removeCenter(centerToShowDetails)" class="text-danger text-end m-3">Eliminar</p>
 
                                    
