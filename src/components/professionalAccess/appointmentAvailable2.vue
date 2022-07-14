ed7c27d65ed0a5e89d334eb9ecb01ced32b65e8e
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-07-14 19:03:43 -0400

    Update appointmentAvailable2.vue

--- src/components/professionalAccess/appointmentAvailable2.vue
+++ src/components/professionalAccess/appointmentAvailable2.vue
@@ -5,35 +5,30 @@ import axios from 'axios';
 </script>
 
 <template>
-
                     <div class="d-flex justify-content-between m-1"  style="background-color: #D4D4D4;  border-radius: 15px;">
                     
                         <div class="p-2"  >
                             <text class="fs-3 fw-light"> {{ appointment.start_time.substring(0,5) }} </text><br>
                             <text class="fs-3 fw-light text-muted" >
-                                <i v-if="appointment.center_visit" class=" bi bi-building"></i>      
-                                <i v-if="appointment.home_visit"  class=" bi bi-house-door" > </i>                                  
-                                <i v-if="appointment.remote_care" class=" bi bi-camera-video"></i>                         
+                                <i v-if="center_data.center_visit" class=" bi bi-building"></i>      
+                                <i v-if="center_data.home_visit"  class=" bi bi-house-door" > </i>                                  
+                                <i v-if="center_data.remote_care" class=" bi bi-camera-video"></i>                         
                             </text> 
                         </div>
                        
-                                <div  class="w-100 text-white display-6 p-2" :style="[ {'background-color' : appointment.calendar_color} , days_expired==true ? { 'background-color': '#444' }:{ 'opacity': '1' }  ]"  style="border-radius: 15px;"  >
+                                <div  class="w-100 text-white display-6 p-2" :style="[ {'background-color' : calendar_data.color} , (days_expired || appointment.lock_day ) ? { 'background-color': '#444' }:{ 'opacity': '1' }  ]"  style="border-radius: 15px;"  >
                                     
                                     <div class=""  >
-                                        <text class="" ><small> {{specialtyId2name(appointment.specialty)}} </small> </text>
+                                        <text class="" ><small>  {{specialtyId2name(appointment.specialty)}} </small> </text>
                                     </div>  
                                 
                                     <div class="">
                                         <text class="" > 
-                                            <small> <small> {{ getCenterName(appointment.center_id) }} </small> </small>
+                                            <small> <small> {{this.center_data.name}} </small> </small>
                                         </text>
                                     </div>
-                                    
-                                </div>
-                        
-
+                                </div>                        
                     </div>
-
 </template>
 
 <style scoped>
@@ -45,13 +40,29 @@ import axios from 'axios';
 export default {
    data : function() {
         return {
+            center_data : null , 
+            calendar_data : null,
         }   
     },
    	
     props: ['days_expired','daterequired','appointment','index','global_specialties', 'global_comunas', 'session_params' ],
 
 	created () {
-	},
+        this.center_data = this.session_params.centers.find(elem => elem.id == this.appointment.center_id )
+        this.calendar_data = this.session_params.calendars.find(elem => elem.id == this.appointment.calendar_id )
+    },
+    
+    /*
+    watch : {
+
+        appointment(newVal)
+        {
+           this.center_data = this.session_params.centers.find(elem => elem.id == newVal.center_id )
+           console.log("New Appointment, get CENTER:"+JSON.stringify(this.center_data));
+        }
+    },
+    */
+
 	methods :{
       	
        specialtyId2name(id){
@@ -65,6 +76,12 @@ export default {
             if (temp != null) { return temp.name }
             else { return "No Existe Centro" }
         },
+        getCenterData(id){
+            let temp= this.session_params.centers.find(elem => elem.id ==  id  )
+            if (temp != null) { return temp }
+            else { return "No Existe Centro" }
+        },
+       
        
 
     }
