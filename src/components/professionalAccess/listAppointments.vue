1978cd12740282de1ff1500a2e3493e888c708c1
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-07-29 13:29:15 -0400

    Update listAppointments.vue

--- src/components/professionalAccess/listAppointments.vue
+++ src/components/professionalAccess/listAppointments.vue
@@ -110,7 +110,6 @@ export default {
    data : function() {
         return {
             hours : [] ,
-            //hr:'01', '02', '03', '04', '05','06','07','08','09','10', '11','12','13','14','15','16','17','18','19','20','21','22','23' ],
             hourCreate : null ,
             hourDetails : null ,
             hourTaken : null ,
