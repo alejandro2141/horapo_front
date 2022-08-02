54d2505999ad48b4ddc22f9893c813e75d41b99d
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-08-02 19:22:13 -0400

    Update lockOptions.vue

--- src/components/professionalAccess/lockOptions.vue
+++ src/components/professionalAccess/lockOptions.vue
@@ -8,10 +8,16 @@ import axios from 'axios';
 
 <template>
         <div class="align-self-end p-2" >
-           <i class=" m-2 fs-2 bi bi-unlock" @click="showLockOptions=!showLockOptions"></i>
+           <!-- <i class=" m-2 fs-2 bi bi-unlock" @click="showLockOptions=!showLockOptions"> </i> -->
+            <div class="text-primary"  >    
+              <i v-if="isLockDay" class=" m-2 fs-2 bi bi-lock-fill " @click="sendUnLockDay()"> </i> 
+              <i v-else class=" m-2 fs-2 bi bi-unlock "  @click="sendLockDay()"> </i> 
+            </div>
+
+
            <Transition>
             <div v-if="showLockOptions">
-                Bloquear
+               
               <hr>
              <text @click="sendLockDay()" >Dia </text>
               <hr>
@@ -40,12 +46,14 @@ export default {
    data : function() {
         return {
               showLockOptions : false ,
+              isLockDay : false ,
         }   
     },
-    props : ['session_params','daterequired'] ,
+  props : [ 'lock_dates', 'session_params','daterequired'] ,
 	emits : ['updateAppList']  ,
 
  	mounted () {
+         
     },
 
     methods: {
@@ -56,7 +64,7 @@ export default {
             async  sendLockDay(hour)
             {
                 console.log("professional_lock_day");
-                var r =confirm("Pacientes no podran agendar horas en este dia ?");
+                var r =confirm("Esta seguro que desea bloquear este dia? Pacientes no podran agendar horas en este dia");
                             if (r == true) {
                                 const json = {  
                                     token: 'apsfdnwe',                         
@@ -70,11 +78,53 @@ export default {
                                 console.log ("RESPONSE professional_lock_day :"+JSON.stringify(response_json.data)) ;
                                 this.appointment_confirm = response_json.data ;
                                 //console.log ("We should display a Confirmation Modal now"+JSON.stringify(appointment_confirm) );
-                                this.$emit('updateAppList');
+                                this.$emit('updateAppointmentList');
+                                }
+            },
+
+            async  sendUnLockDay(hour)
+            {
+                console.log("professional_UN lock_day");
+                var r =confirm("DESBLOQUEAR este dia? Pacientes SI podrán agendar horas en este dia");
+                            if (r == true) {
+                                const json = {  
+                                    token: 'apsfdnwe',                         
+                                    appointment_date : this.daterequired , 
+                                    appointment_professional_id  : this.session_params.professional_id 	, 
+                                };
+
+                                console.log ("professional_lock_day  REQUEST :"+ JSON.stringify(json)  );
+                                let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_unlock_day",json );
+                                //console.log ("RESPONSE save_appointmentJSON.stringify(response_json) :"+JSON.stringify(response_json)) ;
+                                console.log ("RESPONSE professional_lock_day :"+JSON.stringify(response_json.data)) ;
+                                this.appointment_confirm = response_json.data ;
+                                //console.log ("We should display a Confirmation Modal now"+JSON.stringify(appointment_confirm) );
+                                this.$emit('updateAppointmentList');
                                 }
             },
 
         },
+  
+  watch :
+  {
+    daterequired(newVal,oldVal)
+    {
+    
+       console.log("Check if DATE:"+newVal+" is in LockDates"+JSON.stringify(this.lock_dates))
+
+          if (this.lock_dates!=null)
+          {
+            if (this.lock_dates.includes(this.daterequired))
+            {this.isLockDay=true}
+            else{
+              this.isLockDay=false
+            }
+          }     
+
+
+    }
+
+  },
 
 }
 
