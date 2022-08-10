0540537bfacf1a995c561393ae116abaa26b3839
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-08-10 18:33:44 -0400

    Update SearchAppointmentForm.vue

--- src/components/publicSearch/SearchAppointmentForm.vue
+++ src/components/publicSearch/SearchAppointmentForm.vue
@@ -12,16 +12,13 @@ import inputFormComuna  from './InputFormComuna.vue'
             <div id="formSearch" class="mx-auto "  >
                 <form autocomplete="off" >
 
-
                 <input  v-model="form_token" id="form_token" name="form_token" type="hidden"  > 
-
 <!--FORM INPUT SPECIALTY -->                   
                 <div v-if="show_input_specialty()"  class="w-100 mb-1"  >
                     <div class="">
                         <inputFormSpecialty  v-on:selectedSpecialtyCode="selectedSpecialtyCode" :global_specialties="global_specialties" > </inputFormSpecialty> 
                     </div>
-                </div>
-               
+                </div>           
 
 <!-- FORM INPUT LOCATION-->
                 <div  v-if="show_input_date()"  class="row  mb-1   "  >
@@ -37,7 +34,6 @@ import inputFormComuna  from './InputFormComuna.vue'
                     </div>
                 </div>
                  
-
 <!--FORM INPUT  APP TYPE -->
                 <div v-if="show_app_type()" class="d-flex justify-content-around   mb-1" style=" border-radius: 15px;"  >
                     
@@ -55,65 +51,9 @@ import inputFormComuna  from './InputFormComuna.vue'
                        <i class="h5 bi bi-camera-video m-0 p-0" style="color:#b36b00;"  ></i>
                        <text> Remota </text> 
                     </text>
-
-                    <!--
-                    <button type="button" @click="selectedTypeCenter()" class="m-1 p-1  btn btn-outline-primary border-1 " :class="{ 'active' : form_app_type_center }" style=" border-radius: 15px;" > 
-                        <i  class="h5 bi bi-building m-0 p-0" style="color: #781ED1;"></i><br>
-                       <text class="m-0 p-0"> Consulta </text>  
-                    </button>
-                    
-                    <button type="button" @click="selectedTypeHome()"  class="m-1 p-1 btn btn-outline-primary border-1 " :class="{ 'active' : form_app_type_home }" style=" border-radius: 15px;" > 
-                        <i class="h5 bi bi-house-door m-0 p-0" style="color:#3399FF;"></i><br>
-                        <text> Domicilio </text> 
-                    </button>
-
-                    <button type="button" @click="selectedTypeRemote()"  class="m-1 p-1 btn btn-outline-primary border-1" :class="{ 'active' : form_app_type_remote }"  style=" border-radius: 15px; " > 
-                       <i class="h5 bi bi-camera-video m-0 p-0" style="color:#b36b00;"  ></i><br>
-                       <text> Remota </text> 
-                    </button>
-                    -->
                    
                 </div>
 
-            <!--
-                <button type="button" @click="form_app_type_center=!form_app_type_center ; form_app_type_home= false" class=" m-1 btn"  :class="{ 'btn-primary' : form_app_type_center , 'btn-secondary' : !form_app_type_center , 'btn-lg' : form_app_type_center }" >  En Consulta  </button>
-                <button type="button" @click="form_app_type_home=!form_app_type_home ; form_app_type_center= false "     class=" m-1 btn " :class="{ 'btn-primary' : form_app_type_home   , 'btn-secondary' : !form_app_type_home , 'btn-lg' : form_app_type_home     }" > A Domicilio </button>
-            --> 
-                <!--
-                <div class="bg-white  ">
-                    <div class="d-flex justify-content-around h5  "> 
-            
-                        <div><i class="bi bi-building"></i>En Consulta </div>
-                        <div><i class="bi bi-house"></i>A Domicilio </div>
-                    </div>
-                    <div class="d-flex justify-content-around h1 m-0 p-0 "> 
-                         <div><input class="form-check-input border-primary" type="checkbox" id="inlineCheckbox1" value="true" v-model="form_app_type_center"></div>
-                        <div><input class="form-check-input border-primary" type="checkbox" id="inlineCheckbox1" value="true" v-model="form_app_type_home"></div>
-                    </div>
-                
-                </div>
-                -->
-                <!--
-                <div @click="form_home_visit = !form_home_visit"  class="bg-white  border border-primary text-primary rounded d-flex justify-content-between form-control form-control-lg border">
-                    <div>Visita a Domicilio <i class="bi bi-house-door"></i> </div>
-                    <div></div>
-                    <div v-if="form_home_visit != null"> 
-                        <div v-if="form_home_visit"  > <b> SI </b> </div>
-                        <div v-if="!form_home_visit" > <b> NO </b> </div>
-                    </div>
-                    <div v-else> 
-                        ---
-                    </div>
-                </div>
-                -->
-
-                
-                <!--
-                <div class="text-center">
-                <button type="button" v-on:click="sendFormSearch()" class="btn btn-primary btn-lg m-2">  <i class="display-6  bi bi-search   m-0"  ></i> Buscar una Hora</button>
-                </div>
-                -->
-             <!--       <i  type="submit" v-on:click="sendFormSearch()" class="btn  btn-lg btn-block text-white bg-dark " style="width: 100%;"  >{{ search_button_message }}</i>-->
                 </form> 
                
             </div>	
@@ -149,24 +89,16 @@ export default {
             form_app_type_home : false ,
             form_app_type_center : false ,
             form_app_type_remote : false ,
-
-            
-
-           // form_date : "30/12/2021" , 
     }
   },
 
  props: [ 'global_specialties','global_comunas', 'currentDate','n_app_filtered' ], 
  emits: [ 'searchBySpecialty','searchByTypeCenter','searchByTypeHome' ,'searchByTypeRemote' ,'searchByLocation' ,'searchByDate' ],
  
-
-
    created () {    
         this.form_minimum_date = new Date().toISOString().split('T')[0] ;
         this.form_current_date = new Date().toISOString().split('T')[0] ; 
         },
-
- 
 
     methods: {
 
@@ -195,8 +127,6 @@ export default {
             else
             { return false }
         },
-
-
 
 //SELECTED IN FORM TO EMIT  FOR SEARCH 
         //SELECTED SPECIALTY comes from V-ON event component
@@ -286,32 +216,11 @@ export default {
                         location : this.form_location_code ,
                         date :  date ,  
                   		  };
+            if (this.form_specialty != null)
+            {
             this.$emit("searchByDate",search_params );
-        },
-       
-/*
-        //SEND FORM 
-        sendFormSearch(){
-           
-            const search_params = { 
-				// agenda_id : this.par_agenda_id ,			 
-				 specialty : this.form_specialty ,
-                 comuna : this.form_comuna_code ,
-                 type_home : this.form_app_type_home,
-                 type_center : this.form_app_type_center,
-                 type_remote : this.form_app_type_remote,
-                 date :  this.form_current_date ,  
-                  		  };
-            
-            this.$emit("searchAppointments",search_params );
-
-                this.form_app_type_home = false ; 
-                this.form_app_type_center = false ; 
-                this.form_app_type_remote = false ; 
-
             }
-            */
-            
+        },          
 
         },
     
@@ -324,21 +233,7 @@ export default {
             {  
                 this.selectedDate(newValue)
             }
-        },
-        
-        /*
-        form_app_type_center(newValue){
-            this.$emit("form_app_type_center_emit",newValue );
-        },
-         
-        form_app_type_home(newValue){
-            this.$emit("form_app_type_home_emit",newValue );
-        },
-         
-        form_app_type_remote(newValue){
-            this.$emit("form_app_type_remote_emit",newValue );
-        },
-        */  
+        },  
 
     },
 
