600869a4116b95caf04dbf35878401dd2ca020ae
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-08-19 18:12:37 -0400

    Update appointmentAvailable2.vue

--- src/components/professionalAccess/appointmentAvailable2.vue
+++ src/components/professionalAccess/appointmentAvailable2.vue
@@ -7,7 +7,7 @@ import axios from 'axios';
 <template>
 
 
-                    <div class="d-flex justify-content-between"  :class="{ 'border border-3 border-primary': selectApp }"  style="background-color: #D4D4D4;  border-radius: 15px;">
+                    <div class="d-flex justify-content-start border border-3"  :class="{ 'border-primary': selectApp }"  style="background-color: #E4E4E4;  border-radius: 15px;">
                     
                         <div @click="selectApp=!selectApp; $emit('addToBlockList',appointment) " class="p-2 "  >
                             <text class="fs-3 fw-light"> {{ appointment.start_time.substring(0,5) }} </text><br>
@@ -17,8 +17,9 @@ import axios from 'axios';
                                 <i v-if="center_data.remote_care" class=" bi bi-camera-video"></i>                         
                             </text> 
                         </div>
-                       
-                                <div @click="$emit('displayModalAppAvailable', center_data )" class="w-100 text-white display-6 p-2" :style="[ {'background-color' : calendar_data.color} , (days_expired ) ? { 'background-color': '#444'}:{ } , (appointment.lock_day ) ? { 'background-color': '#444', 'background' : 'repeating-linear-gradient( -45deg, #000, #888 1px, #333 5px, #333 10px )' }:{ 'opacity': '1' }  ]"  style="border-radius: 15px;"  >
+
+                        <div class="d-flex justify-content-between w-100" :style="[ {'background-color' : calendar_data.color} ]"  style="border-radius: 15px;" >       
+                                <div @click="selectApp=!selectApp; $emit('addToBlockList',appointment) " class="w-100 text-white display-6 p-2" :style="[ {'background-color' : calendar_data.color} , (days_expired ) ? { 'background-color': '#444'}:{ } , (appointment.lock_day ) ? { 'background-color': '#444', 'background' : 'repeating-linear-gradient( -45deg, #000, #888 1px, #333 5px, #333 10px )' }:{ 'opacity': '1' }  ]"  style=""  >
                                     
                                     <div class=""  >
                                         <text class="" ><small>  {{specialtyId2name(appointment.specialty)}} </small> </text>
@@ -29,7 +30,13 @@ import axios from 'axios';
                                             <small> <small> {{center_data.name}} </small> </small>
                                         </text>
                                     </div>
-                                </div>                        
+                                </div>    
+                                
+                                <div @click="$emit('displayModalAppAvailable', center_data )" >
+                                    <i style="font-size: 60px; padding:0px ; margin:-5px ; border:0px" class="bi bi-chevron-compact-right text-white" ></i> 
+                                </div>   
+                        </div>           
+
                     </div>
 </template>
 
