9ee7f3cd37fb277211e26429a5dba53e694913ff
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-09-05 19:19:53 -0300

    Update appointmentAvailable2.vue

--- src/components/professionalAccess/appointmentAvailable2.vue
+++ src/components/professionalAccess/appointmentAvailable2.vue
@@ -18,7 +18,7 @@ import axios from 'axios';
                         </div>
 
                         <div class="d-flex justify-content-between w-100"  :style="[ {'background-color' : calendar_data.color} , (days_expired ) ? { 'background-color': '#444'}:{ } , (appointment.lock_day ) ? { 'background-color': '#444', 'background' : 'repeating-linear-gradient( -45deg, #000, #888 1px, #333 5px, #333 10px )' }:{ 'opacity': '1' }  ]"  style="border-top-right-radius: 25px;" >       
-                                <div @click="selectApp=!selectApp; $emit('addToBlockList',appointment) " class="w-100 text-dark display-6 p-2"  style=""  >
+                                <div @click="selectApp=!selectApp; $emit('addToBlockList',appointment) " class="w-100 text-dark display-6 p-2" :class="{ 'text-white' : appointment.lock_day}"  style=""  >
                                     
                                     <div class=""  >
                                         <text class="" ><small>  {{specialtyId2name(appointment.specialty)}} </small> </text>
