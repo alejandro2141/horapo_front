0d1832205a758dcde495599712de7b43f719b96c
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-09-27 21:41:45 -0300

    Update appointmentAvailable2.vue

--- src/components/professionalAccess/appointmentAvailable2.vue
+++ src/components/professionalAccess/appointmentAvailable2.vue
@@ -10,6 +10,7 @@ import axios from 'axios';
                     
                         <div @click="selectApp=!selectApp; $emit('addToBlockList',appointment) " class="p-2 "  >
                             <text class="fs-3 fw-light"> {{getLocalTime()}} </text><br>
+                              <text class="fs-3 fw-light"><small><small><small> <small><small> {{getLocalDate()}} </small></small></small></small></small></text><br>
                             <text class="fs-3 fw-light text-muted" >
                                 <i v-if="center_data.center_visit" class=" bi bi-building"></i>      
                                 <i v-if="center_data.home_visit"  class=" bi bi-house-door" > </i>                                  
@@ -17,7 +18,7 @@ import axios from 'axios';
                             </text> 
                         </div>
 
-                        <div class="d-flex justify-content-between w-100"  :style="[ {'background-color' : calendar_data.color} , (days_expired ) ? { 'background-color': '#444'}:{ } , (appointment.lock_day ) ? { 'background-color': '#444', 'background' : 'repeating-linear-gradient( -45deg, #000, #888 1px, #333 5px, #333 10px )' }:{ 'opacity': '1' }  ]"  style="border-top-right-radius: 25px;" >       
+                        <div class="d-flex justify-content-between w-100"  :style="[ {'background-color' : calendar_data.color} , (days_expired ) ? { 'background-color': '#eee'  }:{ } , (appointment.lock_day ) ? { 'background-color': '#444', 'background' : 'repeating-linear-gradient( -45deg, #000, #888 1px, #333 5px, #333 10px )' }:{ 'opacity': '1' }  ]"  style="border-top-right-radius: 25px;" >       
                                 <div @click="selectApp=!selectApp; $emit('addToBlockList',appointment) " class="w-100 text-dark display-6 p-2" :class="{ 'text-white' : appointment.lock_day}"  style=""  >
                                     
                                     <div class=""  >
@@ -83,6 +84,15 @@ export default {
         this.hours   = new String(date.getHours())
         this.minutes =  new String(date.getMinutes())
         return  (this.hours.padStart(2,0)+":"+this.minutes.padStart(2,0))
+       // return ( this.hours.padStart(2,0)+":"+this.minutes.padStart(2,0)+" "+date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear() )
+        },
+        getLocalDate()
+        {
+        let date = new Date(this.appointment.start_time)
+        this.hours   = new String(date.getHours())
+        this.minutes =  new String(date.getMinutes())
+        //return  (this.hours.padStart(2,0)+":"+this.minutes.padStart(2,0))
+        return (date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear() )
         }
        
 
