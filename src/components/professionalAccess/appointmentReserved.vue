b73df9e73cd11069b078727846c0c5f4d29a96b6
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-10-18 19:08:40 -0300

    Update appointmentReserved.vue

--- src/components/professionalAccess/appointmentReserved.vue
+++ src/components/professionalAccess/appointmentReserved.vue
@@ -19,7 +19,7 @@ import axios from 'axios';
                             <text> <i style="color:#5BA199" class="display-3 bi bi-person-fill"></i>    </text> 
                         </div>
 
-                        <div class="d-flex justify-content-between w-100"  :style="[ {'background-color' : calendar_data.color} , (days_expired ) ? { 'background-color': '#444'}:{ } , (appointment.lock_day ) ? { 'background-color': '#444', 'background' : 'repeating-linear-gradient( -45deg, #000, #888 1px, #333 5px, #333 10px )' }:{ 'opacity': '1' }  ]"  style="border-top-right-radius: 25px;" >       
+                        <div class="d-flex justify-content-between w-100"  :style="[ {'background-color' : calendar_data.color} , (days_expired ) ? { 'background-color': '#eee'}:{ } , (appointment.lock_day ) ? { 'background-color': '#444', 'background' : 'repeating-linear-gradient( -45deg, #000, #888 1px, #333 5px, #333 10px )' }:{ 'opacity': '1' }  ]"  style="border-top-right-radius: 25px;" >       
                                 <div @click="selectApp=!selectApp; $emit('addToBlockList',appointment) " class="w-100 text-dark display-6 p-2"  style=""  >
                                     
                                     <div class=""  >
@@ -51,63 +51,6 @@ import axios from 'axios';
 
                     </div>
 
-<!--
-    <div class="d-flex justify-content-between m-1" style="background-color: #D4D4D4; border-bottom-left-radius: 25px; border-top-right-radius: 25px; " >
-                    
-        <div class="p-2">
-            <text class="fs-3 fw-light"> {{ appointment.start_time.substring(0,5) }} </text><br>
-            <text class="fs-3 fw-light" >
-                <i v-if="appointment.app_type_center" class=" bi bi-building"></i>      
-                <i v-if="appointment.app_type_home"  class=" bi bi-house-door" > </i>                                  
-                <i v-if="appointment.app_type_remote" class=" bi bi-camera-video"></i>                         
-            </text> 
-        </div>
-                    
-        <div  class="w-100 text-white  p-2" :style="{ 'background-color' : calendar_data.color  }"  style="border-top-right-radius : 25px;"  >
-                            
-            <div class=""  >
-                <text class="display-6 m-0 p-0" ><small> {{specialtyId2name(appointment.specialty)}} </small> </text>
-            </div>  
-                       
-            <div class="">
-                <text class="display-6 m-0 p-0" > 
-                    <small> <small> {{ getCenterName(appointment.center_id) }} </small> </small>
-                </text>
-            </div>
-
-            <div class="d-flex justify-content-between w-100" >
-                    <div>
-                        <text > {{appointment.patient_name }}</text><br>
-                        <text ><i class="bi bi-dot"></i> id:{{appointment.patient_doc_id }} </text><br>
-                        <text ><i class="bi bi-dot"></i> Edad:{{appointment.patient_age }} </text><br>
-                        <text v-if="appointment.home_visit"  ><i class="bi bi-dot"></i> A Domicilio <br> 
-                        - {{appointment.patient_address }} </text>
-                        
-                        <text v-if="appointment.center_visit"><i class="bi bi-dot"></i>  En Consulta <br>
-                        </text>
-                        
-                        
-                        <text v-if="appointment.remote_care" ><i class="bi bi-dot"></i> Atención Remota  </text>
-                    
-                        {{appointment.message1 }} 
-                        <p></p>
-                   </div>
-
-                   <div @click="$emit('displayModalAppAvailable', center_data )" style="background-color: #000 ; opacity: 0.6;  border-top-right-radius:25px" >
-                        <i style="font-size: 60px; padding:0px ; margin:-5px ; border:0px" class="bi bi-chevron-compact-right text-white"  ></i> 
-                   </div>
-
-            </div>
-
-
-
-                            
-        </div>
-
-
-    </div>
--->
-
 </template>
 
 <style scoped>
@@ -127,7 +70,7 @@ export default {
         }   
     },
    	
-    props: ['daterequired','appointment','index','global_specialties', 'global_comunas', 'session_params' ],
+    props: ['daterequired', 'days_expired' , 'appointment','index','global_specialties', 'global_comunas', 'session_params' ],
     emits:  ['displayModalReservedDetails'],
 
 	created () {
