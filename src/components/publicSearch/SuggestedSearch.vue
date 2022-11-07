1bff97933b70cca6ead7bfc6d74c482a3d0aa210
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-11-07 19:16:32 -0300

    Update SuggestedSearch.vue

--- src/components/publicSearch/SuggestedSearch.vue
+++ src/components/publicSearch/SuggestedSearch.vue
@@ -4,24 +4,115 @@ import axios from 'axios';
 </script>
 
 <template>
-       <div class="" >
-         
-           <p>
-               Busquedas mas comunes
-                
+       <div class="opacity-75" style=" border-radius: 10%;" >
+       
+            <p class="h5 text-muted d-flex justify-content-center mt-4 pt-3">
+               
+             Busquedas mas comunes 
             </p>
-            <hr>
-            <p class="text-start"> <text class="display-5"> Kinesiologia </text><br> <text class="text-muted"> A domicilio, Santiago </text> </p> 
+            <div class="d-flex align-content-start flex-wrap" style="border-radius: 40%; font-size: 0.8em;">
             
-            <p class="text-end"> <text class="display-5"> Psicologia </text><br> <text class="text-muted"> Tele Atención </text> </p> 
-            
-            <p class="text-start"> <text class="display-5"> Nutrición </text><br> <text class="text-muted"> En Centro, Providencia </text> </p> 
-   
-            <p class="text-end"> <text class="display-5"> Psicologia </text><br> <text class="text-muted"> En Centro, Las Condes </text> </p> 
-            
-            <p class="text-start"> <text class="display-5"> Kinesiologia </text><br> <text class="text-muted"> A domicilio, San Miguel </text> </p> 
-            
+                <div  style="background-color:#C7D8D9 ;border-radius: 50% ;width: 8.0em; height: 8.0em;" class="border border-1 m-2 d-flex justify-content-center p-0 m-0 "> 
+                    <div class="p-2 mt-3 text-center" style="" >
+                        <text class=""> Psicologìa 
+                        </text>
+                        <br> 
+                        <i class="bi bi-camera-video m-0 p-0" ></i>    
+                        <text class="text-muted"> Remota </text> 
+                    </div> 
+                </div> 
+                <div style="background-color:#91B8C1 ;border-radius: 50%;width: 8.0em; height: 8.0em;" class="border border-1 m-2 d-flex justify-content-center p-0 m-0"> 
+                    <div class="p-2 mt-3 text-center" style="" >
+                        <text class=""> Kinesiología <text></text> 
+                        </text>
+                        <br> 
+                        <i class="bi bi-house-door m-0 p-0" ></i>    
+                        <text class="text-muted"> A domicilio Santiago </text> 
+                    </div> 
+                </div> 
+                <div style="background-color:#DED7C9 ; border-radius: 50%;width: 8.0em; height: 8.0em;" class="border border-1 m-2 d-flex justify-content-center p-0 m-0"> 
+                    <div class="p-2 mt-3 text-center" style="" >
+                        <text class="">Enfermería<text></text> 
+                        </text>
+                        <br> 
+                        <i class="bi bi-building m-0 p-0" ></i>    
+                        <text class="text-muted"> A domicilio Santiago </text> 
+                    </div> 
+                </div> 
 
+
+
+
+                 <div style="background-color:#568281 ;border-radius: 50%;width: 8.0em; height: 8.0em;" class="border border-1 m-2 d-flex justify-content-center p-0 m-0 text-white"> 
+                    <div class="p-2 mt-3 text-center" style="" >
+                        <text class="">Fonoaudiología<text></text> 
+                        </text>
+                        <br> 
+                        <i class="bi bi-house-door m-0 p-0" ></i>    
+                        <text class="text-white"> A domicilio San Miguel </text> 
+                    </div> 
+                </div> 
+
+                                
+              <div style="background-color:#91B8C1 ;border-radius: 50%;width: 8.0em; height: 8.0em;" class="border border-1 m-2 d-flex justify-content-center p-0 m-0"> 
+                    <div class="p-2 mt-3 text-center" style="" >
+                        <text class=""> Terapia Ocupacional <text></text> 
+                        </text>
+                        <br> 
+                        <i class="bi bi-building m-0 p-0" ></i>    
+                        <text class="text-muted"> En Consulta Providencia </text> 
+                    </div> 
+                </div>
+                
+                <div style="background-color:#DED7C9 ; border-radius: 50%;width: 8.0em; height: 8.0em;" class="border border-1 m-2 d-flex justify-content-center p-0 m-0"> 
+                    <div class="p-2 mt-3 text-center" style="" >
+                        <text class=""> Psicopedagogía <text></text> 
+                        </text>
+                        <br> 
+                        <i class="bi bi-house-door m-0 p-0" ></i>    
+                        <text class="text-muted"> A domicilio Santiago </text> 
+                    </div> 
+                </div> 
+
+                 <div style="background-color:#C7D8D9 ;border-radius: 50%;width: 8.0em; height: 8.0em;" class="border border-1 m-2 d-flex justify-content-center p-0 m-0"> 
+                    <div class="p-2 mt-3 text-center" style="" >
+                        <text class=""> Masoterapia<text></text> 
+                        </text>
+                        <br> 
+                        <i class="bi bi-house-door m-0 p-0" ></i>    
+                        <text class="text-muted"> A domicilio Providencia </text> 
+                    </div> 
+                </div> 
+
+                                
+                
+                  <div style="background-color:#568281 ;border-radius: 50%;width: 8.0em; height: 8.0em;" class="border border-1 m-2 d-flex justify-content-center p-0 m-0 text-white"> 
+                    <div class="p-2 mt-3 text-center" style="" >
+                        <text class=""> Psicologìa <text></text> 
+                        </text>
+                        <br> 
+                        <i class="bi bi-camera-video m-0 p-0" ></i>    
+                        <text class="text-white"> Remoto </text> 
+                    </div> 
+                </div> 
+                
+            <!--
+                <div class="m-3 d-flex justify-content-between"> <text class=""> Psicología </text>, <text class="text-muted"> Tele Atención </text> </div> 
+                <div class="m-3 d-flex justify-content-between"> <text class=""> Nutrición </text>, <text class="text-muted"> En Centro, Providencia </text> </div> 
+                <div class="m-3"> <text class=""> Psicología </text>, <text class="text-muted"> En Centro, Las Condes </text> </div> 
+                <div class="m-3"> <text class=""> Kinesiología </text>, <text class="text-muted"> A domicilio, San Miguel </text> </div> 
+                <div class="m-3"> <text class=""> Enfermería </text>, <text class="text-muted"> A domicilio, Santiago </text> </div> 
+                <div class="m-3"> <text class=""> Fonoaudiología </text>, <text class="text-muted"> A domicilio, San Miguel </text> </div> 
+                <div class="m-3"> <text class=""> Terapia Ocupacional </text>,<text class="text-muted"> A domicilio, Santiago </text> </div> 
+                <div class="m-3"> <text class=""> Psicopedagogía </text>, <text class="text-muted"> A domicilio, San Miguel </text> </div> 
+                <div class="m-3"> <text class=""> Masoterapia </text>, <text class="text-muted"> A domicilio, Santiago </text> </div> 
+            -->
+            <div class="m-5 p-5">
+            </div>
+           
+            </div>
+            
+ 
 
         </div>
 </template>
