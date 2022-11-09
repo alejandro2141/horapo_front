cefe8736d0f0a2fd25df78b2d46f43405f152673
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-11-09 00:15:29 -0300

    Update SuggestedSearch.vue

--- src/components/publicSearch/SuggestedSearch.vue
+++ src/components/publicSearch/SuggestedSearch.vue
@@ -18,13 +18,13 @@ import specialtyCircle from './SpecialtyCircle.vue'
                     <text style="font-size: 0.7em;">En Consulta</text>
                 </div>
             </div>
-            <div @click="search_home = !search_home" :style="{  'border-radius': '50%' ,'width': '5.5em', 'height': '5.5em' }" class="border  border-2  p-0 m-2 d-flex justify-content-center align-items-center" :class="{'border-primary' : search_home }" > 
+            <div @click="search_home = !search_home" :style="{  'border-radius': '50%' ,'width': '5.5em', 'height': '5.5em' }" class="border  border-3  p-0 m-2 d-flex justify-content-center align-items-center" :class="{'border-primary' : search_home }" > 
                 <div class="m-2">
                     <i  class="bi bi-house-door m-0  display-3 d-flex justify-content-center" style="color:#3399FF;"></i>
                     <text style="font-size: 0.7em;">A Domicilio</text>
                 </div>
             </div>
-            <div @click="search_remote = !search_remote" :style="{  'border-radius': '50%' ,'width': '5.5em', 'height': '5.5em' }" class="border  border-2  p-0 m-1 d-flex justify-content-center align-items-center" :class="{'border-primary' : search_remote }" > 
+            <div @click="search_remote = !search_remote" :style="{  'border-radius': '50%' ,'width': '5.5em', 'height': '5.5em' }" class="border  border-3  p-0 m-1 d-flex justify-content-center align-items-center" :class="{'border-primary' : search_remote }" > 
                <div class="m-2">
                     <i  class="bi bi-camera-video m-0 display-3 d-flex justify-content-center" style="color:#b36b00;"></i>
                     <text style="font-size: 0.7em;">Remoto</text>
@@ -41,17 +41,17 @@ import specialtyCircle from './SpecialtyCircle.vue'
             -->
             <div class="w-100 mb-0 mt-3 pb-0 d-flex justify-content-center">
                 <specialtyCircle @click="sendSuggestedSearch(global_specialties[0]); circleColors[0]='#eeeeee'" :specialty='global_specialties[0]' :color='circleColors[0]' > </specialtyCircle>
-                <specialtyCircle @click="sendSuggestedSearch(global_specialties[1])" :specialty='global_specialties[1]' :color='circleColors[1]' > </specialtyCircle>
-                <specialtyCircle @click="sendSuggestedSearch(global_specialties[2])" :specialty='global_specialties[2]' :color='circleColors[2]' > </specialtyCircle>
+                <specialtyCircle @click="sendSuggestedSearch(global_specialties[1]); circleColors[1]='#eeeeee'" :specialty='global_specialties[1]' :color='circleColors[1]' > </specialtyCircle>
+                <specialtyCircle @click="sendSuggestedSearch(global_specialties[2]); circleColors[2]='#eeeeee'" :specialty='global_specialties[2]' :color='circleColors[2]' > </specialtyCircle>
             </div>
             <div class="w-100 mt-0 pt-0 d-flex justify-content-center">
-                <specialtyCircle @click="sendSuggestedSearch(global_specialties[3])" :specialty='global_specialties[3]' :color='circleColors[3]' > </specialtyCircle>
-                <specialtyCircle @click="sendSuggestedSearch(global_specialties[4])" :specialty='global_specialties[4]' :color='circleColors[4]' > </specialtyCircle>
+                <specialtyCircle @click="sendSuggestedSearch(global_specialties[3]); circleColors[3]='#eeeeee'" :specialty='global_specialties[3]' :color='circleColors[3]' > </specialtyCircle>
+                <specialtyCircle @click="sendSuggestedSearch(global_specialties[4]); circleColors[4]='#eeeeee'" :specialty='global_specialties[4]' :color='circleColors[4]' > </specialtyCircle>
             </div>
             <div class="w-100 mt-0 pt-0  d-flex justify-content-center">
-                <specialtyCircle @click="sendSuggestedSearch(global_specialties[5])" :specialty='global_specialties[5]' :color='circleColors[5]' > </specialtyCircle>
-                <specialtyCircle @click="sendSuggestedSearch(global_specialties[6])" :specialty='global_specialties[6]' :color='circleColors[6]' > </specialtyCircle>
-                <specialtyCircle @click="sendSuggestedSearch(global_specialties[7])" :specialty='global_specialties[7]' :color='circleColors[7]' > </specialtyCircle>
+                <specialtyCircle @click="sendSuggestedSearch(global_specialties[5]); circleColors[5]='#eeeeee'" :specialty='global_specialties[5]' :color='circleColors[5]' > </specialtyCircle>
+                <specialtyCircle @click="sendSuggestedSearch(global_specialties[6]); circleColors[6]='#eeeeee'" :specialty='global_specialties[6]' :color='circleColors[6]' > </specialtyCircle>
+                <specialtyCircle @click="sendSuggestedSearch(global_specialties[7]); circleColors[7]='#eeeeee'" :specialty='global_specialties[7]' :color='circleColors[7]' > </specialtyCircle>
             </div>
 
 
