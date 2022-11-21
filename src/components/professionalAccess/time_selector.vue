075af57f7a395bfa9371ac0d536677897f59f81b
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-11-21 19:32:26 -0300

    Update time_selector.vue

--- src/components/professionalAccess/time_selector.vue
+++ src/components/professionalAccess/time_selector.vue
@@ -7,19 +7,21 @@ import { ref } from 'vue'
     <div>
 
         <!-- TIME START -->
-        <div  class="d-flex justify-content-between mt-3">
+                  <!--
+                   <div  class="d-flex flex-row-reverse">
                           
                           <div class="d-flex justify-content-end">
-                              <div @click="show_hour=!show_hour ; show_start_minutes=false" class="bg-dark p-2">
-                                  <text >{{hour}}</text> 
+                              <div @click="show_hour=!show_hour ; show_minutes=false" class="bg-dark p-2">
+                                  <text >{{hour_set}}</text> 
                               </div>
                               <div class="p-2">:</div>
-                              <div @click="show_start_minutes=!show_start_minutes" class="bg-dark p-2 ">
-                                  <text >{{minutes}}</text>
+                              <div @click="show_minutes=!show_minutes; show_hour=false" class="bg-dark p-2 ">
+                                  <text >{{minutes_set}}</text>
                               </div>
                               <div class="p-2">hrs</div>
                           </div>
                   </div>
+                  -->
 
                     <div v-if="show_hour" class="text-white">
                       <div class="bg-success text-white border border-1 border-dark">
@@ -31,41 +33,41 @@ import { ref } from 'vue'
                       <tbody class="border border-dark text-dark  bg-white">
                         <tr class="border border-white">
                           <td @click="selectedHour('00')" class="p-2 border border-dark text-center">00</td>
-                          <td @click="start_hour='01'; show_start_hour=false" class="p-2 border border-dark text-center">01</td>
-                          <td @click="start_hour='02'; show_start_hour=false" class="p-2 border border-dark text-center">02</td>
-                          <td @click="start_hour='03'; show_start_hour=false" class="p-2 border border-dark text-center">03</td>
-                          <td @click="start_hour='04'; show_start_hour=false" class="p-2 border border-dark text-center">04</td>
-                          <td @click="start_hour='05'; show_start_hour=false" class="p-2 border border-dark text-center">05</td>
+                          <td @click="selectedHour('01')" class="p-2 border border-dark text-center">01</td>
+                          <td @click="selectedHour('02')" class="p-2 border border-dark text-center">02</td>
+                          <td @click="selectedHour('03')" class="p-2 border border-dark text-center">03</td>
+                          <td @click="selectedHour('04')" class="p-2 border border-dark text-center">04</td>
+                          <td @click="selectedHour('05')" class="p-2 border border-dark text-center">05</td>
                         </tr>
                         <tr>
-                          <td @click="start_hour='06'; show_start_hour=false" class=" border border-dark  text-center">06</td>
-                          <td @click="start_hour='07'; show_start_hour=false" class=" border border-dark  text-center">07</td>
-                          <td @click="start_hour='08'; show_start_hour=false" class=" border border-dark  text-center">08</td>
-                          <td @click="start_hour='09'; show_start_hour=false" class=" border border-dark  text-center">09</td>
-                          <td @click="start_hour='10'; show_start_hour=false" class=" border border-dark  text-center">10</td>
-                          <td @click="start_hour='11'; show_start_hour=false" class=" border border-dark  text-center">11</td>
+                          <td @click="selectedHour('06')" class=" border border-dark  text-center">06</td>
+                          <td @click="selectedHour('07')" class=" border border-dark  text-center">07</td>
+                          <td @click="selectedHour('08')" class=" border border-dark  text-center">08</td>
+                          <td @click="selectedHour('09')" class=" border border-dark  text-center">09</td>
+                          <td @click="selectedHour('10')" class=" border border-dark  text-center">10</td>
+                          <td @click="selectedHour('11')" class=" border border-dark  text-center">11</td>
                         </tr>
                         <tr>
-                          <td @click="start_hour='12'; show_start_hour=false" class=" border border-dark  text-center">12</td>
-                          <td @click="start_hour='13'; show_start_hour=false" class=" border border-dark  text-center">13</td>
-                          <td @click="start_hour='14'; show_start_hour=false" class=" border border-dark  text-center">14</td>
-                          <td @click="start_hour='15'; show_start_hour=false" class=" border border-dark  text-center">15</td>
-                          <td @click="start_hour='16'; show_start_hour=false" class=" border border-dark  text-center">16</td>
-                          <td @click="start_hour='17'; show_start_hour=false" class=" border border-dark  text-center">17</td>
+                          <td @click="selectedHour('12')" class=" border border-dark  text-center">12</td>
+                          <td @click="selectedHour('13')" class=" border border-dark  text-center">13</td>
+                          <td @click="selectedHour('14')" class=" border border-dark  text-center">14</td>
+                          <td @click="selectedHour('15')" class=" border border-dark  text-center">15</td>
+                          <td @click="selectedHour('16')" class=" border border-dark  text-center">16</td>
+                          <td @click="selectedHour('17')" class=" border border-dark  text-center">17</td>
                         </tr>
                         <tr>
-                          <td @click="start_hour='18'; show_start_hour=false" class=" border border-dark  text-center">18</td>
-                          <td @click="start_hour='19'; show_start_hour=false" class=" border border-dark  text-center">19</td>
-                          <td @click="start_hour='20'; show_start_hour=false" class=" border border-dark  text-center">20</td>
-                          <td @click="start_hour='21'; show_start_hour=false" class=" border border-dark  text-center">21</td>
-                          <td @click="start_hour='22'; show_start_hour=false" class=" border border-dark  text-center">22</td>
-                          <td @click="start_hour='23'; show_start_hour=false" class=" border border-dark  text-center">23</td>
+                          <td @click="selectedHour('18')" class=" border border-dark  text-center">18</td>
+                          <td @click="selectedHour('19')" class=" border border-dark  text-center">19</td>
+                          <td @click="selectedHour('20')" class=" border border-dark  text-center">20</td>
+                          <td @click="selectedHour('21')" class=" border border-dark  text-center">21</td>
+                          <td @click="selectedHour('22')" class=" border border-dark  text-center">22</td>
+                          <td @click="selectedHour('23')" class=" border border-dark  text-center">23</td>
                         </tr>
                       </tbody>
                       </table>
                     </div>
 
-                    <div v-if="show_start_minutes" class="text-white">
+                    <div v-if="show_minutes" class="text-white">
                       <div class="bg-success text-white border border-1 border-dark">
                         <text class="p-2 h5 ">Seleccione Minutos</text>
                       </div>
@@ -74,20 +76,20 @@ import { ref } from 'vue'
                      
                       <tbody class="border border-dark text-dark bg-white">
                         <tr>
-                          <td @click="start_minutes='00'; show_start_minutes=false" class="border border-dark  p-2 text-center">00</td>
-                          <td @click="start_minutes='05'; show_start_minutes=false" class="border border-dark  p-2 text-center">05</td>
-                          <td @click="start_minutes='10'; show_start_minutes=false" class="border border-dark  p-2 text-center">10</td>
-                          <td @click="start_minutes='15'; show_start_minutes=false" class="border border-dark  p-2 text-center">15</td>
-                          <td @click="start_minutes='20'; show_start_minutes=false" class="border border-dark  p-2 text-center">20</td>
-                          <td @click="start_minutes='25'; show_start_minutes=false" class="border border-dark  p-2 text-center">25</td>
+                          <td @click="selectedMinutes('00')" class="border border-dark  p-2 text-center">00</td>
+                          <td @click="selectedMinutes('05')" class="border border-dark  p-2 text-center">05</td>
+                          <td @click="selectedMinutes('10')" class="border border-dark  p-2 text-center">10</td>
+                          <td @click="selectedMinutes('15')" class="border border-dark  p-2 text-center">15</td>
+                          <td @click="selectedMinutes('20')" class="border border-dark  p-2 text-center">20</td>
+                          <td @click="selectedMinutes('25')" class="border border-dark  p-2 text-center">25</td>
                         </tr>
                         <tr>
-                          <td @click="start_minutes='30'; show_start_minutes=false" class="border border-dark  text-center">30</td>
-                          <td @click="start_minutes='35'; show_start_minutes=false" class="border border-dark  text-center">35</td>
-                          <td @click="start_minutes='40'; show_start_minutes=false" class="border border-dark  text-center">40</td>
-                          <td @click="start_minutes='45'; show_start_minutes=false" class="border border-dark  text-center">45</td>
-                          <td @click="start_minutes='50'; show_start_minutes=false" class="border border-dark  text-center">50</td>
-                          <td @click="start_minutes='55'; show_start_minutes=false" class="border border-dark  text-center">55</td>
+                          <td @click="selectedMinutes('30')" class="border border-dark  text-center">30</td>
+                          <td @click="selectedMinutes('35')" class="border border-dark  text-center">35</td>
+                          <td @click="selectedMinutes('40')" class="border border-dark  text-center">40</td>
+                          <td @click="selectedMinutes('45')" class="border border-dark  text-center">45</td>
+                          <td @click="selectedMinutes('50')" class="border border-dark  text-center">50</td>
+                          <td @click="selectedMinutes('55')" class="border border-dark  text-center">55</td>
                         </tr>
                       </tbody>
                       </table>
@@ -108,35 +110,46 @@ import { ref } from 'vue'
 export default {
    data : function() {
         return {
-            hour: null ,
-            minutes : null  ,
-            
+            hour_set: '---' ,
+            minutes_set : '---'  ,
+
+            show_hour : false  ,
+            show_minutes : false ,    
         }   
     },
    	
-	props: ['set_hour','set_minutes'],
-    emits: ['selected_hour','selected_minutes'],
+	props: ['hour','minutes'],
+  emits: ['selected_hour','selected_minutes'],
 
 	created () {
-        
+    this.hour_set = this.hour 
+    this.minutes_set = this.minutes
 	},
 
 	methods :{
         selectedHour(val)
         {
-        show_start_hour=false
+        this.hour_set = val
+        this.show_hour = false 
         $this.emit('selected_hour', val)
+        },
+
+        selectedMinutes(val)
+        {
+        this.minutes_set = val
+        this.show_minutes = false 
+        $this.emit('selected_minutes', val)
         }
     },
 
     watch : {
-        set_hour(val)
+        hour(val)
         {
-        this.hour=val
+        this.hour_set=val
         },
-        set_minutes(val)
+        minutes(val)
         {
-        this.minutes = val
+        this.minutes_set = val
         }
     }
 }
