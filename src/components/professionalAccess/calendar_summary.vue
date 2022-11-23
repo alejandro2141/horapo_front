2414a8c35db3601f6a2d6e34af9615ecbf923625
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-11-23 01:52:27 -0300

    Update calendar_summary.vue

--- src/components/professionalAccess/calendar_summary.vue
+++ src/components/professionalAccess/calendar_summary.vue
@@ -2,46 +2,69 @@
 import { ref } from 'vue'
 import axios from 'axios';
 
-
 </script>
 
 <template>
    
-
     <div class="d-flex justify-content-between bg-primary text-white">
-        
-        
-        <div class=" d-flex justify-content-start">
-           
-            <div class="d-flex justify-content-start p-1 border ">   
-                <div class="">
-                    <small>
-                    R 1 <br> 
-                    D 5 <br>
-                    </small>
+                  
+                <div class="text-center">
+                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[0].day_name }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[0].day_number }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[0].month }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[0].reserved }}</div>
                 </div>
-                <div class="">
-                    <text>Lun</text><br>
-                   {{day1}}
+
+                <div class="text-center">
+                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[1].day_name }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[1].day_number }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[1].month }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[1].reserved }}</div>
                 </div>
-            </div>
-            
+                <div class="text-center">
+                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[2].day_name }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[2].day_number }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[2].month }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[2].reserved }}</div>
+                </div>
+                <div class="text-center">
+                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[3].day_name }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[3].day_number }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[3].month }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[3].reserved }}</div>
+                </div>
+                <div class="text-center">
+                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[4].day_name }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{ month_summary[4].day_number }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[4].month }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[4].reserved }}</div>
+                </div>
 
+                <div class="text-center">
+                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[5].day_name }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[5].day_number }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[5].month }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[5].reserved }}</div>
+                </div>
 
-        </div>
-
-       
-
-
-    </div>
-
-                              
+                <div class="text-center">
+                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[6].day_name }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[6].day_number }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[6].month }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[6].reserved }}</div>
+                </div>
+                <div class="text-center">
+                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[7].day_name }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[7].day_number }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[7].month }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[7].reserved }}</div>
+                </div>
+    </div>        
     
 </template>
 
 <style scoped>
 </style>
-
 
 <script>
 
@@ -49,58 +72,53 @@ export default {
  
    data : function() {
         return {
-        day1 : null ,
-        day2 : null ,
-        day3 : null ,
-        day4 : null ,
-        day5 : null ,
-        day6 : null ,
-        day7 : null ,
-        
         cdate : null ,
-
-        month_summary : [],
-        }   
+        month_summary : ["a","a","a","a","a","a","a","a"],
+        dayData :   {   day_number  :  null ,
+                        day_name    : null  ,
+                        month       :  null ,
+                        reserved    : 0 
+                    },
+            }   
     },
    	
-	props: ['session_params', 'date_required'],
+	props: ['session_params', 'daterequired','forceUpdateCalendarSummary'],
     emits: [],
 
 	created () {
-        this.cdate = new Date()
+            
+            for (let i = 0; i < 8 ; i++) {
+                this.month_summary.push(this.dayData)
+            }
         
-        this.cdate.setHours(0,0,0,0)
-        this.cdate.setDate(this.cdate.getDate() -3  ) 
-        this.day1=this.cdate.getDate()
-    
-        this.cdate.setDate(this.cdate.getDate() + 1  ) 
-        this.day2=this.cdate.getDate()
-
-        this.cdate.setDate(this.cdate.getDate() + 1  ) 
-        this.day3=this.cdate.getDate()
-   
-        this.cdate.setDate(this.cdate.getDate() + 1  ) 
-        this.day4=this.cdate.getDate()
-
-        this.cdate.setDate(this.cdate.getDate() + 1  ) 
-        this.day5=this.cdate.getDate()
- 
-        this.cdate.setDate(this.cdate.getDate() + 1  ) 
-        this.day6=this.cdate.getDate()
-  
-        this.cdate.setDate(this.cdate.getDate() + 1  ) 
-        this.day7=this.cdate.getDate()
-
-
-        this.getMonthSummary()
-            },
+        },
 
 	methods :{
-
-        async getMonthSummary()
+        getdDayData(pos)
         {
-            let aux_start_date = new Date(this.cdate.getTime() - (2629746000/2))
-            let aux_end_date = new Date(this.cdate.getTime() + (2629746000/2))
+            const dayData = {
+                    day_number :  null ,
+                    day_name : null  ,
+                    month : null ,
+                    reserved : 0 , 
+                }
+
+            if ( this.month_summary[pos] != null )
+            {
+              dayData.day_number = this.month_summary[pos].day_number
+              dayData.day_name = this.month_summary[pos].day_name
+              dayData.month = this.month_summary[pos].month
+              dayData.reserved = this.month_summary[pos].reserved
+            }
+            return dayData
+        },
+
+        async updateMonthSummary(startDate)
+        {
+            //let aux_start_date = new Date(this.cdate.getTime() - (2629746000/2))
+            //let aux_end_date = new Date(this.cdate.getTime() + (2629746000/2))
+            let aux_start_date = startDate
+            let aux_end_date = new Date( aux_start_date.getTime() + (86400000*7) )
             
              const json = { 
              // date :  new Date(aux_dateToSearch).toISOString() ,
@@ -113,34 +131,45 @@ export default {
               let response_json =  await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_get_month_summary",json) 
               console.log ("/professional_get_month_summary RESPONSE: "+JSON.stringify(response_json.data)) 
               //this.updateCalendarsMarks();
-              this.month_summary = response_json 
+              this.build_calendar_summary (response_json,aux_start_date,aux_end_date)
+              //this.month_summary = response_json 
         },
 
         build_calendar_summary(app_reserv,date_start,date_end)
         {
             //make slots  
-            let day_names =['Dom','Lun','Mar','Mier','Juev','Vier','Sab']
-            for (var d = new Date(date_start); (d <= date_end && d <= new Date(calendar.date_end)  )  ; d.setDate(new Date(d).getDate() + 1)) 
+            let day_names =['D','L','M','Mi','J','V','S']
+            let month_names =['ENE','FEB','MAR','ABR','MAY','JUN','JUL','AGO','SEP','OCT','NOV','DIC']
+            let i=0 
+            for (var d = new Date(date_start); (d <= date_end && d <= new Date(date_end)  )  ; d.setDate(new Date(d).getDate() + 1)) 
             {
+                //CHECK IF APP TAKEN in app_reserv
+
                 const structure_day = {
                     day_number :  d.getDate(),
                     day_name : day_names[d.getDay()] ,
-                    month : d.getMonth() ,
+                    month : month_names[d.getMonth()] ,
                     reserved : 0 , 
                 } 
-                this.month_summary.push(structure_day)
-                 
+                this.month_summary[i] = structure_day
+                i++
             }
+            //ADD FILTER HERE TO COUNT APPOINTMENTS EXIST IN DATE
+            //calendars =  calendars.filter(cal =>  centers_ids_filtered.includes(cal.center_id) ) 
+  
 
+           console.log("month_summary lenght :"+this.month_summary.length  )
+        }
+    },
 
-            const structure_day = {
-                day_number : null,
-                day_name : null,
-                month : null,
-                reserved : 0 , 
-            } 
+    watch: {
+        
+       
 
-
+        forceUpdateCalendarSummary(new_date) 
+        { console.log("forceUpdateCalendarSummary calendar_summary" +this.daterequired);
+           
+           this.updateMonthSummary(this.daterequired) 
         }
 
 
