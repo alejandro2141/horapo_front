e364e68f7f32073cab791081d8b84d7c8387fa53
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-11-24 01:32:21 -0300

    Update appointmentReserved.vue

--- src/components/professionalAccess/appointmentReserved.vue
+++ src/components/professionalAccess/appointmentReserved.vue
@@ -21,7 +21,7 @@ import axios from 'axios';
                             </text> 
                         </div>
 
-                        <div class="d-flex justify-content-between w-100"  :style="[ {'background-color' : calendar_data.color} , (days_expired ) ? { 'background-color': '#eee'}:{ } , (appointment.lock_day ) ? { 'background-color': '#444', 'background' : 'repeating-linear-gradient( -45deg, #000, #888 1px, #333 5px, #333 10px )' }:{ 'opacity': '1' }  ]"  style="border-top-right-radius: 25px;" >       
+                        <div class="d-flex justify-content-between w-100"  :style="[  (days_expired ) ? { 'background-color': '#eee'}:{ } , (appointment.lock_day ) ? { 'background-color': '#444', 'background' : 'repeating-linear-gradient( -45deg, #000, #888 1px, #333 5px, #333 10px )' }:{ 'opacity': '1' }  ]"  style="border-top-right-radius: 25px;" >       
                                 <div @click="selectApp=!selectApp; $emit('addToBlockList',appointment) " class="w-100 text-dark display-6 p-2"  style=""  >
                                     
                                     <div class=""  >
