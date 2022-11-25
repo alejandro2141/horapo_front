8f00027e563bb18f749e7d8dc63e171cc6ea7b1b
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-11-25 01:46:58 -0300

    Update calendar_summary.vue

--- src/components/professionalAccess/calendar_summary.vue
+++ src/components/professionalAccess/calendar_summary.vue
@@ -7,55 +7,48 @@ import axios from 'axios';
 <template>
    
     <div class="d-flex justify-content-between bg-success bg-opacity-75 text-white">
-                       
                 
-                <div class="text-center" v-if="month_summary[1] !=null ">
-                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[0].day_name }}</div>
-                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[0].day_number }}</div>
-                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[0].month }}</div>
-                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[0].reserved }}</div>
+                <div class="text-center" v-if="month_summary[0] !=null ">
+                    <div style="font-size: 0.9em" class="m-0 p-0" > {{ day_names[month_summary[0].date.getDay()] }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{ month_summary[0].date.getDate() }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" > {{month_names[month_summary[0].date.getMonth()] }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" > {{month_summary[0].reserved}}</div>
                 </div>
                 <div class="text-center" v-if="month_summary[1] !=null ">
-                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[1].day_name }}</div>
-                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[1].day_number }}</div>
-                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[1].month }}</div>
-                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[1].reserved }}</div>
+                    <div style="font-size: 0.9em" class="m-0 p-0" > {{ day_names[month_summary[1].date.getDay()] }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{ month_summary[1].date.getDate() }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" > {{month_names[month_summary[1].date.getMonth()] }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" > {{month_summary[1].reserved}}</div>
                 </div>
                 <div class="text-center" v-if="month_summary[2] !=null ">
-                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[2].day_name }}</div>
-                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[2].day_number }}</div>
-                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[2].month }}</div>
-                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[2].reserved }}</div>
+                    <div style="font-size: 0.9em" class="m-0 p-0" > {{ day_names[month_summary[2].date.getDay()] }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{ month_summary[2].date.getDate() }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" > {{month_names[month_summary[2].date.getMonth()] }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" > {{month_summary[2].reserved}}</div>
                 </div>
                 <div class="text-center" v-if="month_summary[3] !=null ">
-                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[3].day_name }}</div>
-                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[3].day_number }}</div>
-                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[3].month }}</div>
-                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[3].reserved }}</div>
+                    <div style="font-size: 0.9em" class="m-0 p-0" > {{ day_names[month_summary[3].date.getDay()] }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{ month_summary[3].date.getDate() }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" > {{month_names[month_summary[3].date.getMonth()] }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" > {{month_summary[3].reserved}}</div>
                 </div>
                 <div class="text-center" v-if="month_summary[4] !=null ">
-                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[4].day_name }}</div>
-                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[4].day_number }}</div>
-                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[4].month }}</div>
-                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[4].reserved }}</div>
+                    <div style="font-size: 0.9em" class="m-0 p-0" > {{ day_names[month_summary[4].date.getDay()] }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{ month_summary[4].date.getDate() }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" > {{month_names[month_summary[4].date.getMonth()] }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" > {{month_summary[4].reserved}}</div>
                 </div>
                 <div class="text-center" v-if="month_summary[5] !=null ">
-                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[5].day_name }}</div>
-                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[5].day_number }}</div>
-                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[5].month }}</div>
-                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[5].reserved }}</div>
+                    <div style="font-size: 0.9em" class="m-0 p-0" > {{ day_names[month_summary[5].date.getDay()] }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{ month_summary[5].date.getDate() }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" > {{month_names[month_summary[5].date.getMonth()] }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" > {{month_summary[5].reserved}}</div>
                 </div>
                 <div class="text-center" v-if="month_summary[6] !=null ">
-                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[6].day_name }}</div>
-                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[6].day_number }}</div>
-                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[6].month }}</div>
-                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[6].reserved }}</div>
-                </div>
-                <div class="text-center" v-if="month_summary[7] !=null ">
-                    <div style="font-size: 0.9em" class="m-0 p-0" >{{ month_summary[7].day_name }}</div>
-                     <div style="font-size: 1.5em" class="m-0 p-0" >{{month_summary[7].day_number }}</div>
-                     <div style="font-size: 0.7em" class="m-0 p-0" >{{month_summary[7].month }}</div>
-                     <div style="font-size: 0.9em" class="m-0 p-0" >{{month_summary[7].reserved }}</div>
+                    <div style="font-size: 0.9em" class="m-0 p-0" > {{ day_names[month_summary[6].date.getDay()] }}</div>
+                     <div style="font-size: 1.5em" class="m-0 p-0" >{{ month_summary[6].date.getDate() }}</div>
+                     <div style="font-size: 0.7em" class="m-0 p-0" > {{month_names[month_summary[6].date.getMonth()] }}</div>
+                     <div style="font-size: 0.9em" class="m-0 p-0" > {{month_summary[6].reserved}}</div>
                 </div>
                  
     </div>        
@@ -71,7 +64,10 @@ export default {
  
    data : function() {
         return {
-        cdate : null ,
+        cdate       : null , 
+        day_names   : ['D','L','M','Mi','J','V','S'] ,
+        month_names : ['ENE','FEB','MAR','ABR','MAY','JUN','JUL','AGO','SEP','OCT','NOV','DIC']
+            
             }   
     },
    	
