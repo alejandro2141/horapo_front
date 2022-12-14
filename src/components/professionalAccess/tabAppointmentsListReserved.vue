13532390ddbf6a86087dbcf9ad7d44e6a9aeab51
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-12-13 21:07:45 -0300

    Update tabAppointmentsListReserved.vue

--- src/components/professionalAccess/tabAppointmentsListReserved.vue
+++ src/components/professionalAccess/tabAppointmentsListReserved.vue
@@ -5,15 +5,29 @@ import axios from 'axios';
 
 </script>
 <template>
-    <div class="">
-        Lista todas sus citas reservadas
-
-        <p v-for="app in appsTaken" :key='app.id' >
-            {{ formatDate(app.date)  }} - {{ formatTime(app.start_time) }} - {{ app.duration }} - CalendarId:{{ app.calendar_id }}
+    
+    <div class="m-3">
+            <p class="text-center">Se listan todas las Reservadas</p>
+            <div class="md-form mt-0">
+    </div>
+    <!-- Search form -->
+        <div class="m-2 d-flex justify-content-center">
+            <input class="form-control form-control-sm ml-3 w-75"  v-model="pattern" type="text" placeholder="Search" aria-label="Search">
+            <i @click="searchPattern(pattern)" class="bi bi-search display-3"></i>
+        </div>
+      
+        <hr>        
+        <div v-for="app in appTakenFiltered" :key='app.id' >
+            
+          <i class="bi bi-clock-history"></i>  {{ formatDate(app.date)  }} : {{ formatTime(app.start_time) }}({{ app.duration }} Min)  {{getSpecialty(app.specialty_reserved)}}
             <br> 
-            {{ app.patient_name  }} {{ app.patient_doc_id  }} {{ app.patient_doc_id  }}  {{ app.patient_email  }} {{app.patient_phone1}}
+            <small>
+            {{ app.patient_name  }} {{ app.patient_doc_id  }} ({{ app.patient_age  }})  {{ app.patient_email  }} <i class="bi bi-telephone"></i>{{app.patient_phone1}}
+            </small>
             <br>
-         </p>
+        <hr>
+        </div>
+      
 
 
     </div>     
@@ -30,8 +44,11 @@ export default {
  
 data: function () {
 		return {
-            appsTaken : []
-          		 }
+            appsTaken : [],
+            specialties: [],
+            pattern : "Patron de Busqueda",
+            appTakenFiltered : [] 
+           		 }
 	},
 	
 
@@ -53,15 +70,49 @@ data: function () {
 
     destroyed() {
         console.log("TAB AppointmentsListReserved Destroyed");
-    },
+        },
 
     mounted () {
         console.log("TAB AppointmentsListReserved Mounted");
         this.getAppTaken()
-    },
+        },
 
  
     methods: {
+            searchPattern(pattern)
+            {
+               this.appTakenFiltered = [] 
+               console.log("searchPatter:"+pattern)
+               this.pattern.toUpperCase()
+               
+               for (let i = 0; i < this.appsTaken.length; i++) 
+               {
+                    if ( this.appsTaken[i].patient_name.includes(pattern) )
+                    {
+                    this.appTakenFiltered.push(this.appsTaken[i]) 
+                    }
+                
+                }
+
+               
+
+
+            },
+
+            filterby(val)
+            {
+            this.appTakenFiltered = appsTaken.filter(app => app.patient_name ==  val );           
+            },
+
+            getSpecialty(pid)
+            {
+                let spe= this.specialties.find(elem => elem.id ==  pid  )
+
+                if (spe != null) { return spe.name }
+                else { return "" }
+            
+            },
+
             formatDate(obj)
             {
                 let adate = new Date (obj)
@@ -78,14 +129,15 @@ data: function () {
                 const json = { 
                     //professional_id : this.session_params.professional_id ,			   
                     professional_id : this.session_params.professional_id ,			   
-                      
                             };
                     console.log ("professional_get_appointments_taken :"+ JSON.stringify(json)  );
                     let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_get_appointments_taken",json);
-                    this.appsTaken = response_json.data;
-                    console.log ("professional_get_appointments_taken  RESPONSE:"+JSON.stringify(this.appsTaken)) ;                       
+                    console.log ("professional_get_appointments_taken  RESPONSE:"+JSON.stringify(response_json)) ;   
+                    
+                    this.appsTaken = response_json.data.appointments
+                    this.appTakenFiltered = response_json.data.appointments
+                    this.specialties = response_json.data.specialties
             },	
-
 
 		},
   
