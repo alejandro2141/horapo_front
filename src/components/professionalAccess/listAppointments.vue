5936cedeabbc24b53d40a4e71ecdaeed59b34f9b
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-12-26 23:42:55 -0300

    Update listAppointments.vue

--- src/components/professionalAccess/listAppointments.vue
+++ src/components/professionalAccess/listAppointments.vue
@@ -62,7 +62,7 @@ import ModalProfessionalReserveAppointment from './modalProfessionalReserveAppoi
     <div v-if="filteredAppList != null && filteredAppList.length >0  ">
         <div v-for="hour in filteredAppList" :key="hour"  >
                 
-                <div class="m-2 mt-3">
+                <div class="m-2 mt-4">
 
                     <div v-if="hour.app_available != null"  >
                       
