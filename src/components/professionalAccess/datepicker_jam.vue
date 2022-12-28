1ae33fb7d42b17844986fdcf7388891faf3d8b68
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2022-12-28 18:26:16 -0300

    Update datepicker_jam.vue

--- src/components/professionalAccess/datepicker_jam.vue
+++ src/components/professionalAccess/datepicker_jam.vue
@@ -79,89 +79,117 @@ import axios from 'axios';
                     <tr v-if="week1!=null" class="text-dark" >
                         <td  v-for="day in week1" :key="day" colspan="2" class="" > 
                             <text class="h1 fw-lighter" @click="dayPicked(day.date)" v-if="day.date.getDate() < 20"  :class="[{ 'text-danger' : tday.getDate() == day.date.getDate()  && tday.getMonth() == day.date.getMonth() , 'text-primary text-decoration-underline fw-normal' : calendar_date.getDate() == day.date.getDate()  && calendar_date.getMonth() == day.date.getMonth() ,  'opacity-100 fw-lighter' :  day.date.getDate() > 20     }]" > 
-                                <text class="m-0 p-0"> {{day.date.getDate()}}  </text>
-                                <br> 
-                               
-                                <div class="d-flex flex-wrap  opacity-50 justify-content-center" >
+                                <div class="m-0 p-0 d-flex justify-content-center">
+                                    <text class="m-0 p-0"> {{day.date.getDate()}} </text>
+                                    <i v-if="day.locked"  style="margin-left: -20px;" class="opacity-25 text-secondary bi bi-lock-fill" >
+                                    </i>
+                                    <text v-if="day.reserved>0" style="font-size: 0.5em;" >{{day.reserved}}</text>   
+                                </div>
+                                <div class="d-flex flex-wrap  opacity-50 justify-content-center" style="height:8px;">
                                  
                                     <div class="" v-for="color in day.calendar_colors" :key="color"  style="height:8px; width:8px; margin-right: 2px;" :style="{'background-color': color }" > 
                                     </div>
                                 </div>
-
+                                <!--
                                 <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i>  
-                                <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text>    
+                                <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text> 
+                                -->   
                             </text>  
                         </td>                      
                     </tr>
 
                     <tr v-if="week2!=null" class="text-dark" >
                         <td  v-for="day in week2" :key="day" colspan="2" class="" @click="dayPicked(day.date)"> 
-                            <text class="h1 fw-lighter" :class="[{ 'text-danger' : tday.getDate() == day.date.getDate()  && tday.getMonth() == day.date.getMonth() , 'text-primary text-decoration-underline fw-normal'  : calendar_date.getDate() == day.date.getDate()  && calendar_date.getMonth() == day.date.getMonth()     }]" > 
-                                <text class="m-0 p-0"> {{day.date.getDate()}}  </text>
-                                <br> 
-
-                                <div class="d-flex flex-wrap  opacity-50 justify-content-center" >
+                            <div class="h1 fw-lighter" :class="[{ 'text-danger' : tday.getDate() == day.date.getDate()  && tday.getMonth() == day.date.getMonth() , 'text-primary text-decoration-underline fw-normal' : calendar_date.getDate() == day.date.getDate()  && calendar_date.getMonth() == day.date.getMonth()      }]" > 
+                                <div class="m-0 p-0 d-flex justify-content-center"> 
+                                    <text>{{day.date.getDate()}}</text>
+                                    <i v-if="day.locked"  style="margin-left: -20px;" class="opacity-25 text-secondary bi bi-lock-fill" >
+                                    </i>
+                                    <text v-if="day.reserved>0" style="font-size: 0.5em;" >{{day.reserved}}</text>   
+                               
+                                </div>
                                 
-                                    <div class="" v-for="color in day.calendar_colors" :key="color"  style="height:8px; width:8px; margin-right: 2px;" :style="{'background-color': color }" > 
+                                <div class="d-flex flex-wrap opacity-50 justify-content-center"  style="height:8px;" >
+                               
+                                    <div class=""  v-for="color in day.calendar_colors" :key="color"  style="height:8px; width:8px; margin-right: 2px;" :style="{'background-color': color }" > 
                                     </div>
                                 </div>
-
-                                <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i>  
-                                <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text>    
-                            </text>  
+                                    <!--
+                                <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i> 
+                                <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text>   
+                                    -->
+                            </div>  
                         </td>                      
                     </tr>
 
                     <tr v-if="week3!=null" class="text-dark" >
                         <td  v-for="day in week3" :key="day" colspan="2" class="" @click="dayPicked(day.date)"> 
-                            <text class="h1 fw-lighter" :class="[{ 'text-danger' : tday.getDate() == day.date.getDate()  && tday.getMonth() == day.date.getMonth() , 'text-primary text-decoration-underline fw-normal' : calendar_date.getDate() == day.date.getDate()  && calendar_date.getMonth() == day.date.getMonth()     }]" > 
-                                <text class="m-0 p-0"> {{day.date.getDate()}}  </text>
-                                <br> 
-
-                                <div class="d-flex flex-wrap opacity-50 justify-content-center" >
+                            <div class="h1 fw-lighter" :class="[{ 'text-danger' : tday.getDate() == day.date.getDate()  && tday.getMonth() == day.date.getMonth() , 'text-primary text-decoration-underline fw-normal' : calendar_date.getDate() == day.date.getDate()  && calendar_date.getMonth() == day.date.getMonth()      }]" > 
+                                <div class="m-0 p-0 d-flex justify-content-center"> 
+                                    <text>{{day.date.getDate()}}</text>
+                                    <i v-if="day.locked"  style="margin-left: -30px;" class="opacity-50 text-secondary bi bi-lock-fill" >
+                                    </i>
+                                    <text v-if="day.reserved>0" style="font-size: 0.5em;" >{{day.reserved}}</text>   
                                
-                                    <div class="" v-for="color in day.calendar_colors" :key="color"  style="height:8px; width:8px; margin-right: 2px;" :style="{'background-color': color }" > 
+                                </div>
+                                
+                                <div class="d-flex flex-wrap opacity-50 justify-content-center"  style="height:8px;" >
+                               
+                                    <div class=""  v-for="color in day.calendar_colors" :key="color"  style="height:8px; width:8px; margin-right: 2px;" :style="{'background-color': color }" > 
                                     </div>
                                 </div>
-
-                                <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i>  
-                                <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text>    
-                            </text>  
+                                    <!--
+                                <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i> 
+                                <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text>   
+                                    -->
+                            </div>  
                         </td>                      
                     </tr>
 
                     <tr v-if="week4!=null" class="text-dark" >
                         <td  v-for="day in week4" :key="day" colspan="2" class="" @click="dayPicked(day.date)"> 
-                            <text class="h1 fw-lighter" :class="[{ 'text-danger' : tday.getDate() == day.date.getDate()  && tday.getMonth() == day.date.getMonth() , 'text-primary text-decoration-underline fw-normal' : calendar_date.getDate() == day.date.getDate()  && calendar_date.getMonth() == day.date.getMonth()      }]" > 
-                                <text class="m-0 p-0"> {{day.date.getDate()}}  </text>
-                                <br> 
+                            <div class="h1 fw-lighter" :class="[{ 'text-danger' : tday.getDate() == day.date.getDate()  && tday.getMonth() == day.date.getMonth() , 'text-primary text-decoration-underline fw-normal' : calendar_date.getDate() == day.date.getDate()  && calendar_date.getMonth() == day.date.getMonth()      }]" > 
+                                <div class="m-0 p-0 d-flex justify-content-center"> 
+                                    <text>{{day.date.getDate()}}</text>
+                                    <i v-if="day.locked"  style="margin-left: -30px;" class="opacity-50 text-secondary bi bi-lock-fill" >
+                                    </i>
+                                    <text v-if="day.reserved>0" style="font-size: 0.5em;" >{{day.reserved}}</text>   
+                               
+                                </div>
                                 
-                                <div class="d-flex flex-wrap opacity-50 justify-content-center" >
+                                <div class="d-flex flex-wrap opacity-50 justify-content-center"   style="height:8px;" >
                                
                                     <div class=""  v-for="color in day.calendar_colors" :key="color"  style="height:8px; width:8px; margin-right: 2px;" :style="{'background-color': color }" > 
                                     </div>
                                 </div>
-
+                                    <!--
                                 <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i> 
-                                <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text>    
-                            </text>  
+                                <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text>   
+                                    -->
+                            </div>  
                         </td>                      
                     </tr>
 
                     <tr v-if="week5!=null" class="text-dark" >
                         <td  v-for="day in week5" :key="day" colspan="2" class="" > 
                             <text class="h1 fw-lighter" @click="dayPicked(day.date)" v-if="day.date.getDate() > 10"   :class="[{ 'text-danger' : tday.getDate() == day.date.getDate()  && tday.getMonth() == day.date.getMonth() , 'text-primary  text-decoration-underline fw-normal' : calendar_date.getDate() == day.date.getDate()  && calendar_date.getMonth() == day.date.getMonth() ,  'opacity-100 fw-lighter' :  day.date.getDate() < 10    }]" > 
-                                <text class="m-0 p-0"> {{day.date.getDate()}}  </text>
-                                <br> 
                                 
-                                <div class="d-flex flex-wrap opacity-50 justify-content-center" >
+                                <div class="m-0 p-0 d-flex justify-content-center"> 
+                                   <text>{{day.date.getDate()}}</text>
+                                   <i v-if="day.locked"  style="margin-left: -30px;" class="opacity-50 text-secondary bi bi-lock-fill" >
+                                    </i>
+                                    <text v-if="day.reserved>0" style="font-size: 0.5em;" >{{day.reserved}}</text> 
+                                </div>
+
+                                <div class="d-flex flex-wrap opacity-50 justify-content-center"    style="height:8px;" >
                                
                                     <div class="" v-for="color in day.calendar_colors" :key="color"  style="height:8px; width:8px; margin-right: 2px;" :style="{'background-color': color }" > 
                                     </div>
                                 </div>
-
-                            <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i>   
+                                <!--
+                                <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i>   
                                 <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text>   
+                                -->
                             </text>  
                         </td>                      
                     </tr>
@@ -169,17 +197,23 @@ import axios from 'axios';
                     <tr v-if="week6!=null" class="text-dark" >
                         <td  v-for="day in week6" :key="day" colspan="2" class="" > 
                             <text class="h1 fw-lighter" @click="dayPicked(day.date)" v-if="day.date.getDate() > 10"  :class="[{ 'text-danger' : tday.getDate() == day.date.getDate()  && tday.getMonth() == day.date.getMonth() , 'text-primary text-decoration-underline fw-normal' : calendar_date.getDate() == day.date.getDate()  && calendar_date.getMonth() == day.date.getMonth() ,  'opacity-100 fw-lighter' :  day.date.getDate() < 10    }]" > 
-                                <text class="m-0 p-0"> {{day.date.getDate()}}  </text>
-                                <br> 
+                                
+                                <div class="m-0 p-0 d-flex justify-content-center"> 
+                                   <text>{{day.date.getDate()}}</text>
+                                   <i v-if="day.locked"  style="margin-left: -30px;" class="opacity-50 text-secondary bi bi-lock-fill" >
+                                    </i>
+                                    <text v-if="day.reserved>0" style="font-size: 0.5em;" >{{day.reserved}}</text> 
+                                </div>
 
-                                <div class="d-flex flex-wrap opacity-50 justify-content-center" >
+                                <div class="d-flex flex-wrap opacity-50 justify-content-center"  style="height:8px;"  >
                                
                                     <div class="" v-for="color in day.calendar_colors" :key="color"  style="height:8px; width:8px; margin-right: 2px;" :style="{'background-color': color }" > 
                                     </div>
                                 </div>
-
-                            <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i>  
+                                <!--
+                                <i v-if="day.locked"  class="bi bi-lock-fill" style="font-size: 0.7em;"></i>  
                                 <text v-if=" day.reserved>0 " style="font-size: 0.5em;" >&nbsp;{{day.reserved}}  </text>    
+                                -->
                             </text>  
                         </td>                      
                     </tr>
@@ -191,10 +225,10 @@ import axios from 'axios';
             </table> 
 
 
-            <div class="d-flex justify-content-between">
+            <div class="d-flex justify-content-between m-2">
 
                 <text>
-                    <i @click="swLock(calendar_date)" class="fs-1 bi bi-unlock-fill opacity-50"></i> 
+                    <i @click="swLock(calendar_date)" class="text-primary fs-1 bi bi-unlock-fill opacity-50"></i> 
                 </text>
 <!--
                 <div @click="show_days = !show_days" class="text-primary m-2 display-5">
@@ -202,7 +236,7 @@ import axios from 'axios';
                 </div>    
 -->
                 <text @click="show_days = !show_days">
-                    <i class=" text-primary bi bi-x display-3"></i>
+                    <i class=" text-primary bi bi-x-lg display-5"></i>
                 </text>
 
             </div>
