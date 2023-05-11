f0212720b8de9d9dcb21b67a5fa03022211f3194
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-05-11 01:19:00 -0400

    Update AppTypeCircle.vue

--- src/components/publicSearch/AppTypeCircle.vue
+++ src/components/publicSearch/AppTypeCircle.vue
@@ -4,7 +4,7 @@ import { ref } from 'vue'
 </script>
 
 <template>
-        <div @click="$emit('appTypeSelected',appType); " :style="{'border-radius': '50%' ,'width': '5.5em', 'height': '5.5em' }"  class="border    p-0 m-2 d-flex justify-content-center align-items-center" :class="{'bg-primary border-4 border-primary': active }" > 
+        <div @click="checkButton(appType)" :style="{'border-radius': '50%' ,'width': '5.5em', 'height': '5.5em' }"  class="border    p-0 m-2 d-flex justify-content-center align-items-center" :class="{'bg-primary border-4 border-primary': active }" > 
             <div class="m-2">
                 <i  class="bi  m-0  display-5 d-flex justify-content-center" :class="{ 'bi-building':appType==0 , 'bi-house-door':appType==1,'bi-camera-video':appType==2 } " :style="{ 'color' : getIconColor(appType) } " ></i>
             <text style="font-size: 0.7em;" :class="{'text-light':active}"> {{icon_name[appType]}}</text>
@@ -50,6 +50,20 @@ mounted() {
         },
 
     methods: {
+        checkButton(val)
+        {
+          if (this.active)
+          {
+          this.$emit('appTypeSelected',null); 
+          this.active = false  
+          }  
+          else 
+          {
+          this.$emit('appTypeSelected',val); 
+          
+          }
+        },
+
         getIconColor(val)
         {
             if (this.appTypeCode == this.appType )
