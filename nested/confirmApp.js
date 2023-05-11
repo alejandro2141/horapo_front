6be21727b149930dff5a44c310bc640f61f7f67f
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-05-10 23:51:52 -0400

    Update confirmApp.js

--- nested/confirmApp.js
+++ nested/confirmApp.js
@@ -4,7 +4,7 @@ import { BKND_CONFIG } from '../config123.js'
 
 const app = createApp(App)
 app.mount('#app')
-app.config.globalProperties.BKND_CONFIG = BKND_CONFIG
+//app.config.globalProperties.BKND_CONFIG = BKND_CONFIG
 
-console.log("confirmApp app config "+JSON.stringify(app.config))
-console.log("confirmApp app BKND_CONFIG "+JSON.stringify(BKND_CONFIG))
+//console.log("confirmApp app config "+JSON.stringify(app.config))
+//console.log("confirmApp app BKND_CONFIG "+JSON.stringify(BKND_CONFIG))
