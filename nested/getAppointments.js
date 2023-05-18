447ab16e93f88c031e09f41ff0f15880b77cb556
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-05-18 17:34:31 -0400

    Delete getAppointments.js

--- nested/getAppointments.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './getAppointments.vue'
-import { BKND_CONFIG } from '../config123.js'
-
-const app = createApp(App)
-app.mount('#app')
-app.config.globalProperties.BKND_CONFIG = BKND_CONFIG
-
-console.log(app.config)
