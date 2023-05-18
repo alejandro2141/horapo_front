6a8317ae469d373b3ca92663239635b222fb81dc
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-05-18 17:33:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ module.exports = defineConfig({
         main: resolve(__dirname, 'index.html'),
         aboutUs: resolve(__dirname, './nested/aboutUs.html'),
         register: resolve(__dirname, './nested/register.html'),
-        getappointments: resolve(__dirname, './nested/getAppointments.html'),
+       // getappointments: resolve(__dirname, './nested/getAppointments.html'),
         publicsearch: resolve(__dirname, './nested/publicSearch.html'),
         professionalaccess: resolve(__dirname, './nested/professionalAccess.html'),
         publicSearchCalendar: resolve(__dirname, './nested/publicSearchCalendar.html'),
