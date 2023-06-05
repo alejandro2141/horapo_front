6f3375c3e056f0c55f95034d2066453aeae2f8c8
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-06-04 23:00:17 -0400

    Update PublicShareSite.vue

--- src/components/publicSearch/PublicShareSite.vue
+++ src/components/publicSearch/PublicShareSite.vue
@@ -17,17 +17,21 @@ defineProps({
     <div class="m-5 border border-1 bg-white" id="form" style="border-radius: 15px;" >
      
         <div class="p-3">
-           <text class="d-flex justify-content-center m-2 ">Compartir en redes sociales</text>
+           
+            <text @click="sendCloseDialog()" class="d-flex justify-content-end m-2 "> <i class="display-5 bi bi-x-lg text-primary"></i> </text>
+            
+            <text class="d-flex justify-content-center m-2 ">Compartir en redes sociales</text>
             <div class="d-flex justify-content-center">
                 
-                <a href="whatsapp://send?text=Visita%20HoraPO%2C%20es%20las%20mejores%20consultas%20de%20salud%20en%20un%20solo%20lugar%20Whatsapp%20https://www.horapo.com/">  <i class="bi bi-whatsapp display-4  m-3"></i> </a>
+                <a target="_blank" href="whatsapp://send?text=Visita%20HoraPO%2C%20es%20las%20mejores%20consultas%20de%20salud%20en%20un%20solo%20lugar%20Whatsapp%20https://www.horapo.com/">  <i class="bi bi-whatsapp display-4  m-3"></i> </a>
            
-                <a href="http://www.facebook.com/share.php?u=https://www.horapo.com/&title=HoraPO">  <i class="display-4  bi bi-facebook m-3"></i> </a>
+                <a target="_blank" href="http://www.facebook.com/share.php?u=https://www.horapo.com/&title=En%20HoraPO%20puedes%20buscar%20horas%20de%20salud.%20Los%20mejores%20profesionales%20de%20salud%20en%20un%20solo%20lugar">  <i class="display-4  bi bi-facebook m-3"></i> </a>
 
-                <a href="https://twitter.com/share?url=https://www.horapo.com/&text=HoraPO">  <i class="display-4  bi bi-twitter m-3"></i> </a>
+                <a target="_blank" href="https://twitter.com/share?url=https://www.horapo.com/&text=En%20HoraPO%20puedes%20buscar%20horas%20de%20salud.%20Los%20mejores%20profesionales%20de%20salud%20en%20un%20solo%20lugar">  <i class="display-4  bi bi-twitter m-3"></i> </a>
 
-                <a href="https://www.pinterest.com/pin/find/?url=https://www.horapo.com/">  <i class="display-4  bi bi-pinterest m-3"></i>  </a>
-            <!--
+                <!--
+                <a target="_blank" href="https://www.pinterest.com/pin/find/?url=https://www.horapo.com/">  <i class="display-4  bi bi-pinterest m-3"></i>  </a>
+            
                 <a href="mailto:correodestinatario@correo.com?subject=HoraPO.%20Las%20mejores%20profesionales%20de%20salud%20en%20un%20solo%20lugar&body=Email%20Body%20Text">asdfasdf<i class="bi bi-envelope-fill display-4  m-2"></i></a>
             -->
             </div> 
@@ -57,7 +61,10 @@ export default {
             user_email : null ,
            }   
     },
-    
+ 
+    props: [], 
+    emits: ['closePublicShareSite'  ],
+
     mounted () {   
        },
 
@@ -65,6 +72,11 @@ export default {
         },
         
     methods: {
+
+        sendCloseDialog() 
+        {
+            this.$emit("closePublicShareSite","nothing");
+        }
 
         /*
             async sendRecoverApp()
