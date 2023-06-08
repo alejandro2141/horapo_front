c1bcd024257e02bdd8e6a9b6fd987405b9402861
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-06-08 18:49:49 -0400

    Update mainPageMonitoring.vue

--- src/components/adminMonitoring/mainPageMonitoring.vue
+++ src/components/adminMonitoring/mainPageMonitoring.vue
@@ -13,7 +13,7 @@ import { BKND_CONFIG } from '../../../config123.js'
 
 
     <div class="border border-1">
-        Users Rergistered {{ p_register.length }}
+        
         <div v-for="reg of p_register">
         {{ reg.email }}  {{ reg.name }}<br>
 
