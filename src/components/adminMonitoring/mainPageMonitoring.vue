bb75daaf02c208d00f3e1999c07ae02d69c1ffc2
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-06-08 18:37:00 -0400

    Create mainPageMonitoring.vue

---
+++ src/components/adminMonitoring/mainPageMonitoring.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+import { BKND_CONFIG } from '../../../config123.js'
+
+
+</script>
+
+<template>
+<div>
+
+    MAIN PAGE MONITORING
+
+
+    <div class="border border-1">
+        Users Rergistered {{ p_register.length }}
+        <div v-for="reg of p_register">
+        {{ reg.email }}  {{ reg.name }}<br>
+
+        </div>
+    </div>
+
+</div>
+</template>
+
+<style scoped>
+</style>
+
+
+<script>
+
+export default {
+
+data: function () {
+		return {
+            p_register : null 
+		 }
+	},
+
+	props : [],
+  	emits : [] ,
+
+
+    mounted () {
+        this.getProfessionalRegisters()
+         },
+ 
+    methods: {
+        
+        //GET Center List
+        async getProfessionalRegisters() {
+                        const json = { 
+                      			   
+                                      };
+
+                        console.log ("GET PROFESIONAL REGISTERS :"+ JSON.stringify(json)  );
+                        let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/monitoring_get_professional_registers",json);
+                        this.p_register = response_json.data.rows;
+                        console.log ("RESPONSE PROFESIONAL REGISERS:"+JSON.stringify(this.p_register)) ;                       
+                    },	
+
+
+		},
+  
+
+}
+</script>
+
