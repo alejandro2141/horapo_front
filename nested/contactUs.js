6a4a2e38e281e95588deb87a1ad11805a89cafef
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-06-12 20:31:28 -0400

    Create contactUs.js

---
+++ nested/contactUs.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './contactUs.vue'
+import { BKND_CONFIG } from '../config123.js'
+
+const app = createApp(App)
+app.mount('#app')
+app.config.globalProperties.BKND_CONFIG = BKND_CONFIG
+
+console.log(app.config)
