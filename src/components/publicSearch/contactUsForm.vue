8449f73b3bbf9ea3257a52ba7121a7232f251d13
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-06-14 19:09:14 -0400

    Update contactUsForm.vue

--- src/components/publicSearch/contactUsForm.vue
+++ src/components/publicSearch/contactUsForm.vue
@@ -22,15 +22,16 @@ import axios from 'axios';
         -->
     <div v-if="show_confirm == !true">
         <div class="d-flex justify-content-between"> 
-                <text class="text-secondary h5">Envianos un mensaje</text>
+                <text class="text-secondary h5 m-2">Envíanos un mensaje</text>
 
                 <div class=""> 
                     <a HREF="/index.html" type="button" class=""><i class="display-5 bi bi-x-lg text-primary"></i></a>
                 </div>
         </div>
 
-        <input type="email" style="border-radius: 10px;" autocomplete="off" class="form-control w-50 m-1 " id="inputEmail4" placeholder="Ingrese su Email" v-model="email">
-       
+        Email<br>
+        <input type="email" style="border-radius: 10px;" autocomplete="off" class="form-control w-75 m-1 " id="inputEmail4" placeholder="Ingrese su Email" v-model="email">
+       Mensaje
         <div class="d-flex justify-content-start">
 						
                         <div>
@@ -38,24 +39,29 @@ import axios from 'axios';
 							<textarea style="border-radius: 10px; "  class="form-control h-75 w-90 m-2 p-3" id="story" v-model="text_message" name="story"  placeholder="Comment text.">
 							</textarea>
 						</div>
-						<div class="m-1">
-							<text class="text-dark"><small>Tu Animo</small></text> <br>
-							<p>
+						
+		</div>
+
+        Incluye tu Ánimo<br>
+        <div class="m-1 d-flex justify-content-between">
+							
+							<text>
 								<i @click="animo =1;text_message=text_message.concat(' :( ')" class="text-danger bi bi-emoji-angry h2 m-4 "></i><br>
-							</p>
-							<p>
+                            </text>
+							<text>
 							<i @click="animo =2;text_message=text_message.concat(' :| ')" class="text-secondary bi bi-emoji-neutral h2 m-4 "></i><br>
-							</p>
-							<p>
+                            </text>
+							<text>
 							<i @click="animo =3;text_message=text_message.concat(' :) ')"  class="text-success bi bi-emoji-heart-eyes h2 m-4 "></i><br>
-							</p>
+                            </text>
 							<!--
 							<i @click="animo =4;text_message=text_message.concat(' Tiene errores de software.  Arreglalo infame programador !!. ') " class="text-white bi bi-bug h4 p-2"></i><br>
 							<i @click="animo =5;text_message=text_message.concat(' Esta bueno. Me gusta.  Puede mejorar !. ') " class="text-white bi bi-balloon-heart h4 p-2"></i><br>
 							-->
-						</div>
 		</div>
-        
+
+        <br>
+        <br>
         <div class="d-flex justify-content-center  ">
             <div @click="sendComments()" class="border border-2 p-3 bg-primary text-white " style="border-radius: 15px;" >
                 <text> Enviar <i class="bi bi-send " ></i> </text>
@@ -94,7 +100,7 @@ export default {
     return {
        text_message: "",
        email: "" ,
-       animo : "" , 
+       animo : null , 
        show_confirm : false 
     }
   },
@@ -119,25 +125,14 @@ export default {
                 const json = { 
                    message	:	this.text_message ,
                    email : this.email, 
-                   animo : this.animo.toString()
+                   animo : this.animo
                         };
 
                  //app.config.globalProperties.dbhost = 'http://192.168.0.114:8080' ;       
                 console.log ("public_send_comments  REQUEST :"+ JSON.stringify(json)  );
                 let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/public_send_comments",json );
                 console.log ("public_send_comments  RESPONSE:"+JSON.stringify(response_json.data)) ;
-                //app = response_json.data[0];
-                //alert("Gracias por su Registro");
-                //console.log ("Rows : "+JSON.stringify(response_json.data.rows[0].name )  );
-                //if (response_json.data.rows[0] )
-               // if (response_json.data.rows[0].name != null )
-               // {
-                    //display modal 
-                    //this.display = true ;
-                //location.href = "/index.html";
-                //window.location.replace("/index.html");
-                //this.$emit('closeRegisterForm', response_json.data );
-                //}
+               
 
                 this.show_confirm = true ; 
 
