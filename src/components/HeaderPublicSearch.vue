72725fb8bde666bc83131e0b025126742b399e02
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-08-03 18:14:17 -0400

    Update HeaderPublicSearch.vue

--- src/components/HeaderPublicSearch.vue
+++ src/components/HeaderPublicSearch.vue
@@ -10,22 +10,27 @@ import PublicShareSite from './publicSearch/PublicShareSite.vue';
 <div >
     <!-- style="background-color:#2f3e44"--> 
   	
-    <div  class="d-flex justify-content-center  mt-0 pt-0 text-secondary"> 
+    <div  class="d-flex justify-content-between  mt-0 pt-0 text-secondary"> 
 
         
-        <div class="p-3 border" style="border-bottom-right-radius: 40%;" >
+        <div class="p-1"  >
             <!-- 
             <i  @click="show_sub_menu = !show_sub_menu" class="fs-2 bi bi-list text-secondary"></i>
             -->
-            <text v-if="!show_sub_menu" class="p-2" @click="closeAllDialog();show_sub_menu = !show_sub_menu; showRecoverApp=false ;showShareSite=false" style="font-size: 1.0em ; "> <i class="bi bi-water text-primary"></i> </text>
-            
+            <text v-if="!show_sub_menu" class="p-2" @click="closeAllDialog();show_sub_menu = !show_sub_menu; showRecoverApp=false ;showShareSite=false" style="font-size: 1.5em ; "> <i class="bi bi-water"></i> </text>
             <text v-else class="p-2" @click="show_sub_menu = !show_sub_menu" style="font-size: 1.5em ; "> <i class="bi bi-x-lg text-primary"></i> </text>
 
         </div>
  
+        <div>&nbsp;&nbsp;&nbsp;&nbsp;</div>
 
-        <div class="m-0 p-0  mt-3 text-center">
-            <text @click="showShareSite=!showShareSite ; show_sub_menu=false; showRecoverAppointment=false" class="m-5 p-4" style="font-size: 1.7em ; color: rgb(0, 112, 97)" >horapo</text> <br>
+        <div class="p-1   text-center text-success" style="font-size: 1.7em ; ">
+            <div>
+                <i class="bi bi-bug"></i> 
+                <text @click="showShareSite=!showShareSite ; show_sub_menu=false; showRecoverAppointment=false" class=""  >
+                horapo
+                </text> 
+            </div>
             <!--  <text class="m-0 p-0" style="font-size: 0.8em">Mejores consultas un solo lugar</text>
             -->
         </div>
@@ -35,10 +40,16 @@ import PublicShareSite from './publicSearch/PublicShareSite.vue';
                 <text class="">horapo</text>  
             </a>
             -->
+
+        <div>&nbsp;&nbsp;&nbsp;&nbsp;</div>
      
 
-        <div class="p-3 border "   style="border-bottom-left-radius: 40%;">
-            <text v-if="true" @click="showRecoverAppointment=!showRecoverAppointment ; show_sub_menu=false  ; showShareSite=false   " class="p-2 mt-2 text-primary text-center" style="font-size: 0.9em; ">Mis&nbsp;Citas</text>
+        <div class="p-1 "   style="">
+            <text v-if="true" @click="showRecoverAppointment=!showRecoverAppointment ; show_sub_menu=false  ; showShareSite=false   " class="p-2 mt-2  text-center" style="font-size: 1.7em; ">
+            
+                <i class="bi bi-clock-history"></i>
+            
+            </text>
             <!--
             <text v-if="!showRecoverAppointment" @click="show_sub_menu=false  ; showShareSite=false ;showRecoverAppointment=true;  " class="p-2 mt-2 text-primary" style="font-size: 1em; ">citas</text>
             <text v-else class="p-2" @click=" showRecoverAppointment = false ; showShareSite=false ; show_sub_menu=false" style="font-size: 1em ; "> <i class="bi bi-x-lg text-primary p-2"></i> </text>
@@ -50,11 +61,10 @@ import PublicShareSite from './publicSearch/PublicShareSite.vue';
           SUB MENU        
     <div class="d-flex justify-content-end"><a href="/index.html" class=""><i class="display-3 bi bi-x-lg"></i></a></div>
 ---------------------->
-
-    <RecoverAppointmentsForm v-on:closeRecoverAppointmentForm="closeRecoverAppointmentForm" v-if="showRecoverAppointment" style="position: absolute; top: 4em;  z-index: 9;  border-bottom-left-radius: 10px 10px;border-radius: 15px;"></RecoverAppointmentsForm>
-    <PublicShareSite v-on:closePublicShareSite="closePublicShareSite"  v-if="showShareSite" style="position: absolute; top: 4em;  z-index: 9;  border-bottom-left-radius: 10px 10px;border-radius: 15px;" ></PublicShareSite>
-
-    <div v-if="show_sub_menu"   class=" text-primary bg-white border w-100 p-1"  style="font-size: 1.4em ;  position: absolute; top: 3em;  z-index: 999;  border-bottom-left-radius: 10px 10px;border-radius: 15px;"> 
+       
+    
+    
+    <div v-if="show_sub_menu"   class=" text-primary bg-white border w-100 p-1"  style="font-size: 1.4em ;  position: absolute; top: 2em;  z-index: 999;  border-bottom-left-radius: 10px 10px;border-radius: 15px;"> 
             <!--
                 <div class="p-4 ">
                         <a href="/nested/aboutUs.html" class="m-1 text-decoration-none btn-floating "  role="button">
@@ -102,6 +112,13 @@ import PublicShareSite from './publicSearch/PublicShareSite.vue';
 
     </div>
 
+
+    <div class="d-flex justify-content-center">
+      <PublicShareSite v-on:closePublicShareSite="closePublicShareSite"  v-if="showShareSite" style="position: absolute; top: 3em;  z-index: 9;  border-bottom-left-radius: 10px 10px;border-radius: 15px;" ></PublicShareSite>
+      <RecoverAppointmentsForm v-on:closeRecoverAppointmentForm="closeRecoverAppointmentForm" v-if="showRecoverAppointment" style="position: absolute; top: 3em;  z-index: 9;  border-radius: 15px;"></RecoverAppointmentsForm>
+      
+    </div>
+    
 
       
 
