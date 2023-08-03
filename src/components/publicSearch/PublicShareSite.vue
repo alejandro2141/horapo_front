043e2ea68bfd58f5c94df162430222b3158902a0
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-08-03 18:14:14 -0400

    Update PublicShareSite.vue

--- src/components/publicSearch/PublicShareSite.vue
+++ src/components/publicSearch/PublicShareSite.vue
@@ -13,24 +13,24 @@ defineProps({
 
 
 <template>
-<div class="">
-    <div class="m-1 border border-1 bg-white" id="form" style="border-radius: 15px;" >
+<div class="w-100 ">
+    <div class="m-0 border border-1 bg-white  " id="form" style="border-radius: 15px;" >
      
-        <div class="p-3">
+        <div class="p-5">
            
-            <div class="d-flex justify-content-between"> 
+            <div class="d-flex justify-content-between p-4"> 
                 <text></text>
-                <text class="d-flex justify-content-center  m-0 h5">Compartir</text>
+                <text class="d-flex justify-content-center  m-0 h5 ">Compartir</text>
                 <text @click="sendCloseDialog()" class="d-flex justify-content-end "> <i class="h3 bi bi-x-lg text-primary"></i> </text>
             </div>
 
-            <div class="d-flex justify-content-center">
+            <div class="d-flex justify-content-center " style="font-size:3em;">
                 
-                <a target="_blank" href="whatsapp://send?text=Visita%20HoraPO%2C%20Las%20mejores%20consultas%20de%20salud%20en%20un%20solo%20lugar%20https://www.horapo.com/">  <i class="bi bi-whatsapp display-4  m-3"></i> </a>
+                <a target="_blank" href="whatsapp://send?text=Visita%20HoraPO%2C%20Las%20mejores%20consultas%20de%20salud%20en%20un%20solo%20lugar%20https://www.horapo.com/">  <i class="text-success bi bi-whatsapp   m-3 p-3"></i> </a>
            
-                <a target="_blank" href="http://www.facebook.com/share.php?u=https://www.horapo.com/&title=En%20HoraPO%20puedes%20buscar%20horas%20de%20salud.%20Los%20mejores%20profesionales%20de%20salud%20en%20un%20solo%20lugar">  <i class="display-4  bi bi-facebook m-3"></i> </a>
+                <a target="_blank" href="http://www.facebook.com/share.php?u=https://www.horapo.com/&title=En%20HoraPO%20puedes%20buscar%20horas%20de%20salud.%20Los%20mejores%20profesionales%20de%20salud%20en%20un%20solo%20lugar">  <i class=" bi bi-facebook m-3 p-3"></i> </a>
 
-                <a target="_blank" href="https://twitter.com/share?url=https://www.horapo.com/&text=En%20HoraPO%20puedes%20buscar%20horas%20de%20salud.%20Los%20mejores%20profesionales%20de%20salud%20en%20un%20solo%20lugar">  <i class="display-4  bi bi-twitter m-3"></i> </a>
+                <a target="_blank" href="https://twitter.com/share?url=https://www.horapo.com/&text=En%20HoraPO%20puedes%20buscar%20horas%20de%20salud.%20Los%20mejores%20profesionales%20de%20salud%20en%20un%20solo%20lugar">  <i class="  bi bi-twitter m-3 p-3" ></i> </a>
                 <!--
                 <a target="_blank" href="https://www.pinterest.com/pin/find/?url=https://www.horapo.com/">  <i class="display-4  bi bi-pinterest m-3"></i>  </a>
                 <a href="mailto:correodestinatario@correo.com?subject=HoraPO.%20Las%20mejores%20profesionales%20de%20salud%20en%20un%20solo%20lugar&body=Email%20Body%20Text">asdfasdf<i class="bi bi-envelope-fill display-4  m-2"></i></a>
