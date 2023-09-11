4e732e67a50d3d4813a21c02dc046152ce756dc7
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-09-11 19:12:32 -0300

    Update appointmentBlockedByProfessional.vue

--- src/components/professionalAccess/appointmentBlockedByProfessional.vue
+++ src/components/professionalAccess/appointmentBlockedByProfessional.vue
@@ -35,7 +35,7 @@ import axios from 'axios';
                                 
                                     <div class="">
                                         <text class="" > 
-                                            <small> <small> {{center_data.name}} </small> </small>
+                                            <small> <small> {{center_data.name.substring(0,20)}} </small> </small>
                                         </text>
                                     </div>
                                 </div>    
