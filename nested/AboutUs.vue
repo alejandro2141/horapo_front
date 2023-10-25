d17b7458060a610a9b12a8568b4c83349925fcd7
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-10-25 18:51:00 -0300

    Update AboutUs.vue

--- nested/AboutUs.vue
+++ nested/AboutUs.vue
@@ -11,6 +11,9 @@ import { BKND_CONFIG } from '../config123.js'
 <div>
     <GeneralHeader></GeneralHeader>
 
+
+    <!-- DIV SHOW BIG IMAGE --> 
+<!--
     <div v-if="showBigImg" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
         <div class="d-flex justify-content-end m-3"> 
                
@@ -25,6 +28,111 @@ import { BKND_CONFIG } from '../config123.js'
         </div>
     
     </div>
+-->
+
+
+    <div v-if="showBigImg1" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg1=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/quees_horapo_graphic1.png" > 
+        </div>
+    </div>
+
+    <div v-if="showBigImg2" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg2=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/quees_horapo_graphic2.png" > 
+        </div>
+    </div>
+
+    <div v-if="showBigImg3" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg3=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/public/quees_horapo_graphic3.png" > 
+        </div>
+    </div>
+
+    <div v-if="showBigImg4" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg4=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/public/quees_horapo_graphic4.png" > 
+        </div>
+    </div>
+
+    <div v-if="showBigImg5" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg5=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/public/quees_horapo_graphic5.png" > 
+        </div>
+    </div>
+
+
+    <div v-if="showBigImg6" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg6=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/public/quees_horapo_graphic6.png" > 
+        </div>
+    </div>
+
+    <div v-if="showBigImg10" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg10=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/public/quees_horapo_graphic10.png" > 
+        </div>
+    </div>
+
+    <div v-if="showBigImg11" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg11=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/public/quees_horapo_graphic11.png" > 
+        </div>
+    </div>
+
+    <div v-if="showBigImg12" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg12=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/public/quees_horapo_graphic12.png" > 
+        </div>
+    </div>
+
+
+    <div v-if="showBigImg13" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg13=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/public/quees_horapo_graphic13.png" > 
+        </div>
+    </div>
+
+    <div v-if="showBigImg14" class="bg-white p-3"  style="width : 100%; position: fixed;   top: 50%;   left: 50%;   transform: translate(-50%, -50%); ">
+        <p class="text-end"> 
+                <i @click="showBigImg14=false"  class="text-primary display-1 bi bi-x-lg"></i>
+        </p>
+        <div class="d-flex justify-content-center">
+            <img class="m-2" style="width : 20em ; "  src="/public/quees_horapo_graphic14.png" > 
+        </div>
+    </div>
+
+
 
     <div class="bg-light">
 
@@ -51,8 +159,8 @@ import { BKND_CONFIG } from '../config123.js'
         </div>
       
     <div class="d-flex justify-content-around">
-        <img @click="showImg(1)" class="m-2 text-center" style="width : 7em ; "  src="/public/quees_horapo_graphic1.png" >
-        <img @click="showImg(2)" class="m-2 text-center" style="width : 7em ; "  src="/public/quees_horapo_graphic2.png" >
+        <img @click="showBigImg1 = true" class="m-2 text-center" style="width : 7em ; "  src="/quees_horapo_graphic1.png" >
+        <img @click="showBigImg2 = true" class="m-2 text-center" style="width : 7em ; "  src="/quees_horapo_graphic2.png" >
     </div>
 
     <p class="align-items-star m-2">  <b>horapo</b> es la plataforma Web de búsqueda y reserva de horas de salud,  No es una App, y no requiere instalacion en tu smartphone
@@ -65,7 +173,7 @@ import { BKND_CONFIG } from '../config123.js'
         
         <div class="d-flex justify-content-start">
             <div>
-                <img @click="showImg(10)" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic10.png" >     
+                <img @click="showBigImg10 = true" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic10.png" >     
             </div>
             <div class="d-flex align-items-center">
                 <i class="bi bi-check2-square text-success h3"></i> Publican sus agendas de horas disponibles
@@ -75,7 +183,7 @@ import { BKND_CONFIG } from '../config123.js'
         <div class="d-flex justify-content-start">
             
             <div>
-                <img @click="showImg(11)" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic11.png" >     
+                <img @click="showBigImg11 = true" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic11.png" >     
             </div>
             <div class="d-flex align-items-center">
                 <i class="bi bi-check2-square text-success h3"></i> Envian a pacientes el LinkWeb de su Agenda para una busqueda de horas disponibles.
@@ -85,7 +193,7 @@ import { BKND_CONFIG } from '../config123.js'
 
         <div class="d-flex justify-content-start">
             <div>
-                <img @click="showImg(12)" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic12.png" >     
+                <img @click="showBigImg12=true" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic12.png" >     
             </div>
             
             <div class="d-flex align-items-center">
@@ -97,7 +205,7 @@ import { BKND_CONFIG } from '../config123.js'
 
         <div class="d-flex justify-content-start">
             <div>
-                <img @click="showImg(13)" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic13.png" >  
+                <img @click="showBigImg13=true" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic13.png" >  
             </div>
             
             <div class="d-flex align-items-center">
@@ -110,7 +218,7 @@ import { BKND_CONFIG } from '../config123.js'
 
         <div class="d-flex justify-content-start">
             <div>
-                <img @click="showImg(14)" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic14.png" >  
+                <img @click="showBigImg14=true" class="m-1" style="width : 5em ; "  src="/public/quees_horapo_graphic14.png" >  
             </div>
             
             <div class="d-flex align-items-center">
@@ -136,7 +244,7 @@ import { BKND_CONFIG } from '../config123.js'
 <div class="m-2">
    
     <div class="d-flex flex-column align-items-center">
-        <img @click="showImg(3)" class="m-2" style="width : 18em ; "  src="/public/specialties.png" > 
+        <img @click="showBigImg3=true" class="m-2" style="width : 18em ; "  src="/public/specialties.png" > 
     </div>
 </div>
 
@@ -160,20 +268,20 @@ import { BKND_CONFIG } from '../config123.js'
             <div>
             Busca
             </div>
-            <img @click="showImg(4)" class="m-1" style="width : 7em ; "  src="/public/quees_horapo_graphic4.png" > 
+            <img @click="showBigImg4=true" class="m-1" style="width : 7em ; "  src="/public/quees_horapo_graphic4.png" > 
             
         </div>
         <div class="text-center">
             <div>
             Encuentra
             </div>
-            <img @click="showImg(5)" class="m-1" style="width : 7em ; "  src="/public/quees_horapo_graphic5.png" > 
+            <img @click="showBigImg5=true" class="m-1" style="width : 7em ; "  src="/public/quees_horapo_graphic5.png" > 
         </div>
         <div class="text-center">
             <div>
             Reserva
             </div>
-            <img @click="showImg(6)" class="m-1" style="width : 7em ; "  src="/public/quees_horapo_graphic6.png" > 
+            <img @click="showBigImg6=true" class="m-1" style="width : 7em ; "  src="/public/quees_horapo_graphic6.png" > 
         </div>
     </div>
 
@@ -271,6 +379,20 @@ export default {
        showForm:false ,
        showBigImg : false,
        showBigImgN : 0,
+
+       showBigImg1 : false ,
+       showBigImg2 : false ,
+       showBigImg3 : false ,
+       showBigImg4 : false ,
+       showBigImg5 : false ,
+       showBigImg6 : false ,
+
+       showBigImg10 : false ,
+       showBigImg11 : false ,
+       showBigImg12 : false ,
+       showBigImg13 : false ,
+       showBigImg14 : false ,
+
     }
   },
 
