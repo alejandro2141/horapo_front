7bd6c5765d6db1a7b814800063c2fd23dffe18f4
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-09-27 19:53:15 -0300

    Update professionalAccess.vue

--- nested/professionalAccess.vue
+++ nested/professionalAccess.vue
@@ -244,7 +244,7 @@ methods: {
    			   	      };
 			console.log ("REQUEST to professional_login_session:"+ JSON.stringify(json)  );
 			
-			let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/professional_login_session",json);
+			let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_login_session",json);
             console.log ("RESPONSE login:"+JSON.stringify(response_json.data)) ;
 
 
@@ -296,14 +296,14 @@ methods: {
 
     async loadGlobalSpecialties() {
                 console.log ("APP GET SPECIALTY LIST METHOD"); 
-				let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_specialty_list");
+				let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/common_get_specialty_list");
                 this.global_specialties = response_json.data.rows;
                 console.log("APP global_specialties: "+JSON.stringify(this.global_specialties) );
             },
 
     async loadGlobalComunas() {
                  console.log ("APP GET COMUNA LIST METHOD"); 
-				let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/common_get_comuna_list");
+				let response_json = await axios.post(this.BKND_CONFIG.BKND_HOST+"/common_get_comuna_list");
                 this.global_comunas = response_json.data.rows;
                 console.log("APP Comuna list: "+JSON.stringify(this.global_comunas) );
             },
