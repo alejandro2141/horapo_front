844452d9f09838ee6cefd3ab155c009e100fbe22
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-10-01 14:56:15 -0300

    Update tutorial_center.vue

--- src/components/professionalAccess/tutorial_center.vue
+++ src/components/professionalAccess/tutorial_center.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios';
+import { BKND_CONFIG } from '../../../config123.js'
 
 </script>
 
@@ -129,7 +130,7 @@ data: function () {
                      professional_id: this.session_params.professional_id ,
                      tutorial: 2,
            };
-            let restemp = await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_shutdown_tutorial",json);
+            let restemp = await axios.post(BKND_CONFIG.BKND_HOST+"/professional_shutdown_tutorial",json);
             this.session_params.tutorial_center = false ;   
         },
 
@@ -149,7 +150,7 @@ data: function () {
               tutorial : 2
            };
 
-            let restemp = await axios.post(this.BKND_CONFIG.BKND_HOST+"/professional_shutdown_tutorial",json);
+            let restemp = await axios.post(BKND_CONFIG.BKND_HOST+"/professional_shutdown_tutorial",json);
             this.session_params.tutorial_center = false ;   
         },
  
